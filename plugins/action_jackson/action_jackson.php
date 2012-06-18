03beaa884fa657e9925b3c70f294406bc86689aa
Sebastian Frohm <sebastian.frohm@gmail.com>
2012-06-18 16:54:23 -0500

    Adding baseline action jackson

--- plugins/action_jackson/action_jackson.php
+++ plugins/action_jackson/action_jackson.php
@@ -14,20 +14,76 @@
 	@define('ACTIONJACSON_ACTIONS_DIR', 'base/');
 	@define('ACTIONJACSON_INSTANCES_DIR', 'controllers/instances/');
 	@define('ACTIONJACSON_VIEWS_DIR', 'views/');
-	
+
 	//include(USERACTIONS_ACTIONS_DIR.'UserActionsViews.actions.php');
 	//include(USERACTIONS_ACTIONS_DIR.'UserActionsAdmin.actions.php');
 	include('base/action_jackson_query.php');
+    include('models/user_action_post.php');
 
+    function get_actions() {
+        global $current_user;
+        get_currentuserinfo();
+
+        $ajQuery = new ActionJacksonQuery();
+
+        $ajQuery->getUserActions('posts', 'ID', $current_user->ID);
+        //$ajQuery->getUserActions('term', 'term_id', $current_user->ID, null, null, null, 'category');
+        //$ajQuery->addUserAction(4, 'term', 'upvote', 'something', 1);
+    }
+
+//    add_action('init', 'get_actions');
+
+    add_filter( 'posts_clauses', array('ActionJacksonQuery', 'getUserActions'), 2, 3);
+    add_filter('posts_results', 'afterwards');
+    add_filter('pre_get_posts', 'dont_suppress_filters');
+
+function afterwards($posts) {
+    $existingIds = array();
+
+    foreach($posts as $post) {
+        $actions[] = new UserActionPost($post);
+    }
+
+    foreach($posts as $key=>$post) {
+        if(!in_array($post->ID, $existingIds)) {
+            $existingIds[] = $post->ID;
+        } else {
+            unset($posts[$key]);
+        }
+    }
+
+    foreach($posts as $key=>$post) {
+        $newOrder[] = $post;
+    }
+
+    $posts = $newOrder;
+
+    foreach($posts as $post) {
+        foreach($actions as $action) {
+            if($action->objectId == $post->ID) {
+                $post->actions[] = $action;
+            }
+        }
+    }
+
+    return $posts;
+}
+
+function dont_suppress_filters($query){
+    $query->query_vars['suppress_filters'] = false;
+    return $query;
+}
+
+function addUserAction() {
     $ajQuery = new ActionJacksonQuery();
 
-    $ajQuery->getUserActions(1, 'posts', 'ID');
+    $result = $ajQuery->addUserAction((int)$_POST['id'], $_POST['type'], $_POST['userAction'], $_POST['subtype'], (int)$_POST['user']);
 
-//
-//        public function updateUserAction($actionId,
-//                                        $objId=0,
-//                                        $objType='',
-//                                        $objSubType='',
-//                                        $action='') {
+    echo json_encode($result);
+    exit;
+}
 
-    //$uaQuery->updateUserAction(4, 12, 'term', 'something', 'downvote');
+add_action('wp_ajax_add_user_action', 'addUserAction');
+add_action('wp_ajax_nopriv_add_user_action', 'addUserAction');
+
+
--- plugins/action_jackson/base/action_jackson_query.php
+++ plugins/action_jackson/base/action_jackson_query.php
@@ -15,7 +15,7 @@
                                     $objType,
                                     $action,
                                     $objSubType=null,
-                                    $userId=null) {
+                                    $userId=1) {
             if((int)$objId <= 0 || is_null($objId)) {
                 Throw new Exception('You need to pass an object ID!');
             }
@@ -24,16 +24,20 @@
                 Throw new Exception('You need to pass an object ID!');
             }
 
-            $args = array(
-                'user_id' => $userId,
-                'action_type' => $action,
-                'object_type' => $objType,
-                'object_id' => $objId,
-                'object_subtype' => $objSubType,
-                'action_added' => strtotime('now')
-           	);
+            $result = $this->_addPostAction($objId, $objType, $action, $objSubtype);
+            if(isset($result) && $result > 0) {
+                $args = array(
+                    'user_id' => $userId,
+                    'object_id' => $result,
+                    'action_added' => strtotime('now')
+               	);
 
-            $result = $this->_wpdb->insert($this->_wpdb->prefix.'user_actions', $args);
+                $result = $this->_wpdb->insert($this->_wpdb->prefix.'user_actions', $args);
+
+                return $result;
+            } else {
+                return false;
+            }
         }
 
         public function updateUserAction(
@@ -51,13 +55,32 @@
 
             //$this->update( $table, $data, $where, $format = null, $where_format = null );
             $result = $this->_wpdb->update($this->_wpdb->prefix.'user_actions', $args, array('user_action_id' => $actionId));
+            if($result == true) {
+                $this->_updatePostAction($actionId, $objId, $objType, $objSubType, $action);
+            }
         }
 
-        public function getUserActions($object_type, $object_type_id_key_name, $user_id=1, $user_action_id=null, $action_type=null, $object_id=null, $object_subtype=null, $limit=10, $page=1) {
-            global $wp_query;
-            echo '<pre>';
-            var_dump($wp_query);
-            echo '</pre>';
+//        public function getUserActions($object_type, $object_type_id_key_name, $user_id=1, $user_action_id=null, $action_type=null, $object_id=null, $object_subtype=null, $page=1, $limit=10) {
+        public function getUserActions($asdf, $qwer) {
+            $asdf['join'] .= '
+                            JOIN
+                                wp_user_actions ua
+                            JOIN
+                                wp_post_actions pa
+                                    ON
+                                        ua.object_id=pa.post_action_id';
+
+//            $asdf['where'] .= ' AND pa.action_type="upvote"';
+            $asdf['fields'] .= ', ua.*, pa.*';
+//            $asdf['distinct'] = 'DISTINCT(wp_posts.ID)';
+
+            return $asdf;
+
+            global $ua_wp_query;
+
+            wp_reset_query();
+
+            $ua_wp_query = new WP_Query('numpost='.$limit.'&paged='.$page);
 
             if(
                 ((isset($objId) && is_int($objId) && $objId > 0 ) && (!isset($objType) || is_null($objType) || $objType == ''))
@@ -75,32 +98,31 @@
             unset($args['limit']);
             unset($args['object_type_id_key_name']);
             unset($args['page']);
-
-            foreach($args as $key=>$arg) {
-                if(is_null($arg) || empty($arg)) {
-                    unset($args[$key]);
-                }
-            }
-
-            $argCount = count($args);
+            unset($args['wp_query']);
 
             $startLimit = ($page * $limit) - $limit;
 
+            $args = $this->_unsetNulls($args);
+
+            $argCount = count($args);
+
             $query = 'SELECT
-                            ua.*, '.$object_type.'.*
+                            ua.*, pa.*
                         FROM
-                            '.$this->_wpdb->prefix.'user_actions'.' ua
+                            '.$this->_wpdb->prefix.'user_actions ua
                         JOIN
-                            '.$this->_wpdb->prefix.''.$object_type.' '.$object_type.'
+                            '.$this->_wpdb->prefix.'post_actions pa
                                 ON
-                                    ua.object_id='.$object_type.'.'.$object_type_id_key_name.'
+                                    ua.object_id=pa.post_action_id
                         WHERE ';
 
             foreach($args as $key=>$arg) {
                 if(!is_null($arg) && !empty($arg)) {
                     $arg = is_string($arg) ? '"'.$arg.'"' : $arg;
 
-                    $query .= ($i < ($argCount - 1)) ? 'ua.'.$key.'='.$arg.' AND ' : 'ua.'.$key.'='.$arg;
+                    $tableAlias = ($key == 'user_id') ? 'ua' : 'pa';
+
+                    $query .= ($i < ($argCount - 1)) ? $tableAlias.'.'.$key.'='.$arg.' AND ' : $tableAlias.'.'.$key.'='.$arg;
 
                     $i++;
                 }
@@ -110,11 +132,129 @@
 
             $results = $this->_wpdb->get_results($query);
 
-            //create  model
-            //return model
+            if($object_type == 'posts') {
+                foreach($results as $key=>$result) {
+                    foreach($wp_query->posts as $post) {
+                        if($post->ID == $result->object_id) {
+                            echo 'mathec<br/>';
+                            $post->actions[] = $result;
+                        }
+                    }
+                }
+            } elseif($object_type == 'term') {
+                $terms = get_terms($object_subtype);
+                foreach($results as $key=>$result) {
+                    foreach($terms as $term) {
+                        if($term->term_id == $result->object_id) {
+                            $term->actions[] = $result;
+                        }
+                    }
+                }
+
+                return $terms;
+            }
         }
 
-        private function _addPostAction($objId, $objType, $action) {
+        public function getPostAction($object_type, $object_id, $post_action_id=null, $object_sub_type=null, $action_type=null, $limit=10, $page=1) {
+            global $wp_query;
 
+            $wp_query = new WP_Query('numpost='.$limit.'&paged='.$page);
+
+            $args = get_defined_vars();
+
+            unset($args['limit']);
+            unset($args['object_type_id_key_name']);
+            unset($args['page']);
+            unset($args['wp_query']);
+
+            $startLimit = ($page * $limit) - $limit;
+
+            $args = $this->_unsetNulls($args);
+
+            $argCount = count($args);
+
+            $query = 'SELECT
+                            *
+                        FROM
+                            '.$this->_wpdb->prefix.'post_actions
+                        WHERE ';
+
+            foreach($args as $key=>$arg) {
+                if(!is_null($arg) && !empty($arg)) {
+                    $arg = is_string($arg) ? '"'.$arg.'"' : $arg;
+
+                    $query .= ($i < ($argCount - 1)) ? $key.'='.$arg.' AND ' : $key.'='.$arg;
+
+                    $i++;
+                }
+            }
+
+            $query .= ' LIMIT '.$startLimit.','.$limit;
+
+            return $this->_wpdb->get_results($query);
+        }
+
+        private function _addPostAction($objId, $objType, $action, $objectSubtype=null) {
+            $result = $this->getPostAction($objType, $objId, null, null, $action);
+            if(isset($result) && !empty($result)) {
+                $updated = $this->_updatePostAction($result[0]->post_action_id, null, null, null, $action, $result[0]->action_total);
+                if($updated == 1) {
+                    return $result[0]->post_action_id;
+                }
+            }
+
+            $args = array(
+                'action_type' => $action,
+                'object_type' => $objType,
+                'object_subtype' => $objSubType,
+                'object_id' => $objId,
+                'action_total' => 1,
+                'last_modified' => strtotime('now')
+            );
+
+            $result = $this->_wpdb->insert($this->_wpdb->prefix.'post_actions', $args);
+            if($result == 1) {
+                return $this->_wpdb->insert_id;
+            }
+        }
+
+        private function _updatePostAction($actionId, $objId=null, $objType=null, $objSubType=null, $action=null, $action_total=null) {
+            $args = $this->_unsetNulls(array(
+                            'action_type' => $action,
+                            'object_type' => $objType,
+                            'object_subtype' => $objSubType,
+                            'object_id' => $objId,
+                            'action_total' => $action_total + 1
+                       	));
+
+            $formats = $this->_buildFormats($args);
+
+            return $this->_wpdb->update($this->_wpdb->prefix.'post_actions', $args, array('post_action_id' => $actionId), $formats, array('%d'));
+        }
+
+        private function _unsetNulls($args) {
+            foreach($args as $key=>$arg) {
+                if(is_null($arg) || empty($arg)) {
+                    unset($args[$key]);
+                }
+            }
+
+            return $args;
+        }
+
+        private function _buildFormats($args) {
+            $formats = array();
+
+            foreach($args as $key=>$arg) {
+                if(is_string($arg)) {
+                    $formats[] = '%s';
+                } elseif(is_int($arg)) {
+                    $formats[] = '%d';
+                } elseif(is_float($arg)) {
+                    $formats[] = '%f';
+                }
+            }
+
+            return $formats;
         }
     }
