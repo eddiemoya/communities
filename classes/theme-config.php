d799e71c5b4624ebaa8fed7dee2162407da56ea6
Tim Steele <tsteel5@searshc.com>
2012-06-28 13:47:11 -0500

    Eris theme file structure -- boilerplate 'whitelabel' theme merge

---
+++ classes/theme-config.php
@@ -0,0 +1,16 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of theme-config
+ *
+ * @author emoya1
+ */
+class theme-config {
+//put your code here
+}
+?>
---
+++ sidebar.php
@@ -0,0 +1,9 @@
+<div id="sidebar_right">
+    
+    <ul class="widgets">
+        <?php if ( !dynamic_sidebar('Right Sidebar') ) : ?> 
+
+        <? endif; ?>
+    </ul>
+    
+</div> <!-- #sidebar_right -->
