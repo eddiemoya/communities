2085224b18de0302210fcd41e7d486bacb98046a
Carl Albrecht-Buehler <calbre1@searshc.com>
2012-07-02 14:37:57 -0500

    Finishing styling of top level navigation.

--- parts/header.php
+++ parts/header.php
@@ -31,13 +31,41 @@
                 <?php wp_nav_menu( array( 'theme_location' => 'main-navigation', 'menu_id' => 'main-nav') ); ?>
             </div>
             
-            <nav id="headernav">
-                <ul>
-                <li style="float: right"><a href="#">Customer Care</a></li>
-                  <li><a href="#">Categories</a></li>
-                  <li><a href="#">Q&amp;A's</a></li>
-                  <li><a href="#">Blog</a></li>
-                  <li><a href="#">Buying Guides</a></li>
+            <nav>
+                <ul id="header_nav" class="clearfix">
+                  <li class="right_button"><a href="#">Customer Care</a></li>
+                  <li>
+                    <a href="#"><span>Categories</span></a>
+                    <ul>
+                      <li><a href="#">Item 1</a></li>
+                      <li><a href="#">Item 2</a></li>
+                      <li><a href="#">Item 3</a></li>
+                    </ul>
+                  </li>
+                  <li>
+                    <a href="#"><span>Q&amp;A's</span></a>
+                    <ul>
+                      <li><a href="#">Item 4</a></li>
+                      <li><a href="#">Item 5</a></li>
+                      <li><a href="#">Item 6</a></li>
+                    </ul>
+                  </li>
+                  <li>
+                    <a href="#"><span>Blog</span></a>
+                    <ul>
+                      <li><a href="#">Item 7</a></li>
+                      <li><a href="#">Item 8</a></li>
+                      <li><a href="#">Item 9</a></li>
+                    </ul>
+                  </li>
+                  <li>
+                    <a href="#"><span>Buying Guides</span></a>
+                    <ul>
+                      <li><a href="#">Item a</a></li>
+                      <li><a href="#">Item b</a></li>
+                      <li><a href="#">Item c</a></li>
+                    </ul>
+                  </li>
                 </ul>
             </nav>
             
