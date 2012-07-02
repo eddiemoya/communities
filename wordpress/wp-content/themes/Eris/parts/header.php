6fa67159a1841c54278b349670970ff8e99c4045
Carl Albrecht-Buehler <calbre1@searshc.com>
2012-07-02 15:55:36 -0500

    Removed customer care dropdown

--- wordpress/wp-content/themes/Eris/parts/header.php
+++ wordpress/wp-content/themes/Eris/parts/header.php
@@ -1,5 +1,4 @@
 <!DOCTYPE html>
-<!-- paulirish.com/2008/conditional-stylesheets-vs-css-hacks-answer-neither/ -->
 <!--[if lt IE 7]> <html class="no-js lt-ie9 lt-ie8 lt-ie7" lang="en"> <![endif]-->
 <!--[if IE 7]>    <html class="no-js lt-ie9 lt-ie8" lang="en"> <![endif]-->
 <!--[if IE 8]>    <html class="no-js lt-ie9" lang="en"> <![endif]-->
@@ -42,20 +41,6 @@
                 <ul id="header_nav" class="clearfix">
                   <li class="right_button">
                     <a href="#">Customer Care</a>
-                    <ul>
-                      <li><a href="#">Repairs, Maintenance &amp; Parts</a></li>
-                      <li><a href="#">Product Issues &amp; Questions</a></li>
-                      <li><a href="#">Protection Agreements</a></li>
-                      <li><a href="#">In-Store Issues &amp; Orders</a></li>
-                      <li><a href="#">Marketplace Orders</a></li>
-                      <li><a href="#">Online Orders</a></li>
-                      <li><a href="#">Shop Your Way Rewards</a></li>
-                      <li><a href="#">Auto</a></li>
-                      <li><a href="#">Home Improvement</a></li>
-                      <li><a href="#">Corporate News &amp; Press</a></li>
-                      <li><a href="#">Credit</a></li>
-                      <li><a href="#">Optical, Photo &amp; Other Sears Businesses</a></li>
-                    </ul>
                   </li>
                   <li>
                     <a href="#"><span>Categories</span></a>
