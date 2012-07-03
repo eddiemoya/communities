e1b1ef9b862b8de07a382dd46c46a8b295bcd3bb
Carl Albrecht-Buehler <calbre1@searshc.com>
2012-07-03 11:02:32 -0500

    Beginning to use uihelper gem. Using a lookup function now for sears- and kmart-specific colors.

--- parts/header.php
+++ parts/header.php
@@ -42,38 +42,24 @@
                   <li class="right_button">
                     <a href="#">Customer Care</a>
                   </li>
+                  <?php
+                    $a_navigation = array(
+                      "Categories", "Q&A's", "Blog", "Buying Guides"
+                    );                  
+                  ?>
+                  <?php foreach ( $a_navigation as $nav_item ): ?>
                   <li>
-                    <a href="#"><span>Categories</span></a>
+                    <a href="#"><span><?php echo htmlentities( $nav_item ); ?></span></a>
                     <ul>
                       <li><a href="#">Item 1</a></li>
                       <li><a href="#">Item 2</a></li>
                       <li><a href="#">Item 3</a></li>
+                      <?php if ($nav_item != "Categories"): ?>
+                        <li><a href="#">All <?php echo htmlentities( $nav_item ); ?><?php if ( $nav_item == "Blog" ) { echo " Posts"; } ?></a></li>
+                      <?php endif; ?>
                     </ul>
                   </li>
-                  <li>
-                    <a href="#"><span>Q&amp;A's</span></a>
-                    <ul>
-                      <li><a href="#">Item 4</a></li>
-                      <li><a href="#">Item 5</a></li>
-                      <li><a href="#">Item 6</a></li>
-                    </ul>
-                  </li>
-                  <li>
-                    <a href="#"><span>Blog</span></a>
-                    <ul>
-                      <li><a href="#">Item 7</a></li>
-                      <li><a href="#">Item 8</a></li>
-                      <li><a href="#">Item 9</a></li>
-                    </ul>
-                  </li>
-                  <li>
-                    <a href="#"><span>Buying Guides</span></a>
-                    <ul>
-                      <li><a href="#">Item a</a></li>
-                      <li><a href="#">Item b</a></li>
-                      <li><a href="#">Item c</a></li>
-                    </ul>
-                  </li>
+                  <?php endforeach; ?>
                 </ul>
             </nav>
             
