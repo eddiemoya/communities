74fe58df8db1f08cabe50fe373f563d88ec3fa78
Tim Steele <tsteel5@searshc.com>
2012-07-03 10:56:28 -0500

    Finished Sears header styles

---
+++ parts/navigation.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * @author Carl Albrecht-Buehler 
+ */
+?>
+<nav id="navigation">
+  <ul id="header_nav" class="clearfix">
+    <li class="right_button">
+      <a href="#">Customer Care</a>
+    </li>
+    <li>
+      <a href="#"><span>Categories</span></a>
+      <ul>
+        <li><a href="#">Item 1</a></li>
+        <li><a href="#">Item 2</a></li>
+        <li><a href="#">Item 3</a></li>
+      </ul>
+    </li>
+    <li>
+      <a href="#"><span>Q&amp;A's</span></a>
+      <ul>
+        <li><a href="#">Item 4</a></li>
+        <li><a href="#">Item 5</a></li>
+        <li><a href="#">Item 6</a></li>
+      </ul>
+    </li>
+    <li>
+      <a href="#"><span>Blog</span></a>
+      <ul>
+        <li><a href="#">Item 7</a></li>
+        <li><a href="#">Item 8</a></li>
+        <li><a href="#">Item 9</a></li>
+      </ul>
+    </li>
+    <li>
+      <a href="#"><span>Buying Guides</span></a>
+      <ul>
+        <li><a href="#">Item a</a></li>
+        <li><a href="#">Item b</a></li>
+        <li><a href="#">Item c</a></li>
+      </ul>
+    </li>
+  </ul>
+</nav>
