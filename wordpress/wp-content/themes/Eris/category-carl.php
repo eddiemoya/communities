a1957b315cd6888c86ab119afe49f66c4f8a0960
Carl Albrecht-Buehler <calbre1@searshc.com>
2012-07-17 11:05:25 -0500

    Testing push ability.

---
+++ wordpress/wp-content/themes/Eris/category-carl.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * @package WordPress
+ * @subpackage White Label
+ */
+
+get_template_part('parts/header');
+
+loop();
+
+echo "Hi Carl Hi Carl Hi Carl";
+
+get_template_part('parts/footer');
