1073647ef6487b04e52f9cef4c750671a8037c18
Tim Steele <tsteel5@searshc.com>
2012-07-17 11:03:38 -0500

    Changes to category-tim.php

---
+++ category-tim.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ * @package WordPress
+ * @subpackage White Label
+ */
+
+get_template_part('parts/header');
+
+loop();
+?>
+
+	<section class="span8">
+		
+		<article class="content-container featured-question">
+			
+			<header class="content-header">
+				<h3>Featured Question</h3>
+				<h4>Featured question about automotive.</h4>
+			</header>
+	
+			<section class="content-body">
+				
+				<div class="category-image">
+					
+				</div>
+				
+				<p class="question-headline">
+					Q:
+					<a href="#" title="Lorem ipsum dolor sit amet, ut pri congue viven scaevola. Primis verear honestatis vel no, habeo prima delicata te pro. Quodsi omnesque iracundia et ius, munere viderer?">Lorem ipsum dolor sit amet, ut pri congue viven scaevola. Primis verear honestatis vel no, habeo prima delicata te pro. Quodsi omnesque iracundia et ius, munere viderer?</a>
+				</p>
+				
+				
+				<div clas="share">
+					<!-- AddThis Button BEGIN -->
+					<div class="addthis_toolbox addthis_default_style ">
+						<a class="addthis_button_facebook_like" fb:like:layout="button_count" fb:like:href="http://www.sears.com"></a>
+						<a class="addthis_button_tweet" tw:url="http://www.sears.com"></a>
+						<a class="addthis_button_email"></a>
+						<!-- <a class="addthis_counter addthis_pill_style"></a> -->
+					</div>
+					<a class="addthis_button_compact addthis_default_style" href="#">Share</a>
+					<script type="text/javascript" src="http://s7.addthis.com/js/250/addthis_widget.js#pubid=xa-5004724f074d6a45"></script>
+					<!-- AddThis Button END -->
+				</div>
+				
+			</section>
+	
+		</article>
+	
+	</section>
+
+
+	<section class="span4">
+		
+	</section>
+<?php
+get_template_part('parts/footer');
+?>
