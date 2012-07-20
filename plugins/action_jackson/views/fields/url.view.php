f4536c7477939023e1ff44db44eb7ae070a9cc74
Tim Steele <tsteel5@searshc.com>
2012-07-20 11:12:17 -0500

    Fixed deletions

--- plugins/action_jackson/views/fields/url.view.php
+++
@@ -1,6 +0,0 @@
-<tr>
-	<td>API Url (Please ensure this is in the http://www.mysite.com/ template)</td>
-	<td>
-		<input type="text" value="<?php echo $value; ?>" name="fitStudioOptions-apiUrl" />
-	</td>
-</tr>
