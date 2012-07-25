ac71a4630b1d7124130eb84c0f4a6f871ddf39ad
Carl Albrecht-Buehler <calbre1@searshc.com>
2012-07-25 10:29:01 -0500

    Beginning Profile html and css.

--- category-carl.php
+++ category-carl.php
@@ -7,43 +7,63 @@
   get_template_part('parts/header');
 
   loop();
-
+  
+  $self_lookup = true;
+  
+  $current_tab = "Community Activity";
+  $current_nav = "Recent Activity";
+  
+  $a_tabs = array(
+    "Community Activity" => "#",
+    "About Me" => "#",
+  );
+  
+  $a_navigation = array(
+    "Recent Activity" => "#",
+    "Q&A's" => "#",
+    "Blog Posts" => "#",
+    "Buying Guides" => "#"
+  );
 ?>
   
-  <section class="span8">
-    <section class="span12 content-container featured-question columns">
-      <hgroup class="content-header">
-        <h3>Featured Questions</h3>
-      </hgroup>
-      <ol class="content-body clearfix">
-<?php
-  $i = 0;
-  while ( $i < 2 ):
-?>
-        <li class="featured">
-          <section class="column clearfix">
-            <div class="span4 badge">
-              <img src="<?php echo get_template_directory_uri() ?>/assets/img/zzexpert.jpg" alt="Team Player" title="Team Player" />
-              <h4><a href="#">screenname</a></h4>
-              <address>Chicago, IL</address>
-            </div>
-            <div class="span8 info content-details">
-              <time class="content-date" datetime="<?php echo date( "Y-m-d" ); ?>" pubdate="pubdate"><?php echo date( "F n, Y g:ia" ); ?></time>
-              <h4 class="content-headline"><a href="#">Post's Title</a></h4>
-              <ul class="content-comments">
-                <li>x answers</li>
-                <li>y community answers</li>
-              </ul>
-              </div>
-            </section>
-        </li>
-<?php
-  $i++;
-  endwhile;
-?>
-      </ol>
+  <section class="span<?php echo ( $self_lookup ? "12" : "8" ); ?>">
+    <section class="profile">
+      <nav class="clearfix">
+        <ul class="tabs clearfix">
+          <?php
+          foreach ( $a_tabs as $lone_tab => $tab_url ) {
+            if ( $lone_tab == $current_tab ) {
+              echo '<li class="active">' . $lone_tab . '</li>';
+            }
+            else {
+              echo '<li><a href="' . $tab_url . '">' . $lone_tab . '</a></li>';
+            }
+          }
+          ?>
+        </ul>
+      </nav>
+      <nav class="bar clearfix">
+        <ul class="clearfix">
+          <?php
+          foreach ( $a_navigation as $lone_nav => $nav_url ) {
+            if ( $lone_nav == $current_nav ) {
+              echo '<li class="active">' . $lone_nav . '</li>';
+            }
+            else {
+              echo '<li><a href="' . $nav_url . '">' . $lone_nav . '</a></li>';
+            }
+          }
+          ?>
+        </ul>
+      </nav>
     </section>
   </section>
+  
+  <?php if ( !$self_lookup ): ?>
+  <section class="span4">
+    <!-- Widgets Go Here -->
+  </section>
+  <?php endif; ?>
 
 <?php
   get_template_part('parts/footer');
