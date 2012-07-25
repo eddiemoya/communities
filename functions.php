fba0a3c9797cb6582c60c9e1ed59a671707d01d6
Tim Steele <tsteel5@searshc.com>
2012-07-25 14:15:34 -0500

    Updates to Sears JSL

--- functions.php
+++ functions.php
@@ -107,13 +107,16 @@ function enqueue_scripts() {
         wp_deregister_script('jquery'); 
         wp_register_script('jquery', get_template_directory_uri() . '/assets/js/vendor/jquery-1.7.2.min.js', array(), '1.7.2');
 
-        /* @todo: Does modernizr not require jQuery as a dependancy? */
-		wp_register_script('modernizr', get_template_directory_uri() . '/assets/js/vendor/modernizr-2.5.3.min.js', array(), '2.5.3');
-		wp_register_script('shcJSL', get_template_directory_uri() . '/assets/js/shc-jsl.js', array(), '1.0');
+        /* @todo: Does modernizr not require jQuery as a dependancy? [TIM] No it does not. */
+				wp_register_script('modernizr', get_template_directory_uri() . '/assets/js/vendor/modernizr-2.5.3.min.js', array(), '2.5.3');
+				wp_register_script('shcJSL', get_template_directory_uri() . '/assets/js/shc-jsl.js', array(), '1.0');
+				wp_register_script('authentic8r', get_template_directory_uri() . '/assets/js/json/authentic8r.js', array(), '1.0');
+				wp_register_script('moodle', get_template_directory_uri() . '/assets/js/widgets/jquery.moodle.js', array(), '1.0');
         wp_enqueue_script('jquery');    
         wp_enqueue_script('modernizr');
         wp_enqueue_script('shcJSL');    
-				
+				wp_enqueue_script('authentic8r');
+				wp_enqueue_script('moodle');
 				
        	//wp_register_script('shcproducts', get_template_directory_uri() . '/assets/js/shcproducts.js', array('jquery'), '1.0');
         //wp_enqueue_script('shcproducts');
@@ -123,6 +126,9 @@ function enqueue_scripts() {
         wp_register_style('main-styles', get_stylesheet_uri());
         wp_enqueue_style( 'main-styles');
         
+				
+				
+				
     }
 }
 
