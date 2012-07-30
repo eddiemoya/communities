f09222d20eddf6d8052fd14123267630d603eb1b
Dan Crimmins <dbcrimmins@gmail.com>
2012-07-30 08:59:07 -0500

    Added user profile class

--- author.php
+++ author.php
@@ -1,32 +1,84 @@
 <?
+/*
+ * QueryString params:
+ * 
+ * 'part='
+ * 
+ * 1. recent - Recent Activities
+ * 2. qa = Q/A's
+ * 3. blog = Blog Posts
+ * 4. guides = Buying Guides
+ * 
+ */
 get_template_part('parts/header');
 
-//Set profile type
+$profile_user = get_userdata(get_query_var('author'));
+
+//Set profile_type
 if(is_user_logged_in()){
 	
 	//The authenticated user is viewing their own profile
-	if(get_query_var('author') == $current_user->data->ID) {
+	if($profile_user->data->ID == $current_user->data->ID) {
 		
+		$profile_type = 'myprofile';
 		
 	} else { //Authenticated user is viewing another user's profile
 		
-		echo '<pre>';
-		var_dump($current_user);
-		echo '</pre>';
+		$profile_type = (in_array('communityexpert', $profile_user->roles)) ? 'expert' : 'member';
+			
 	}
-	//Retrieves '/author/username' username's userid
-	$author = get_query_var('author');
-	echo 'Profile for user: ' . $author . '<br>';
-	
-	echo 'Current logged in user: ' . $current_user->data->ID . '<br>';
-	/*var_dump($current_user);
-	exit;*/
 	
 } else {
+	
+	$profile_type = (in_array('communityexpert', $profile_user->roles)) ? 'expert' : 'member';
+	
+}
+
+
+/*
+ * Content Section
+ */
+
+//Menu Tabs
+if($profile_type == 'expert' || $profile_type == 'member') {
+
+	
+} elseif($profile_type == 'myprofile') {
 	
 	
 }
 
-echo 'Profile Page';
+//Include template parts,based on section being requested
+if(isset($_GET['part'])) {
+	
+	switch($_GET['part']) {
+		
+		//Recent Activities
+		case 'recent':
+
+	
+		break;
+		
+		//Q&A
+		case 'qa':
+			
+		break;
+		
+		//Blog Posts
+		case 'blog':
+			
+		break;
+		
+		//Buying Guides
+		case 'guides':
+			
+		break;
+	}
+
+} else {
+	//Recent Activities
+	
+	
+}
 
 get_template_part('parts/footer');
