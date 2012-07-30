5d30ad0d000ae46891298a434c05ae9d6cf52e26
Carl Albrecht-Buehler <calbre1@searshc.com>
2012-07-30 10:18:32 -0500

    Creating test page for threaded discussion. Minor updates to recent activity styling. Adding recent activity page.

--- wordpress/wp-content/themes/Eris/category-recent_activity.php
+++ wordpress/wp-content/themes/Eris/category-recent_activity.php
@@ -22,11 +22,9 @@
   
   $a_navigation = array(
     "Recent Activity" => "#",
-    "Questions" => "#",
-    "Answers" => "#",
-    "Comments" => "#",
-    "Follows" => "#",
-    "Likes" => "#"
+    "Q&A's" => "#",
+    "Blog Posts" => "#",
+    "Buying Guides" => "#"
   );
   
   $actions = array( "ask", "answer", "comment", "follow", "like", "post" );
@@ -37,15 +35,15 @@
     "1" => array(
       "action" => "0",
       "category" => "0",
-      "title" => "How do I fix my lawnmower?",
+      "title" => "Where do babies come from?",
       "excerpt" => "Some snarky remark.",
       "date" => time(),
     ),
     "2" => array(
       "action" => "1",
       "category" => "1",
-      "title" => "What's the best Fridge?",
-      "excerpt" => "Kenmores are the best.",
+      "title" => "Has anyone seen my keys?",
+      "excerpt" => "Yes.",
       "date" => time() - ( 3 * $seconds_in_day ),
     ),
     "3" => array(
@@ -105,7 +103,42 @@
       </ul>
     </nav>
     
-    
+    <section class="span4">
+      <section class="span12 content-container recent-activity">
+        <?php if ( $is_widget ): ?>
+          <hgroup class="content-header">
+            <h3>Recent Activity</h3>
+          </hgroup>
+        <?php endif; ?>
+        <ol class="content-body clearfix">
+          <?php
+            foreach ($activities as $activity):
+              $this_action = substr( $actions[ $activity["action"] ], -1 ) == "e" ? $actions[ $activity["action"] ] . "d" : $actions[ $activity["action"] ] . "ed";
+              $excerpt = strlen( $activity["excerpt"] ) > 180 ? substr( $activity["excerpt"], 0, 180 ) . " &#8230;" : $activity["excerpt"];
+          ?>
+          <li class="clearfix">
+            <?php if ( $is_widget ): ?>
+              <div class="badge span3">
+                <img src="<?php echo get_template_directory_uri() ?>/assets/img/zzexpert.jpg" alt="Team Player" title="Team Player" />
+              </div>
+            <?php endif; ?>
+            <div<?php if ( $is_widget ) { echo ' class="span9"'; } ?>>
+              <time class="content-date" datetime="<?php echo date( "Y-m-d", $activity["date"] ); ?>" pubdate="pubdate"><?php echo date( "F j, Y g:ia", $activity["date"] ); ?></time>
+              <h3>
+                <?php if ( $is_widget ): ?><a href="#">screenname</a><?php endif; ?>
+                <span><?php echo ucfirst( $this_action ); ?></span>
+                <a href="#"><?php echo $activity["title"]; ?></a>
+                <span>in <a href="#"><?php echo $categories[ $activity["category"] ]; ?></a><?php if( in_array( $activity["action"], array( 1, 2 ) ) ) { echo ' with'; } ?></span>
+              </h3>
+              <?php if( in_array( $activity["action"], array( 1, 2, 4 ) ) ): ?>
+                <article class="excerpt"><?php echo $excerpt; ?></article>
+              <?php endif; ?>
+            </div>
+          </li>
+          <? endforeach; ?>
+        </ol>
+      </section>
+    </section>
 <?php
 
   $is_widget = false;
@@ -123,18 +156,19 @@
             $this_action = substr( $actions[ $activity["action"] ], -1 ) == "e" ? $actions[ $activity["action"] ] . "d" : $actions[ $activity["action"] ] . "ed";
             $excerpt = strlen( $activity["excerpt"] ) > 180 ? substr( $activity["excerpt"], 0, 180 ) . " &#8230;" : $activity["excerpt"];
         ?>
-        <li class="clearfix">
+        <li>
           <?php if ( $is_widget ): ?>
             <div class="badge span3">
               <img src="<?php echo get_template_directory_uri() ?>/assets/img/zzexpert.jpg" alt="Team Player" title="Team Player" />
             </div>
           <?php endif; ?>
           <div<?php if ( $is_widget ) { echo ' class="span9"'; } ?>>
+            <time class="content-date" datetime="<?php echo date( "Y-m-d", $activity["date"] ); ?>" pubdate="pubdate"><?php echo date( "F j, Y g:ia", $activity["date"] ); ?></time>
             <h3>
-              <span><?php if ( $is_widget ): ?><a href="#">screenname</a> <?php endif; ?><?php echo ucfirst( $this_action ); ?> this:</span>
-              <time class="content-date" datetime="<?php echo date( "Y-m-d", $activity["date"] ); ?>" pubdate="pubdate"><?php echo date( "F j, Y g:ia", $activity["date"] ); ?></time>
-              <a href="#" class="category"><?php echo $categories[ $activity["category"] ]; ?></a>
+              <?php if ( $is_widget ): ?><a href="#">screenname</a><?php endif; ?>
+              <span><?php echo ucfirst( $this_action ); ?></span>
               <a href="#"><?php echo $activity["title"]; ?></a>
+              <span>in <a href="#"><?php echo $categories[ $activity["category"] ]; ?></a><?php if( in_array( $activity["action"], array( 1, 2 ) ) ) { echo ' with'; } ?></span>
             </h3>
             <?php if( in_array( $activity["action"], array( 1, 2, 4 ) ) ): ?>
               <article class="excerpt"><?php echo $excerpt; ?></article>
