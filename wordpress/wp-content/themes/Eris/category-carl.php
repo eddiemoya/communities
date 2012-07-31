906595a8960389b08f849e299a1ef76731df5631
Carl Albrecht-Buehler <calbre1@searshc.com>
2012-07-31 16:58:48 -0500

    OpenID Login options mirroring sears.com

--- wordpress/wp-content/themes/Eris/category-carl.php
+++ wordpress/wp-content/themes/Eris/category-carl.php
@@ -111,6 +111,21 @@
   $is_widget = false;
 
 ?>
+  
+  <fieldset id="use_your_account_from">
+    <legend class="downcase">Use Your Account From</legend>
+      <section class="login_options">
+        <a href="#" title="Login with Facebook" class="facebook">Facebook</a>
+        <a href="#" title="Hint" class="hint">Helpful Hint</a>
+        <a href="#" title="Login with Yahoo" class="yahoo">Yahoo</a>
+        <a href="#" title="Login with Google" class="google">Google</a>
+        <a href="#" title="Login with AOL" class="aol">AOL</a>
+        <a href="#" title="Login with MySpace" class="myspace">MySpace</a>
+        <a href="#" title="Login with Twitter" class="twitter">Twitter</a>
+        <a href="#" title="Login with OpenID" class="openid">OpenID</a>
+      </section>
+  </fieldset>
+  
     <section class="span12 content-container recent-activity">
       <?php if ( $is_widget ): ?>
         <hgroup class="content-header">
