c8a965ef0fc747f649075647d6c8ff10c20d663f
Carl Albrecht-Buehler <calbre1@searshc.com>
2012-08-01 10:35:48 -0500

    11.0 Working on improvements for Recent Activity

--- wordpress/wp-content/themes/Eris/category-carl.php
+++ wordpress/wp-content/themes/Eris/category-carl.php
@@ -13,7 +13,7 @@
   $seconds_in_day = 24 * 60 * 60;
   
   $current_tab = "Community Activity";
-  $current_nav = "Recent Activity";
+  $current_nav = "1";
   
   $a_tabs = array(
     "Community Activity" => "#",
@@ -21,49 +21,75 @@
   );
   
   $a_navigation = array(
-    "Recent Activity" => "#",
-    "Questions" => "#",
-    "Answers" => "#",
-    "Comments" => "#",
-    "Follows" => "#",
-    "Likes" => "#"
+    "1" => array(
+      "type" => "Recent Activity",
+      "url" => "#"
+    ),
+    "2" => array(
+      "type" => "Questions",
+      "url" => "#"
+    ),
+    "3" => array(
+      "type" => "Answers",
+      "url" => "#"
+    ),
+    "4" => array(
+      "type" => "Comments",
+      "url" => "#"
+    ),
+    "5" => array(
+      "type" => "Follows",
+      "url" => "#"
+    ),
+    "6" => array(
+      "type" => "Helpful Votes",
+      "url" => "#"
+    ),
+    "7" => array(
+      "type" => "Posts",
+      "url" => "#"
+    ),
+    "8" => array(
+      "type" => "Buying Guides",
+      "url" => "#"
+    ),
   );
   
-  $actions = array( "ask", "answer", "comment", "follow", "like", "post" );
+  $actions = array( "", "Asked", "Answered", "Commented", "Followed", "Liked", "Wrote" );
   
   $categories = array( "Appliance", "Kitchen", "Garden", "Automotive", "Industrial Machinery" );
   
   $activities = array(
     "1" => array(
-      "action" => "0",
+      "action" => "1",
       "category" => "0",
-      "title" => "How do I fix my lawnmower?",
+      "title" => "Where do babies come from?",
       "excerpt" => "Some snarky remark.",
       "date" => time(),
     ),
     "2" => array(
-      "action" => "1",
+      "action" => "2",
       "category" => "1",
-      "title" => "What's the best Fridge?",
-      "excerpt" => "Kenmores are the best.",
+      "title" => "Has anyone seen my keys?",
+      "excerpt" => "Yes.",
       "date" => time() - ( 3 * $seconds_in_day ),
     ),
     "3" => array(
-      "action" => "2",
+      "action" => "3",
       "category" => "2",
       "title" => "Blogging is Fun",
       "excerpt" => "No, it isn't.",
       "date" => time() - ( 5 * $seconds_in_day ),
     ),
     "4" => array(
-      "action" => "3",
+      "action" => "4",
       "category" => "3",
       "title" => "Who's got warez?",
       "excerpt" => "noobz.",
       "date" => time() - ( 7 * $seconds_in_day ),
     ),
     "5" => array(
-      "action" => "4",
+      "action" => "5",
       "category" => "4",
       "title" => "Guiding the Buyer",
       "excerpt" => "I've now entered an Earthbound paradise where consumerism has saved my mortal soul. I've now entered an Earthbound paradise where consumerism has saved my mortal soul. I've now entered an Earthbound paradise where consumerism has saved my mortal soul. I've now entered an Earthbound paradise where consumerism has saved my mortal soul.",
@@ -71,7 +97,9 @@
     ),
   );
   
-  $is_widget = true;
+  $a_actions_taken = array( 1, 2, 3, 4, 5 );
+  
+  $is_widget = false;
 ?>
   
   <section class="span<?php echo ( $self_lookup ? "12" : "8" ); ?> profile">
@@ -93,39 +121,22 @@
     <nav class="bar clearfix">
       <ul class="clearfix">
         <?php
-        foreach ( $a_navigation as $lone_nav => $nav_url ) {
-          if ( $lone_nav == $current_nav ) {
-            echo '<li class="active">' . $lone_nav . '</li>';
-          }
-          else {
-            echo '<li><a href="' . $nav_url . '">' . $lone_nav . '</a></li>';
+        foreach ( $a_navigation as $lone_nav_id => $lone_nav ) {
+          if ( in_array($lone_nav_id, $a_actions_taken) ) {
+            if ( $lone_nav_id == $current_nav ) {
+              echo '<li class="active">' . $lone_nav["type"] . '</li>';
+            }
+            else {
+              echo '<li><a href="' . $lone_nav["url"] . '">' . $lone_nav["type"] . '</a></li>';
+            }
           }
         }
         ?>
       </ul>
     </nav>
     
+    <section class="span12"></section>
     
-<?php
-
-  $is_widget = false;
-
-?>
-  
-  <fieldset id="use_your_account_from">
-    <legend class="downcase">Use Your Account From</legend>
-      <section class="login_options">
-        <a href="#" title="Login with Facebook" class="facebook">Facebook</a>
-        <a href="#" title="Hint" class="hint">Helpful Hint</a>
-        <a href="#" title="Login with Yahoo" class="yahoo">Yahoo</a>
-        <a href="#" title="Login with Google" class="google">Google</a>
-        <a href="#" title="Login with AOL" class="aol">AOL</a>
-        <a href="#" title="Login with MySpace" class="myspace">MySpace</a>
-        <a href="#" title="Login with Twitter" class="twitter">Twitter</a>
-        <a href="#" title="Login with OpenID" class="openid">OpenID</a>
-      </section>
-  </fieldset>
-  
     <section class="span12 content-container recent-activity">
       <?php if ( $is_widget ): ?>
         <hgroup class="content-header">
@@ -135,7 +146,6 @@
       <ol class="content-body clearfix">
         <?php
           foreach ($activities as $activity):
-            $this_action = substr( $actions[ $activity["action"] ], -1 ) == "e" ? $actions[ $activity["action"] ] . "d" : $actions[ $activity["action"] ] . "ed";
             $excerpt = strlen( $activity["excerpt"] ) > 180 ? substr( $activity["excerpt"], 0, 180 ) . " &#8230;" : $activity["excerpt"];
         ?>
         <li class="clearfix">
@@ -146,20 +156,75 @@
           <?php endif; ?>
           <div<?php if ( $is_widget ) { echo ' class="span9"'; } ?>>
             <h3>
-              <span><?php if ( $is_widget ): ?><a href="#">screenname</a> <?php endif; ?><?php echo ucfirst( $this_action ); ?> this:</span>
+              <span>
+                <?php
+                if ( in_array( $current_nav, array( 1 ) ) ) {
+                  if ( $is_widget ) {
+                    echo '<a href="#">screenname</a>';
+                  }
+                  echo $actions[ $activity["action"] ] . ' this:';
+                }
+                ?>
+              </span>
               <time class="content-date" datetime="<?php echo date( "Y-m-d", $activity["date"] ); ?>" pubdate="pubdate"><?php echo date( "F j, Y g:ia", $activity["date"] ); ?></time>
               <a href="#" class="category"><?php echo $categories[ $activity["category"] ]; ?></a>
               <a href="#"><?php echo $activity["title"]; ?></a>
             </h3>
-            <?php if( in_array( $activity["action"], array( 1, 2, 4 ) ) ): ?>
+            <?php
+            if ( in_array( $current_nav, array( 1, 3, 4, 5, 6 ) ) ) {
+              if ( in_array( $activity["action"], array( 2, 3, 5 ) ) ) {
+            ?>
               <article class="excerpt"><?php echo $excerpt; ?></article>
-            <?php endif; ?>
+            <?php
+              }
+            }
+            ?>
           </div>
         </li>
         <? endforeach; ?>
       </ol>
     </section>
-      
+<?php
+
+  $is_widget = true;
+
+?>
+
+    <section class="span4">
+      <section class="span12 content-container recent-activity">
+        <?php if ( $is_widget ): ?>
+          <hgroup class="content-header">
+            <h3>Recent Activity</h3>
+          </hgroup>
+        <?php endif; ?>
+        <ol class="content-body clearfix">
+          <?php
+            foreach ($activities as $activity):
+              $this_action = substr( $actions[ $activity["action"] ], -1 ) == "e" ? $actions[ $activity["action"] ] . "d" : $actions[ $activity["action"] ] . "ed";
+              $excerpt = strlen( $activity["excerpt"] ) > 180 ? substr( $activity["excerpt"], 0, 180 ) . " &#8230;" : $activity["excerpt"];
+          ?>
+          <li class="clearfix">
+            <?php if ( $is_widget ): ?>
+              <div class="badge span3">
+                <img src="<?php echo get_template_directory_uri() ?>/assets/img/zzexpert.jpg" alt="Team Player" title="Team Player" />
+              </div>
+            <?php endif; ?>
+            <div<?php if ( $is_widget ) { echo ' class="span9"'; } ?>>
+              <h3>
+                <span><?php if ( $is_widget ): ?><a href="#">screenname</a> <?php endif; ?><?php echo $actions[ $activity["action"] ]; ?> this:</span>
+                <time class="content-date" datetime="<?php echo date( "Y-m-d", $activity["date"] ); ?>" pubdate="pubdate"><?php echo date( "F j, Y g:ia", $activity["date"] ); ?></time>
+                <a href="#" class="category"><?php echo $categories[ $activity["category"] ]; ?></a>
+                <a href="#"><?php echo $activity["title"]; ?></a>
+              </h3>
+              <?php if( in_array( $activity["action"], array( 1, 2, 4 ) ) ): ?>
+                <article class="excerpt"><?php echo $excerpt; ?></article>
+              <?php endif; ?>
+            </div>
+          </li>
+          <? endforeach; ?>
+        </ol>
+      </section>
+    </section>
   </section>
   
   <?php if ( !$self_lookup ): ?>
