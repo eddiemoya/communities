414312c39cd3f5051e443ada199e20251b15009b
Tim Steele <tsteel5@searshc.com>
2012-08-03 14:16:24 -0500

    Modal complete

--- assets/js/widgets/shcJSL.moodle.js
+++ assets/js/widgets/shcJSL.moodle.js
@@ -27,6 +27,7 @@ MOODLE.modal = $Moodle = function(element, options) {
 	 *
 	 * @var getPosition (Function) top and left position of an element
 	 * @var centerModal (Function) Centers the modal window 
+	 * @var escapeModal (Function) Use the escape key to close the modal
 	 * @var gears (object) The parts that make up the modal window
 	 * @var self (Object) The main modal window object
 	 * @var toggleLoading (Function) Toggles the modal loading screen
@@ -34,6 +35,7 @@ MOODLE.modal = $Moodle = function(element, options) {
 	 */
 	var centerModal;
 	var getPosition;
+	var escapeModal;
 	var gears = {};
 	var self 	= this;
 	var toggleLoading;
@@ -76,6 +78,21 @@ MOODLE.modal = $Moodle = function(element, options) {
 	/**
 	 * PRIVATE METHODS
 	 */
+	
+	/**
+	 * escapeModal
+	 * 
+	 * Closes the modal window when the escape key is pressed.
+	 * 
+	 * @access Private
+	 * @author Tim Steele
+	 * @since 1.0
+	 */
+	
+	escapeModal = function(event) {
+		event.preventDefault();
+		if (event.keyCode == 27) self.destroy(event);
+	}
 	
 	/**
 	 * getPosition
@@ -206,7 +223,7 @@ MOODLE.modal = $Moodle = function(element, options) {
 			// Event for triggering Gizmos on new content.
 			$(gears.modal).bind('moodle-update', shcJSL.gizmos.activate);
 			// Trigger the moodle create event
-			$(gears.modal).trigger('moodle-create');
+			$(window).trigger('moodle-create');
 			// Send to update to build out modal
 			self.update(element, options);
 		}
@@ -292,20 +309,18 @@ MOODLE.modal = $Moodle = function(element, options) {
 		}
 		
 		if (String(settings.method).toLowerCase() === 'local') {
-			if (document.getElementById(settings.target)) {
+			try {
 				compose(document.getElementById(settings.target));
-			}
-			else {
-				console.log("fail");
+			} catch(e) {
+				console.log(e);
 			}
 		}
 		
 		function compose(content, data) {
-			console.log(settings);
 			if (settings.width && settings.width != 'auto') if (!(isNaN(settings.width))) $(content).css('width', settings.width);
 			if (settings.height && settings.height != 'auto') if (!(isNaN(settings.height))) $(content).css('height', settings.width);
 			
-			$(document.body).append($(content).toggleClass('moodle_transit').css('opacity','0'));
+			$(document.body).append($(content).show().toggleClass('moodle_transit').css('opacity','0'));
 			window.scrollTo(0,0);
 			
 			$(gears.modal).animate({
@@ -319,11 +334,13 @@ MOODLE.modal = $Moodle = function(element, options) {
 					$(gears.container).append(content);
 					
 					// SET UP CLOSE BUTTON
-					$(gears.container).append(shcJSL.createNewElement("a","close-button",{href:'#'}))
-					if (settings.clickOverlayToClose) $(gears.overlay).bind('click',self.destroy);
-					$(document).keyup(function(e){if (e.keyCode == 27) self.destroy();})
+					$(gears.container).append($(shcJSL.createNewElement("a","close-button",{href:'#'})).bind('click', {data:settings}, self.destroy))
+					if (settings.clickOverlayToClose) $(gears.overlay).bind('click', {data:settings},self.destroy);
+					else $(gears.overlay).unbind('click',self.destroy);
 					
-					$(gears.modal).trigger('moodle-update', gears.modal);
+					$(document).bind('keyup', {data:settings}, escapeModal)
+					
+					$(window).trigger('moodle-update');
 					
 					$(content).toggleClass("moodle_transit").animate({
 						opacity:100
@@ -346,8 +363,22 @@ MOODLE.modal = $Moodle = function(element, options) {
 	 * @since 1.0 
 	 */
 	
-	this.destroy = function() {
+	this.destroy = function(event) {
+		settings = event.data.data;
 		
+		if (String(settings.method).toLowerCase() == 'local') {
+			try {$(document.body).append($("#"+settings.target).hide())} 
+			catch(error) {}
+		}
+		
+		toggleLoading();
+				
+		$(gears.modal).remove();
+		$(window).unbind("resize", centerModal);
+		$(document).unbind('keyup',escapeModal);
+		$(window).trigger("moodle-close");
+		toggleLoading();
+		toggleOverlay();
 	}
 	
 	/**
@@ -386,187 +417,7 @@ shcJSL.methods.moodle = function(target, options) {
 	}
 	
 	($Moodle instanceof MOODLE.modal)? $Moodle[method](target, this):($Moodle = new $Moodle())[method](target,this)
-		
-	// if (method === ("create" || "destroy")) {
-		// eval(capture)();
-	// }
-	
-	return;
-	
-	//var options;	// (Object) Settings for the moodle widget.
-	var assess;
-	var center;
-  var defaults; // Default settings for the modal window
-  var modal; // Modal window element
-  var overlay; // Overlay element
-  var settings; // Configurations for the modal
-  var self = this;    
-
-	defaults	= {
-		clickOverlayToClose: true,
-		closeButton: true,
-		height: 'auto',
-		width: 'auto'
-	};
-	
-	assess = function(element) {
-		if (!element) element = modal;
-		return {
-			left: ($(window).width() - $(element).outerWidth())/2,
-			top:	($(window).height() - $(element).outerHeight())/2
-		};
-	}
-		
-	center = function() {
-		var styles; // Object for CSS key value pairs
-		
-		styles = {};
-		if (assess().left < 0) {
-			// if($(modal).css("width")) {
-				// if ($(modal).data("width") == 'undefined' || $(modal).data("width") == undefined) $(modal).data("width",width);
-			// }
-			styles.left = 0;
-			styles.width = $(window).width() - 24;
-			styles.height = "auto";
-		}
-		else if (assess().left > 0) {
-			styles.left = assess().left;
-			if (($(modal).data("width") != 'undefined') && ($(modal).data("width") < $(modal).outerWidth())) styles.width = $(modal).data("width").width;
-		}
-		(assess().top < 0)? styles.top = 10:styles.top = assess().top;
-		$(modal).css(styles);
-		return modal;
-	}
-
-	/*
-			[OVERLAY]
-	*/
-	this.overlay = {
-		on	: function() {$('body').append(overlay); return self.overlay;},
-		off	: function() { $(overlay).remove(); return self.overlay;}
-	};
-	
-	this.loading = function() {
-		// CHANGE TO USE ATTACH/DETACH
-		if (($(container).children()).length == 0) {
-			$(container).append("<section class='loading'></section>");
-		} else {
-			$(container).children().detach()
-			$(container).append("<section class='loading'></section>");
-		}
-		//$(container).html("<section class='loading'></section>");
-		return self;
-	}
-	
-	/*
-			[PUBLIC METHODS]
-	*/
-	this.open =	function(options) {
-		if ($(modal).parents().is($('body').get(0))) {
-			$(window).trigger("resize");
-			if (options) settings = $.extend({},defaults,options);
-		} else {
-			self.overlay.on();
-			$('body').append(modal.hide()); $(center()).show();
-			if (options) settings = $.extend({},defaults,options);
-			//$(window).bind("resize", center);
-			
-			// Apply event for firing when content is loaded
-			$(modal).bind('update', $b.activate)
-			$(modal).bind('update', $w.activate)
-			// if (mobile_client && mobile_client == true) {
-				// $(modal).bind('update', function() {
-					// $.each($(modal).find('input[type="text"]'), function() {
-// 						
-						// $(this).bind('blur', function() {
-							// $(window).bind('resize',center);
-						// })
-						// $(this).bind('focus', function() {
-							// $(window).unbind('resize',center);
-						// })
-// 						
-					// }) // END each
-				// }) // END bind update
-			// } // END if mobile_client	
-		}
-	};
-	
-	this.update = function(custom) {
-		var thread; // The current thread/window to use.
-		
-		if (custom && custom.modal && custom.modal.complete && (typeof custom.modal.complete == 'function')) $(modal).bind('update', custom.modal.complete);
-		
-		if (custom && custom.thread) thread = custom.thread;
-		else thread = $wf.threads[$wf.threads.length - 1][$wf.threads[$wf.threads.length-1].length - 1];
-		
-		if (custom && custom.refresh) {
-			extras = {};
-			extras.thread = thread;
-			$.ajax({
-				dataType: 'html',
-				statusCode: {
-					200: function(data, text, xhr) {
-					$xhr[200](data, text, xhr, extras);
-					}, // END StatusCode -> 200
-					404: function(data, text, xhr) {
-						$xhr[404](data, text, xhr, extras);
-					}, // END StatusCode -> 404
-					500: function(data, text, xhr) {
-						$xhr[500](data, text, xhr, extras);
-					} // END StatusCode -> 500
-				},
-				url: thread.url
-			})
-			return;
-		}
-						
-		$($('body').get(0)).append($($$.preloadImages(thread.html)).toggleClass('transit').css("opacity","0"));
-		window.scrollTo(0,0)
-		$(modal).animate({ 
-			// Animate style parameters
-				height:$(thread.html).outerHeight(),
-				left:(assess(thread.html).left > 0)? assess(thread.html).left -12 : 12,
-				top:(assess(thread.html).top > 0)? assess(thread.html).top - 12 : 12,
-				width:$(thread.html).outerWidth()
-			},{
-				complete: function() {
-					$(container).children().detach();
-					$(container).append(thread.html);
-					
-					// Settings updater
-					// if (opts && opts.settings) {};
-					// if (settings.clickToClose == true) $(background).bind('click', self.close);
-					if (settings.xButton !== false) {
-					  $(container).find(".content-display").append($("<a href='#' class='close-button'>x</a>").bind('click', function(event) {Modal.close(); event.preventDefault();}))
-					}
-					$(modal).trigger('update', modal);
-					
-					$(thread.html).toggleClass("transit").animate({
-						opacity:100
-					},{
-						complete:function() {
-							$(modal).css("height","auto");
-						},
-						duration:500
-					});
-				},
-				duration:500
-			}
-		)
-	};
-	
-	this.close = function() {
-		Modal.loading();
-		$($('body').get(0)).trigger("modal:close");
-		$wf.kill()
-		$(modal).remove();
-		self.overlay.off();
-		$(window).unbind("resize", center);
-	};
-	
-	return this;
-	
-};
+}
 
 shcJSL.gizmos.bulletin['shcJSL.moodle.js'] = true;
 
