ab31ea3aadbb9b776b85bf2dea01866bc34884d7
Eddie Moya <eddie.moya@gmail.com>
2012-08-02 19:08:14 -0500

    settings up new and better structire for each featured posts item

--- widgets/featured-post/single-question.php
+++ widgets/featured-post/single-question.php
@@ -1,65 +1,68 @@
-<?php
-if (have_posts()) { while (have_posts()) { the_post(); ?>
-
-<!--/Users/emoya1/Public/Projects/comm/wordpress/wp-content/themes/Eris/widgets/parts/category.php -->
 <article class="content-container featured-question">
     <?php if(function_exists('is_widget')) : ?>
-    <?php if (is_widget()->show_title && !empty(is_widget()->widget_title)) : ?>
+        <?php if (is_widget()->show_title && !empty(is_widget()->widget_title)) : ?>
 
-        <header class="content-header">
-            <h3><?php echo is_widget()->widget_title; ?></h3>
-            <?php if(is_widget()->show_subtitle) : ?>
-                <h4><?php echo is_widget()->widget_subtitle; ?></h4>
-            <?php endif; ?>
-        </header>
-
-    <?php endif; ?>
-
-    <section class="content-body clearfix">
-
-        <?php if (is_widget()->show_category || is_widget()->show_date) : ?>
-            <div class="content-details clearfix">
-
-                <?php if (is_widget()->show_category) : ?>
-                    <span class="content-category">
-                        <a href="#" title="Kittens">
-                            <?php 
-                                $c = get_the_category(); 
-                                echo $c[0]->cat_name; 
-                            ?>
-                        </a>
-                    </span>
+            <header class="content-header">
+                <h3><?php echo is_widget()->widget_title; ?></h3>
+                <?php if(is_widget()->show_subtitle) : ?>
+                    <h4><?php echo is_widget()->widget_subtitle; ?></h4>
                 <?php endif; ?>
+            </header>
 
-                <time class="content-date" datetime="<?php the_time('Y-m-d'); ?>">
-                    <?php the_time('F j, Y'); ?>
-                </time>
-
-            </div>
         <?php endif; ?>
 
+        <section class="content-body clearfix">
 
 
-        <div class="category-image">
-            <?php //get_category_image_url($c[0]->term_id, true, true); ?>
-        </div>
+            <?php if (have_posts()) { while (have_posts()) { the_post(); ?>
 
-        <h6 class="content-headline">
-            <a href="<?php the_permalink(); ?>">
-                Q:
-                <?php the_title(); ?>
-            </a>
-        </h6>
-
-        <ul class="content-comments">
-            <li><?php comments_number(); ?></li>
-        </ul>
+                <?php if (is_widget()->show_category || is_widget()->show_date) : ?>
 
 
-        </div>
+                    <div class="content-details clearfix">
 
-    </section>
-<?php endif; ?>
+                        <?php if (is_widget()->show_category) : ?>
+                            <span class="content-category">
+                                <a href="#" title="Kittens">
+                                    <?php 
+                                        $c = get_the_category(); 
+                                        echo $c[0]->cat_name; 
+                                    ?>
+                                </a>
+                            </span>
+                        <?php endif; //show category; ?>
+
+
+                        <time class="content-date" datetime="<?php the_time('Y-m-d'); ?>">
+                            <?php the_time('F j, Y'); ?>
+                        </time>
+
+                    </div>
+                <?php endif; //show category or show date ?>
+
+
+
+                <div class="category-image">
+                    <?php //get_category_image_url($c[0]->term_id, true, true); ?>
+                </div>
+
+
+
+                <h6 class="content-headline">
+                    <a href="<?php the_permalink(); ?>">
+                        Q:
+                        <?php the_title(); ?>
+                    </a>
+                </h6>
+
+
+                <ul class="content-comments">
+                    <li><?php comments_number(); ?></li>
+                </ul>
+
+            <?php }}  // end loop ?>
+
+
+        </section><!-- content-body -->
+    <?php endif; //if function exists is_widget ?>
 </article> 
-<?php
-}}
