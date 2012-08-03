acfce6986edb0f693afb6252eec1a325f7590074
Eddie Moya <eddie.moya@gmail.com>
2012-08-02 19:10:21 -0500

    adding rewrite rules to questions

--- functions/post-types.php
+++ functions/post-types.php
@@ -11,35 +11,42 @@ add_action('init', 'register_questions_type');
  */
 function register_questions_type() {
     $labels = array(
-        'name' => _x('Questions', 'post type general name'),
+        'name'          => _x('Questions', 'post type general name'),
         'singular_name' => _x('Questions', 'post type singular name'),
-        'add_new' => _x('Add New', 'question'),
-        'add_new_item' => __('Add New Question'),
-        'edit_item' => __('Edit Question'),
-        'new_item' => __('New Question'),
-        'all_items' => __('All Questions'),
-        'view_item' => __('View Question'),
-        'search_items' => __('Search Questions'),
-        'not_found' => __('No questions found'),
+        'add_new'       => _x('Add New', 'question'),
+        'add_new_item'  => __('Add New Question'),
+        'edit_item'     => __('Edit Question'),
+        'new_item'      => __('New Question'),
+        'all_items'     => __('All Questions'),
+        'view_item'     => __('View Question'),
+        'search_items'  => __('Search Questions'),
+        'not_found'     => __('No questions found'),
         'not_found_in_trash' => __('No questions found in Trash'),
         'parent_item_colon' => '',
-        'menu_name' => 'Questions'
+        'menu_name'     => 'Questions'
+    );
+    $rewrite = array(
+        'slug'          => 'question',
+        'with_front'    => true,
+        'feeds'         => true,
+        'paged'         => true,
+
     );
     $args = array(
-        'labels' => $labels,
-        'public' => true,
+        'labels'        => $labels,
+        'public'        => true,
         'publicly_queryable' => true,
-        'show_ui' => true,
-        'show_in_menu' => true,
-        'query_var' => false,
-        'rewrite' => false,
+        'show_ui'       => true,
+        'show_in_menu'  => true,
+        'query_var'     => false,
+        'rewrite'       => $rewrite,
         'capability_type' => 'post',
-        'has_archive' => false,
-        'hierarchical' => false,
+        'has_archive'   => false,
+        'hierarchical'  => false,
         'menu_position' => 8,
-        'supports' => array('title', 'editor', 'author', 'comments'),
-        'menu_icon' => get_template_directory_uri() . '/assets/img/admin/questions_admin_icon.gif',
-        'taxonomies' => array('category', 'post_tag')
+        'supports'      => array('title', 'editor', 'author', 'comments'),
+        'menu_icon'     => get_template_directory_uri() . '/assets/img/admin/questions_admin_icon.gif',
+        'taxonomies'    => array('category', 'post_tag')
     );
     register_post_type('question', $args);
 }
