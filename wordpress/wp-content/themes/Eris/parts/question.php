386528428753cda3353feac2fe97739c06444190
Sebastian Frohm <sebastian.frohm@gmail.com>
2012-08-06 11:49:25 -0500

    Fixing single.php

--- wordpress/wp-content/themes/Eris/parts/question.php
+++ wordpress/wp-content/themes/Eris/parts/question.php
@@ -30,10 +30,8 @@
         </div>
         <div class="comments">
             <?php
-//                comments('/parts/answers.php');
-
-                get_template_part('../parts/comments.php');
-                //include('/Users/dasfisch/communities/wordpress/wp-content/themes/Eris/parts/comments.php');
+                comments_template('/parts/commentForm.php');
+                comments_template('/parts/comments.php');
             ?>
         </div>
     </div>
