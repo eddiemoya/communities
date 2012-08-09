b1a989043f42682cd4fa879b87de5b77ba065ab6
Dan Crimmins <dbcrimmins@gmail.com>
2012-08-09 15:37:02 -0500

    changes to oid

--- page-forgot-password.php
+++ page-forgot-password.php
@@ -8,27 +8,69 @@
  * @subpackage White Label
  */
 
+//Form handler
+if(! empty($_POST)) {
+	
+	//Only proceed if SSO plugin is active
+	if(class_exists('SSO_Profile')) {
+	
+		$profile = new SSO_Profile;
+	
+		//Forgot password request
+		if(isset($_POST['login_email'])) {
+			
+			$response = $profile->reset_password($_POST['login_email']);
+			
+		}
+		
+		//Enter new password 
+		if(isset($_POST['new_password']) && isset($_POST['auth_token'])) {
+			
+			$response = $profile->authorize_reset($_POST['new_password'], $_POST['auth_token']);
+			
+			var_dump($response);
+		}
+	
+	}
+	
+	/**
+	 * $response is an array with the response message. 
+	 * Access the message via $response['message']
+	 */
+}
+
 get_template_part('parts/header'); ?>
 	<section class="span8">
-		
+		<?php echo $response['message'];?>
 		<article class="content-container post-your-question span12">
 			<section class="content-body clearfix">
 				
-				<h6 class="content-headline">Forgot Password</h6>
+				<h6 class="content-headline"><?php echo (isset($_GET['auth_token'])) ? 'Enter New Password' : 'Forgot Password'?></h6>
 				<p>
-        	Please verify the email address this account and press continue.
+        	<?php echo (isset($_GET['auth_token'])) ? 'Enter a new password for your account below.' : 'Please verify the email address this account and press continue.';?>
         </p>
 				
-				<form class="form_login">
+				<form class="form_login" method="post">
 
           <ul class="form-fields">
-              
+              <?php if(! isset($_GET['auth_token'])):?>
               <li>
                   <dl class="clearfix">
                       <dt class="span3"><label for="login_email">Email:</label></dt>
                       <dd class="span9"><input type="text" name="login_email" class="input_text" id="login_email" /></dd>
                   </dl>
               </li>
+              <?php else:?>
+              <li>
+                  <dl class="clearfix">
+                      <dt class="span3"><label for="login_email">New Password:</label></dt>
+                      <dd class="span9"><input type="text" name="new_password" class="input_text" id="new_password" /></dd>
+                  </dl>
+              </li>
+              
+               <input type="hidden" name="auth_token" value="<?php echo $_GET['auth_token'];?>" />
+               
+              <?php endif;?>
               <li class="clearfix">
                   <dl>
                       <dd class="span3">&nbsp;</dd>
