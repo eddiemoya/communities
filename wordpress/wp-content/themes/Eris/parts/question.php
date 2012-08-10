ac117cd0b54db0d50887f0d4051d2bdc75387750
Carl Albrecht-Buehler <calbre1@searshc.com>
2012-08-10 14:59:23 -0500

    Added new partial-calling function.

--- wordpress/wp-content/themes/Eris/parts/question.php
+++ wordpress/wp-content/themes/Eris/parts/question.php
@@ -18,21 +18,7 @@
         <form class="actions clearfix" method="post" action="">
             <button type="button" name="button1" value="flag" id="flag-comment-<?php echo $comment->comment_ID; ?>" class="flag">Flag</button>
             <button type="button" name="button1" value="Follow" id="flag-comment-<?php echo $comment->comment_ID; ?>" class="follow">Follow</button>
-            <script type="text/javascript" src="http://s7.addthis.com/js/250/addthis_widget.js"></script>
-
-            <div class="addthis_toolbox">
-                <div class="share_menu_links_text">
-                    <span class="share_button">Share</span>
-                </div>
-                <div class="share_menu_links">
-                    <a class="addthis_button_email">Email</a>
-                    <a class="addthis_button_twitter">Twitter</a>
-                    <a class="addthis_button_facebook">Facebook</a>
-                    <a class="addthis_button_shopyourway" addthis:url="http://www.sears.com" addthis:title="ShopYourWay this"><img src="<?php echo get_template_directory_uri() ?>/assets/img/shopyourway_small.jpg" />ShopYourWay</a>
-                </div>
-            </div>
-
-            <script type="text/javascript" src="<?php echo get_template_directory_uri() ?>/assets/js/addthis.js"></script>
+            <?php get_partial( 'parts/share' ); ?>
         </form>
     </div>
     
---
+++ wordpress/wp-content/themes/Eris/parts/share.php
@@ -0,0 +1,22 @@
+<script type="text/javascript" src="http://s7.addthis.com/js/250/addthis_widget.js"></script>
+<?php if ( $version == 'long' ): ?>
+<div class="addthis_toolbox addthis_default_style">
+    <a class="addthis_button_facebook_like"></a>
+    <a class="addthis_button_tweet"></a>
+    <a class="addthis_button_email"><img src="<?php echo get_template_directory_uri() ?>/assets/img/email.png" /></a>
+    <a class="addthis_button_www.shopyourway.com" addthis:url="http://www.sears.com" addthis:title="ShopYourWay this"><img src="<?php echo get_template_directory_uri() ?>/assets/img/shopyourway_large.png" /></a>
+</div>
+<?php else: ?>
+<div class="addthis_toolbox">
+    <div class="share_menu_links_text">
+        <span class="share_button">Share</span>
+    </div>
+    <div class="share_menu_links">
+        <a class="addthis_button_email">Email</a>
+        <a class="addthis_button_twitter">Twitter</a>
+        <a class="addthis_button_facebook">Facebook</a>
+        <a class="addthis_button_shopyourway" addthis:url="http://www.sears.com" addthis:title="ShopYourWay this"><img src="<?php echo get_template_directory_uri() ?>/assets/img/shopyourway_small.jpg" />ShopYourWay</a>
+    </div>
+</div>
+<?php endif; ?>
+<script type="text/javascript" src="<?php echo get_template_directory_uri() ?>/assets/js/addthis.js"></script>
