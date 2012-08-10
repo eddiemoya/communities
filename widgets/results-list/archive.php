638b87f5ad74b8bf0090780f35c93b68720a76c4
Eddie Moya <eddie.moya@gmail.com>
2012-08-10 18:21:04 -0500

    making the category dropdowns function dynamically when categories with children are picked

--- widgets/results-list/archive.php
+++ widgets/results-list/archive.php
@@ -1,20 +1,31 @@
-      <header class="content-header">
-        <form method="post" action="<?php echo (isset($_SERVER["HTTPS"]) && $_SERVER["HTTPS"]==”on”) ? "https://" : "http://" . $_SERVER["SERVER_NAME"] . $_SERVER['REQUEST_URI']; ?>">
-          <label for="sort-results">Sort By</label>
-          <select id="sort-results">
-            <option value="">Select a category</option>
-            <option value="1">Cat 1</option>
-            <option value="2">Cat 2</option>
-          </select>
-          <input type="submit" value="submit" name="submit" />
-        </form>
-      </header>
-      <ol class="content-body">
 
-    
-      		<?php loop_by_type('results-list'); ?>
-     
+<header class="content-header">
+	<form method="post" action="<?php echo (isset($_SERVER["HTTPS"]) && $_SERVER["HTTPS"]==”on”) ? "https://" : "http://" . $_SERVER["SERVER_NAME"] . $_SERVER['REQUEST_URI']; ?>">
+		<label for="sort-results">Sort By</label>
+			<?php 
+			wp_dropdown_categories(array(
+				'depth'=> 1,
+				'selected' => get_queried_object()->term_id,
+				'hierarchical' => true,
+				'hide_if_empty' => true,
+				'class' => 'input_select',
+				'name' => 'category',
+				'id' => 'sort-results'
+			));
+			wp_dropdown_categories(array(
+				'depth'=> 1,
+				'child_of' => get_queried_object()->term_id,
+				'hierarchical' => true,
+				'hide_if_empty' => true,
+				'class' => 'input_select',
+				'name' => 'sub-category',
+				'id' => 'sub-category'
+			));
+									?>
+		<input type="submit" value="submit" name="submit" />
+	</form>
+</header>
+<ol class="content-body">
+	<?php loop_by_type('results-list'); ?>
+</ol>
 
-
-      </ol>
-    </section>
