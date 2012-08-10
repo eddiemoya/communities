404c9b5f1649f333542c4ca29b30835546455db7
Carl Albrecht-Buehler <calbre1@searshc.com>
2012-08-10 14:11:44 -0500

    Added buttons to questions. A bit of refactoring to ensure better fallback states for non-JS browsers.

--- parts/question.php
+++ parts/question.php
@@ -15,11 +15,25 @@
         <time datetime="<?php echo date( "Y-m-d" ); ?>" pubdate="pubdate"><?php echo the_date(); ?></time>
         <h2><?php the_title(); ?></h2>
         <?php the_content(); ?>
-        <ul class="actions">
-            <li>Follow</li>
-            <li>Share</li>
-            <li>Flag</li>
-        </ul>
+        <form class="actions clearfix" method="post" action="">
+            <button type="button" name="button1" value="flag" id="flag-comment-<?php echo $comment->comment_ID; ?>" class="flag">Flag</button>
+            <button type="button" name="button1" value="Follow" id="flag-comment-<?php echo $comment->comment_ID; ?>" class="follow">Follow</button>
+            <script type="text/javascript" src="http://s7.addthis.com/js/250/addthis_widget.js"></script>
+
+            <div class="addthis_toolbox">
+                <div class="share_menu_links_text">
+                    <span class="share_button">Share</span>
+                </div>
+                <div class="share_menu_links">
+                    <a class="addthis_button_email">Email</a>
+                    <a class="addthis_button_twitter">Twitter</a>
+                    <a class="addthis_button_facebook">Facebook</a>
+                    <a class="addthis_button_shopyourway" addthis:url="http://www.sears.com" addthis:title="ShopYourWay this"><img src="<?php echo get_template_directory_uri() ?>/assets/img/shopyourway_small.jpg" />ShopYourWay</a>
+                </div>
+            </div>
+
+            <script type="text/javascript" src="<?php echo get_template_directory_uri() ?>/assets/js/addthis.js"></script>
+        </form>
     </div>
     
     <?php
