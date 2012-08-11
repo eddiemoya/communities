3f3e97bea0d104012dd629c861a05360bc747bc1
Eddie Moya <eddie.moya@gmail.com>
2012-08-10 20:57:50 -0500

    making the dropdown form actually work, show child terms, and properly filter the contents of the widget - and only the contents of the widget - all without adding any additional database queries aside from the 2 needed

--- widgets/results-list/archive.php
+++ widgets/results-list/archive.php
@@ -1,28 +1,52 @@
 
 <header class="content-header">
-	<form method="post" action="<?php echo (isset($_SERVER["HTTPS"]) && $_SERVER["HTTPS"]==”on”) ? "https://" : "http://" . $_SERVER["SERVER_NAME"] . $_SERVER['REQUEST_URI']; ?>">
+	<form method="post" action="">
 		<label for="sort-results">Sort By</label>
 			<?php 
+			// Get the normal queried category term id.
+			$category = get_queried_object()->term_id;
+			$subcategory = null;
+
+			// Check if either category or sub-category have been passed as get or post variables.
+			if( isset($_REQUEST['filter-category']) || isset($_REQUEST['filter-sub-category']) ) {
+				$category = absint($_REQUEST['filter-category']);
+				$subcategory = absint($_REQUEST['filter-sub-category']);
+
+			//If not..
+			} else {
+				//... get the current term object (as opposed to just the ID above)...
+				$term_object = get_the_category();
+				//...and see if its parent is empty.
+				if( !empty($term_object[0]->parent) ){
+
+					//... If its not empty, then were in a subcategory, and we need to set the two vars accordingly.
+					$subcategory = $term_object[0]->term_id;
+					$category = $term_object[0]->parent;
+				}
+			}
+
 			wp_dropdown_categories(array(
 				'depth'=> 1,
-				'selected' => get_queried_object()->term_id,
+				'selected' => $category,
 				'hierarchical' => true,
 				'hide_if_empty' => true,
 				'class' => 'input_select',
-				'name' => 'category',
+				'name' => 'filter-category',
 				'id' => 'sort-results'
 			));
 			wp_dropdown_categories(array(
 				'depth'=> 1,
-				'child_of' => get_queried_object()->term_id,
+				'selected' => $subcategory,
+				'child_of' => $category,
 				'hierarchical' => true,
 				'hide_if_empty' => true,
-				'class' => 'input_select',
-				'name' => 'sub-category',
+				'class' => '',
+				'name' => 'filter-sub-category',
 				'id' => 'sub-category'
 			));
-									?>
-		<input type="submit" value="submit" name="submit" />
+			?>
+		<input type="hidden" value="results-list" name="widget" />
+		<input type="submit" value="Submit" name="submit" />
 	</form>
 </header>
 <ol class="content-body">
