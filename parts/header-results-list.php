7d2d10ca4fffd3a71ea88a8c9132a872724a9f0c
Eddie Moya <eddie.moya@gmail.com>
2012-08-10 22:09:26 -0500

    refactoring the results widget, moved the header out to its own partial to simplify the widget template

---
+++ parts/header-results-list.php
@@ -0,0 +1,51 @@
+<header class="content-header">
+	<form method="post" action="">
+		<label for="sort-results">Sort By</label>
+			<?php 
+			// Get the normal queried category term id.
+			$category = get_queried_object()->term_id;
+			$subcategory = null;
+
+			// Check if either category or sub-category have been passed as get or post variables.
+			if( isset($_REQUEST['filter-category']) || isset($_REQUEST['filter-sub-category']) ) {
+				$category = absint($_REQUEST['filter-category']);
+				$subcategory = absint($_REQUEST['filter-sub-category']);
+
+			//If not..
+			} else {
+				//... get the current term object (as opposed to just the ID above)...
+				$term_object = get_the_category();
+				//...and see if its parent is empty.
+				if( !empty($term_object[0]->parent) ){
+
+					//... If its not empty, then were in a subcategory, and we need to set the two vars accordingly.
+					$subcategory = $term_object[0]->term_id;
+					$category = $term_object[0]->parent;
+				}
+			}
+
+			wp_dropdown_categories(array(
+				'depth'=> 1,
+				'selected' => $category,
+				'hierarchical' => true,
+				'hide_if_empty' => true,
+				'class' => 'input_select',
+				'name' => 'filter-category',
+				'id' => 'sort-results'
+			));
+			wp_dropdown_categories(array(
+				'depth'=> 1,
+				'selected' => $subcategory,
+				'child_of' => $category,
+				'hierarchical' => true,
+				'hide_if_empty' => true,
+				'class' => '',
+				'name' => 'filter-sub-category',
+				'id' => 'sub-category'
+			));
+			?>
+		<input type="hidden" value="results-list" name="widget" />
+		<input type="submit" value="Submit" name="submit" />
+	</form>
+</header>
+<ol class="content-body">
