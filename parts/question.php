55bfc73806ffa7f7901d9e3a5cbdc00d94fb5e5f
Carl Albrecht-Buehler <calbre1@searshc.com>
2012-08-13 08:37:45 -0500

    Made badge into partial with options. Added some html and css polish to comments and question.

--- parts/question.php
+++ parts/question.php
@@ -1,23 +1,22 @@
 <?php
     $i = 0;
     $categories = get_the_category( $post->ID );
+    $badge_options = array(
+        "user_id" => $post->post_author
+    );
 ?>
 <article class="post-n-comments">
     <header class="section-header">
         Question
     </header>
-    <div class="span2 badge">
-        <img src="<?php echo get_template_directory_uri() ?>/assets/img/zzexpert.jpg" alt="Team Player" title="Team Player" />
-        <h4><a href="#"><?php the_author_link(); ?></a></h4>
-        <address>Chicago, IL</address>
-    </div>
+    <?php get_partial( 'parts/badge', $badge_options ); ?>
     <div class="span10 info content-details">
         <time datetime="<?php echo date( "Y-m-d" ); ?>" pubdate="pubdate"><?php echo the_date(); ?></time>
         <h2><?php the_title(); ?></h2>
         <?php the_content(); ?>
         <form class="actions clearfix" method="post" action="">
-            <button type="button" name="button1" value="flag" id="flag-comment-<?php echo $comment->comment_ID; ?>" class="flag">Flag</button>
-            <button type="button" name="button1" value="Follow" id="flag-comment-<?php echo $comment->comment_ID; ?>" class="follow">Follow</button>
+            <button type="button" name="button1" value="flag" title="Flag this question" id="flag-comment-<?php echo $comment->comment_ID; ?>" class="flag">flag</button>
+            <button type="button" name="button1" value="follow" title="Follow this question" id="follow-comment-<?php echo $comment->comment_ID; ?>" class="follow">follow</button>
             <?php get_partial( 'parts/share' ); ?>
         </form>
     </div>
