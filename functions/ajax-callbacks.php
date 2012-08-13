748cb450b9dd007115b37c86348dcb422afa237c
Eddie Moya <eddie.moya@gmail.com>
2012-08-13 11:18:44 -0500

    moved profile related ajax callbacks to the ajax-callbacks.php file, and the is_ajax() function to template tags

--- functions/ajax-callbacks.php
+++ functions/ajax-callbacks.php
@@ -69,3 +69,75 @@ function get_posts_ajax(){
 
 add_action('wp_ajax_nopriv_get_posts_ajax', 'get_posts_ajax');
 add_action('wp_ajax_get_posts_ajax', 'get_posts_ajax');
+
+
+/**
+ * @author Dan Crimmins
+ * 
+ * Handles 'More' ajax pagination requests
+ * 
+ * @returns string - HTML output
+ */
+function profile_paginate() {
+     
+    $uid = $_POST['uid'];
+    $type = $_POST['type'];
+    $page = $_POST['page'];
+    
+    
+    require_once get_template_directory() . '/classes/communities_profile.php';
+    
+    //User Profile object
+    $user_activities = new User_Profile($uid);
+    
+    ob_start();
+    
+        //Comments
+        if($type == 'answer' || $type == 'comment') {
+            
+            $activities = $user_activities->page($page)
+                                            ->get_user_comments_by_type($type)
+                                            ->comments;
+                                            
+                                                                                                        
+            include(get_template_directory() . '/parts/profile-comments.php');
+        }
+        
+        //Posts
+        if($type == 'posts' || $type == 'guides' || $type == 'question') {
+            
+            $activities = $user_activities->page($page)
+                                            ->get_user_posts_by_type($type)
+                                            ->posts;
+                                            
+            include(get_template_directory() . '/parts/profile-posts.php');
+        }
+        
+        //Actions
+        if($type == 'follow' || $type == 'upvote') {
+            
+            $activities = $user_activities->page($page)
+                                            ->get_actions($type)
+                                            ->activities;
+            
+            include(get_template_directory() . '/parts/profile-recent.php');
+        }
+        
+        if($type == 'recent') {
+            
+            $activities = $user_activities->page($page)
+                                            ->get_recent_activities()
+                                            ->activities;
+                                            
+            include(get_template_directory() . '/parts/profile-recent.php');
+        }
+        
+    $output = ob_get_clean();
+    
+    echo $output;
+    
+    die;
+}
+
+add_action('wp_ajax_profile_paginate', 'profile_paginate');
+add_action('wp_ajax_nopriv_profile_paginate', 'profile_paginate');
