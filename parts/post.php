a4f67749d608fe8822b621d04df73ead029ffda7
Eddie Moya <eddie.moya@gmail.com>
2012-08-15 16:04:00 -0500

    need to echo get_category_link()

--- parts/post.php
+++ parts/post.php
@@ -1,6 +1,7 @@
 <?php
     $i = 0;
     $categories = get_the_category( $post->ID );
+    echo __FILE__;
 ?>    
 <article class="post-n-comments">
     <header class="section-header">
@@ -8,7 +9,7 @@
     </header>
     <div class="content">
         <time class="date" pubdate="pubdate"><?php echo the_date(); ?></time>
-        <a href="<?php get_category_link($categories[0]->term_id); ?>" class="category"><?php echo $categories[0]->name; ?></a>
+        <a href="<?php echo get_category_link($categories[0]->term_id); ?>" class="category"><?php echo $categories[0]->name; ?></a>
         <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
         <p><?php the_content(); ?></p>
         <p class="by">By <?php the_author_link(); ?></p>
