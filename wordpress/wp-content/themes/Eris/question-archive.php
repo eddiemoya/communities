0325e64a1cbc277a49286261c59e3274a313cc55
Eddie Moya <eddie.moya@gmail.com>
2012-08-20 11:45:18 -0500

    moving post a question header around so it works from outside the widget

--- wordpress/wp-content/themes/Eris/question-archive.php
+++ wordpress/wp-content/themes/Eris/question-archive.php
@@ -1,11 +1,28 @@
-<?php
-/**
- * @package WordPress
- * @subpackage White Label
- */
+<?php get_template_part('parts/header'); ?>
 
-get_template_part('parts/header');
+<?php $data = process_front_end_question(); ?>
 
-loop('question');
+<ul class="span8 widget_dropzone_widget dropzone">
+	<article class="span12 widget_post_a_question content-container widget post-your-question">
+		<?php include (get_template_directory() . '/parts/forms/post-a-question-step-'. $data['step'] . '.php'); ?>
+	</article>
+	<article class="span12 widget_post_a_question content-container widget post-your-question">
+		<?php get_template_part('widgets/results-list/archive'); ?>
+	</article>
+</ul>
 
-get_template_part('parts/footer');
+
+
+<ul class="span4 widget_dropzone_widget dropzone">
+	<article class="span12 widget_post_a_question content-container widget post-your-question">
+
+		<?php if ( !dynamic_sidebar('Q&A Sidebar') ) : ?> 
+        <? endif; ?>
+
+	</article>
+</ul>
+
+
+ <?php //echo "<pre>";print_r($wp_query);echo "</pre>"; ?>
+
+<?php get_template_part('parts/footer'); ?>
