aa07dcbcfbade1433b82e76ad839d089c5151910
Eddie Moya <eddie.moya@gmail.com>
2012-08-20 11:46:38 -0500

    this will be the temp section until section fronts can be modifued to work with post type query strings

---
+++ archive.php
@@ -0,0 +1,22 @@
+<?php get_template_part('parts/header'); ?>
+
+<?php //$data = process_front_end_question(); ?>
+<!-- 
+<ul class="span8 widget_dropzone_widget dropzone">
+	<article class="span12 widget_post_a_question content-container widget post-your-question">
+		<?php //include (get_template_directory() . '/parts/forms/post-a-question-step-'. $data['step'] . '.php'); ?>
+	</article>
+</ul>
+
+<ul class="span4 widget_dropzone_widget dropzone">
+	<article class="span12 widget_post_a_question content-container widget post-your-question">
+
+		<?php ?>
+
+	</article>
+</ul>
+ -->
+
+ <?php //echo "<pre>";print_r($wp_query);echo "</pre>"; ?>
+
+<?php get_template_part('parts/footer'); ?>
