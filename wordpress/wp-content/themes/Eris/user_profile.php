4743f023a9a18067cbf8203b80879196caf77b33
jcorradino <jcorra4@searshc.com>
2012-08-20 09:29:12 -0500

    adding user photo uploader

--- wordpress/wp-content/themes/Eris/user_profile.php
+++ wordpress/wp-content/themes/Eris/user_profile.php
@@ -2,14 +2,16 @@
 /*
  * Template Name: user_profile
  */
+if ($_FILES["userphoto_image_file"]["name"] != "") {
+    userphoto_profile_update(wp_get_current_user()->ID);
+}
+get_template_part('parts/header'); ?>
 
-get_template_part('parts/header');
+<form enctype="multipart/form-data" method="post">
+<?php userphoto_display_selector_fieldset(); ?>
+<button type="submit" value="submit">Submit</button>
+</form>
+<?php 
+echo userphoto(wp_get_current_user()->ID);
 
-//loop(); 
-
-//comments_template('/parts/comments.php');
-
-
-
-//get_sidebar();
 get_template_part('parts/footer');
