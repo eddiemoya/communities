f03dc4daf17ed87781ba71b532b2fd985ee863ca
Sebastian Frohm <sebastian.frohm@gmail.com>
2012-08-20 11:50:51 -0500

    Adding display logic for active actions

--- wordpress/wp-content/themes/Eris/parts/forms/post-n-comment-actions.php
+++ wordpress/wp-content/themes/Eris/parts/forms/post-n-comment-actions.php
@@ -1,4 +1,7 @@
 <?php
+    global $current_user;
+    get_currentuserinfo();
+
     $buttons = array();
 
     $acts = array('upvote', 'downvote', 'follow');
@@ -7,44 +10,71 @@
         foreach($actions as $action) {
             switch($action->action) {
                 case 'upvote':
-                    $acts['upvote'] = $action;
+                    $acts['upvote']['action'] = $action;
 
                     break;
                 case 'downvote':
-                    $acts['downvote'] = $action;
+                    $acts['downvote']['action'] = $action;
 
                     break;
                 case 'follow':
-                    $acts['follow'] = $action;
+                    $acts['follow']['action'] = $action;
 
                     break;
                 default:
                     break;
             }
+
+            if(isset($action->user) && get_class($action->user) === 'UserAction') {
+                if(is_user_logged_in()) {
+                    if($current_user->id == $action->user->userId) {
+                        switch($action->action) {
+                            case 'upvote':
+                                $acts['upvote']['myaction'] = ' active';
+
+                                break;
+                            case 'downvote':
+                                $acts['downvote']['myaction'] = ' active';
+
+                                break;
+                            case 'follow':
+                                $acts['follow']['myaction'] = ' active';
+
+                                break;
+                            default:
+                                break;
+                        }
+                    }
+                }
+            }
         }
     }
 
-    $downvoteTotal = isset($acts['downvote']->total) ? $acts['downvote']->total : 0;
-    $upvoteTotal = isset($acts['upvote']->total) ? $acts['upvote']->total : 0;
+    $downvoteTotal = isset($acts['downvote']['action']->total) ? $acts['downvote']['action']->total : 0;
+    $upvoteTotal = isset($acts['upvote']['action']->total) ? $acts['upvote']['action']->total : 0;
+
+    $myActionDownvote = isset($acts['downvote']['myaction']) ? $acts['downvote']['myaction'] : '';
+    $myActionFollow = isset($acts['follow']['myaction']) ? $acts['follow']['myaction'] : '';
+    $myActionUpvote = isset($acts['upvote']['myaction']) ? $acts['upvote']['myaction'] : '';
 
     if ( isset( $options ) && ( !empty( $options ) ) ) {
         if ( in_array( "reply", $options ) ) {
             $buttons[] = '<div class="reply"><a href="#">Reply</a></div>';
         }
         if ( in_array( "follow", $options ) ) {
-            $buttons[] = '<button type="button" shc:gizmo="actions" name="button1" value="follow" title="Follow this ' . $type . '" id="follow-question-' . $id . '" class="follow" shc:gizmo="actions" shc:gizmo:options="{actions:{post:{id:'.$id.',name:\'follow\',sub_type:\''.$sub_type.'\',type:\''.$type.'\'}}}">follow</button>';
+            $buttons[] = '<button type="button" shc:gizmo="actions" name="button1" value="follow" title="Follow this ' . $type . '" id="follow-question-' . $id . '" class="follow'.$myActionFollow.'" shc:gizmo="actions" shc:gizmo:options="{actions:{post:{id:'.$id.',name:\'follow\',sub_type:\''.$sub_type.'\',type:\''.$type.'\'}}}">follow</button>';
         }
         if ( in_array( "share", $options ) ) {
             $buttons[] = return_partial( 'parts/share' );
         }
         if ( in_array( "flag", $options ) ) {
-            $buttons[] = '<button type="button" name="button1" value="flag" title="Flag this ' . $type . '" id="flag-comment-' . $id . '" class="flag">flag</button>';
+            $buttons[] = '<button type="button" name="button1" value="flag" title="Flag this ' . $type . '" id="flag-comment-' . $id . '" class="flag'.$myActionFollow.'">flag</button>';
         }
         if ( in_array( "downvote", $options ) ) {
-            $buttons[] = '<label class="metainfo" for="downvote-comment-' . $id . '">('.$downvoteTotal.')</label><button shc:gizmo="actions" shc:gizmo:options="{actions:{post:{id:'.$id.',name:\'downvote\',sub_type:\''.$sub_type.'\',type:\''.$type.'\'}}}" type="button" name="button1" value="down vote" title="Down vote this ' . $type . '" id="downvote-comment-' . $id . '" class="downvote">down vote</button>';
+            $buttons[] = '<label class="metainfo" for="downvote-comment-' . $id . '">('.$downvoteTotal.')</label><button shc:gizmo="actions" shc:gizmo:options="{actions:{post:{id:'.$id.',name:\'downvote\',sub_type:\''.$sub_type.'\',type:\''.$type.'\'}}}" type="button" name="button1" value="down vote" title="Down vote this ' . $type . '" id="downvote-comment-' . $id . '" class="downvote'.$myActionDownvote.'">down vote</button>';
         }
         if ( in_array( "upvote", $options ) ) {
-            $buttons[] = '<label class="metainfo" shc:gizmo="actions" for="upvote-comment-' . $id . '">('.$upvoteTotal.')</label><button shc:gizmo="actions" shc:gizmo:options="{actions:{post:{id:'.$id.',name:\'upvote\',sub_type:\''.$sub_type.'\',type:\''.$type.'\'}}}" type="button" name="button1" value="helpful" title="Up vote this ' . $type . '" id="upvote-comment-' . $id . '" class="upvote">helpful</button>';
+            $buttons[] = '<label class="metainfo" shc:gizmo="actions" for="upvote-comment-' . $id . '">('.$upvoteTotal.')</label><button shc:gizmo="actions" shc:gizmo:options="{actions:{post:{id:'.$id.',name:\'upvote\',sub_type:\''.$sub_type.'\',type:\''.$type.'\'}}}" type="button" name="button1" value="helpful" title="Up vote this ' . $type . '" id="upvote-comment-' . $id . '" class="upvote'.$myActionUpvote.'">helpful</button>';
         }
     }
     
