0fdefede4ab0f869fcee87cc2d4716b5359f2e7a
Eddie Moya <eddie.moya@gmail.com>
2012-08-20 15:31:11 -0500

    making comments work as answers on question pages

--- parts/commentForm.php
+++ parts/commentForm.php
@@ -1,11 +1,12 @@
 <?php
-    $comment_type = get_post_type( $post->ID ) == 'question' ? 'an answer' : 'a comment';
+    $comment_type_text = get_post_type( $post->ID ) == 'question' ? 'an answer' : 'a comment';
+    $comment_type = get_post_type( $post->ID ) == 'question' ? 'answer' : 'comment';
 
     if(!is_user_logged_in()) {
 ?>
         <div class="commentForm" xmlns="http://www.w3.org/1999/html">
             <div class="top clearfix">
-                <a class="leaveComment" shc:gizmo="moodle" shc:gizmo:options="{moodle: {width:480, target:ajaxdata.ajaxurl, type:'POST', data:{action: 'get_template_ajax', template: 'page-login'}}}">Leave <?php echo $comment_type; ?> <span class="smaller">&#9660;</span></a>
+                <a class="leaveComment" shc:gizmo="moodle" shc:gizmo:options="{moodle: {width:480, target:ajaxdata.ajaxurl, type:'POST', data:{action: 'get_template_ajax', template: 'page-login'}}}">Leave <?php echo $comment_type_text; ?> <span class="smaller">&#9660;</span></a>
             </div>
         </div>
 <?php
@@ -36,14 +37,14 @@
     ?>
         <div class="commentForm" xmlns="http://www.w3.org/1999/html">
             <div class="top clearfix">
-                <span class="leaveComment">Leave <?php echo $comment_type; ?> <span class="smaller">&#9660;</span></span>
+                <span class="leaveComment">Leave <?php echo $comment_type_text;?> <span class="smaller">&#9660;</span></span>
             </div>
             <?php
                 if ( get_option( 'comment_registration' ) && !is_user_logged_in() ) :
                     echo $args['must_log_in'];
                     do_action( 'comment_form_must_log_in_after' );
                 else : ?>
-                    <form action="<?php echo site_url( '/wp-comments-post.php' ); ?>?comment_type=answer" method="post" id="<?php echo esc_attr( $args['id_form'] ); ?>">
+                    <form action="<?php echo site_url( '/wp-comments-post.php' ); ?>" method="post" id="<?php echo esc_attr( $args['id_form'] ); ?>">
                         <?php
                             do_action( 'comment_form_top' );
 
@@ -59,6 +60,7 @@
                                     <input type="submit" shc:gizmo="tooltip" shc:gizmo:options="{tooltip:{events:['click'],displayData:'shower2'}}" class="<?php echo theme_option("brand"); ?>_button azure" value="Cancel" />
                                     <?php comment_id_fields( $post->ID ); ?>
                                 </p>
+                                <input type='hidden' name='comment_type' value='<?php echo $comment_type; ?>' />
                             <?php do_action( 'comment_form', $post->ID ); ?>
                         </form>
                     <?php endif; ?>
