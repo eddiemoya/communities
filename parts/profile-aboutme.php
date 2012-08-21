f521fa55553cef8c44647aba1d282038264093b1
Carl Albrecht-Buehler <calbre1@searshc.com>
2012-08-21 10:19:13 -0500

    Working on userphoto with Jason. Some minor fixes for author.php

--- parts/profile-aboutme.php
+++ parts/profile-aboutme.php
@@ -18,24 +18,24 @@
         <div class="span3 badge">
             <?php echo userphoto(wp_get_current_user()->ID);?>
             
-            <form enctype="multipart/form-data" method="post">
+            <form enctype="multipart/form-data" method="post" class="upload-photo">
             <?php userphoto_display_selector_fieldset_frontend(); ?>
-            <button type="submit" value="submit">Submit</button>
+            <button type="submit" value="save">Save</button>
             </form>
             
         </div>
         <div class="span9 info">
-            <h4><?php echo (isset($user_profile['name']) && ! empty($user_profile['name'])) ? $user_profile['name'] : 'n/a';?></h4>
+            <h4><?php echo (isset($user_profile['name']) && ! empty($user_profile['name'])) ? $user_profile['name'] : '&nbsp;';?></h4>
         </div>
         <dl class="span12 vitals">
             <dt>Screen Name</dt>
-            <dd><?php echo (isset($user_profile['screenname']) && ! empty($user_profile['screenname'])) ? $user_profile['screenname'] : 'n/a'; ?></dd>
+            <dd><?php echo (isset($user_profile['screenname']) && ! empty($user_profile['screenname'])) ? $user_profile['screenname'] : '&ndash;'; ?></dd>
             <dt>Email</dt>
-            <dd><?php echo (isset($user_profile['email']) && ! empty($user_profile['email']) ) ? $user_profile['email'] : 'n/a'; ?></dd>
+            <dd><?php echo (isset($user_profile['email']) && ! empty($user_profile['email']) ) ? $user_profile['email'] : '&ndash;'; ?></dd>
             <dt>Location</dt>
-            <dd><address><?php echo (isset($user_profile['zipcode']) && ! empty($user_profile['zipcode'])) ? $user_profile['zipcode']: 'n/a'; ?></address></dd>
+            <dd><address><?php echo (isset($user_profile['zipcode']) && ! empty($user_profile['zipcode'])) ? $user_profile['zipcode']: '&ndash;'; ?></address></dd>
             <dt>Birthday</dt>
-            <dd><time datetime="<?php echo (isset($user_profile['dob']) && ! empty($user_profile['dob'])) ? date( "Y-m-d", strtotime($user_profile["dob"])) : ''; ?>" pubdate="pubdate"><?php echo (isset($user_profile['dob']) && ! empty($user_profile['dob'])) ? date( "F j, Y", strtotime($user_profile["dob"])) : 'n/a'; ?></time></dd>
+            <dd><time datetime="<?php echo (isset($user_profile['dob']) && ! empty($user_profile['dob'])) ? date( "Y-m-d", strtotime($user_profile["dob"])) : ''; ?>" pubdate="pubdate"><?php echo (isset($user_profile['dob']) && ! empty($user_profile['dob'])) ? date( "F j, Y", strtotime($user_profile["dob"])) : '&ndash;'; ?></time></dd>
         </dl>
         
         <?php else:?>
