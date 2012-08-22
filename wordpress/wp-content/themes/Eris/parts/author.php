ad0a3006aff8228633d8968e97ab26ad4a79e708
Eddie Moya <eddie.moya@gmail.com>
2012-08-22 00:28:17 -0500

    correcting comment and answer counts

--- wordpress/wp-content/themes/Eris/parts/author.php
+++ wordpress/wp-content/themes/Eris/parts/author.php
@@ -1,6 +1,10 @@
         <?php 
         	$last_post = get_posts(array('numberposts'=>1, 'author' => $data->ID)); 
         	$last_post_date = (isset($last_post[0])) ? strtotime($last_post[0]->post_date): '0';
+
+          //echo "<pre>";print_r($data);echo "</pre>";
+          $answer_count = (function_exists('get_custom_comment_count')) ? get_custom_comment_count('answer', null, array('user_id' => $data->ID) ) : '';
+          $comment_count = (function_exists('get_custom_comment_count')) ? get_custom_comment_count('comment', null, array('user_id' => $data->ID) ) : '';
         ?>
         <li class="member lone-result clearfix">
           <div class="span2 badge labeled">
@@ -14,9 +18,9 @@
             <article class="content-comments">
               <p>Last posted on <time datetime="<?php echo date('Y-m-d',$last_post_date); ?>" pubdate="pubdate"><?php echo date('M d, Y',$last_post_date); ?></time>.</p>
               <ul>
-                <li>8 answers</li>
+                <li><?php echo $answer_count; ?> answers</li>
                 <li><?php echo count_user_posts($data->ID); ?> blog posts</li>
-                <li><?php comments_number(); ?></li>
+                <li><?php echo $comment_count; ?> comments</li>
               </ul>
             </article>
           </div>
