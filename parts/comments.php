f6338332b19a1076538737eedbdb5661eb7e4936
Sebastian Frohm <sebastian.frohm@gmail.com>
2012-08-21 20:09:06 -0500

    Adding child comments; ensuring action retrieval happens first; ensuring post types display excerpts properly

--- parts/comments.php
+++ parts/comments.php
@@ -53,19 +53,38 @@
                     echo 'not active';
                 }
             ?>
+            <form action="http://localhost:4000/wp-comments-post.php" method="post" id="commentform" style="display: block; ">
+                <textarea id="comment" name="comment" cols="45" rows="8" shc:gizmo="tooltip" shc:gizmo:options="{tooltip:{events:['click'],displayData:'shower'}}" aria-required="true"></textarea>
+                <p class="form-submit">
+                    <input type="submit" id="submit" class="kmart_button" value="Post">
+                    <input type="submit" shc:gizmo="tooltip" shc:gizmo:options="{tooltip:{events:['click'],displayData:'shower2'}}" class="kmart_button azure" value="Cancel">
+                    <input type="hidden" name="comment_post_ID" value="332" id="comment_post_ID">
+                    <input type="hidden" name="comment_parent" id="comment_parent" value="<?php echo $comment->comment_ID; ?>">
+                </p>
+                <input type="hidden" name="comment_type" value="<?php echo $comment_type; ?>">
+                <input type="hidden" id="_wp_unfiltered_html_comment" name="_wp_unfiltered_html_comment" value="27ff0ea567">
+            </form>
         </div>
-        <!-- <ol class="children">
-             <li class="comment clearfix<?php echo $container_class; ?>">
-                 <?php get_partial( 'parts/crest', $crest_options ); ?>
-                 <div class="span10">
-                     <time class="content-date" datetime="<?php echo date( "Y-m-d", $comment_date ); ?>" pubdate="pubdate"><?php echo date( "F j, Y g:ia", $comment_date ); ?></time>
-                     <article>
-                         <?php echo $comment->comment_content; ?>
-                     </article>
-                     <?php get_partial( 'parts/forms/post-n-comment-actions', $comment_actions ); ?>
-                 </div>
-             </li>
-        </ol> -->
+    <?php
+         if(isset($comment->children) && !empty($comment->children)) :
+             foreach($comment->children as $child) :
+    ?>
+            <ol class="children">
+                 <li class="comment clearfix<?php echo $container_class; ?>">
+                     <?php get_partial( 'parts/crest', $crest_options ); ?>
+                     <div class="span10">
+                         <time class="content-date" datetime="<?php echo date( "Y-m-d", $comment_date ); ?>" pubdate="pubdate"><?php echo date( "F j, Y g:ia", $comment_date ); ?></time>
+                         <article>
+                             <?php echo $comment->comment_content; ?>
+                         </article>
+                         <?php get_partial( 'parts/forms/post-n-comment-actions', $comment_actions ); ?>
+                     </div>
+                 </li>
+            </ol>
+    <?php
+            endforeach;
+         endif;
+    ?>
     </li>
 <?php
         }
