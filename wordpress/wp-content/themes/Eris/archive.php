369b35d625905033542318655372ec70d72d7bde
Eddie Moya <eddie.moya@gmail.com>
2012-08-21 23:34:53 -0500

    adding thr archive page for pposts

--- wordpress/wp-content/themes/Eris/archive.php
+++ wordpress/wp-content/themes/Eris/archive.php
@@ -1,24 +1,75 @@
 <?php get_template_part('parts/header'); ?>
 
-<?php loop('post');
+<?php
+	$posts = get_posts(array('post_type' => 'guide', 'numberposts' => 2));
 
-//$data = process_front_end_question(); ?>
-<!-- 
+	//echo "<pre>";print_r($posts);echo "</pre>";
+	$nav_menus = get_terms( 'nav_menu', array( 'hide_empty' => true ) );
+	$sidebar_args = array(
+	    'before_widget' => '<article class="span12 content-container widget dropzone-widget sidebar-widget widget %2$s" id="%1$s">',
+	    'after_widget' => '</article>',
+	    'before_title' => '<header class="content-header"><h3 class="widgettitle">',
+	    'after_title' => '</h3></header>'
+	);
+	$fp1_instance = array(
+		'widget_title' => 'The Community Blog',
+		'widget_subtitle' => 'Find out more. About whatever interests you.',
+		'show_title' => true,
+		'show_subtitle' => true,
+		'show_comment_count' => 'on',
+		'show_category' => true,
+		'show_date' => true,
+		'show_thumbnail' => true,
+		'show_content' => true,
+		'show_share'	=> true,
+		'filter-by' => 'manual',
+		'widget_name' => 'featured-post',
+		'limit' => 1,
+		'post__in_1' => $posts[0]->ID
+	);
+	$fp2_instance = array(
+		'show_comment_count' => 'on',
+		'show_category' => true,
+		'show_date' => true,
+		'show_thumbnail' => true,
+		'show_content' => true,
+		'show_share'	=> true,
+		'filter-by' => 'manual',
+		'widget_name' => 'featured-post',
+		'limit' => 1,
+		'post__in_1' => (isset($posts[1])) ? $post[1]->ID : 100
+	);
+
+ ?>
+
 <ul class="span8 widget_dropzone_widget dropzone">
-	<article class="span12 widget_post_a_question content-container widget post-your-question">
-		<?php //include (get_template_directory() . '/parts/forms/post-a-question-step-'. $data['step'] . '.php'); ?>
-	</article>
+
+	<?php if(!dynamic_sidebar('Posts Content Area')) : ?> 
+			<!-- <article class="span12 widget_featured-post content-container widget featured-post"> -->
+			<?php the_widget('Featured_Posts_Widget', $fp1_instance, $sidebar_args ) ?>
+			<?php the_widget('Featured_Posts_Widget', $fp2_instance, $sidebar_args ) ?>
+		
+		</article>
+	<?php endif; ?> 
+
+
 </ul>
 
 <ul class="span4 widget_dropzone_widget dropzone">
-	<article class="span12 widget_post_a_question content-container widget post-your-question">
+	<?php 
+		if(!dynamic_sidebar('Posts Sidebar')){
+	
+			$instance = array('title' => 'About', 'text' => '<p><strong>The Community Blog</strong> is a place where you can discover countless posts about the things that interest you. So go ahead...</p><p><strong>Explode. Comment. Repeat.</strong></p>');
+			the_widget('Content_Blurb', $instance, $sidebar_args);
 
-		<?php ?>
+			$instance = array('nav_menu' => $nav_menus[4]->term_id, 'title' => 'First Time Here?', 'sub-title' => "Here's our best stuff");
+			the_widget('Communities_Menu_Widget', $instance, $sidebar_args);
 
-	</article>
+			$instance = array('nav_menu' => $nav_menus[5]->term_id, 'title' => 'Related Stories');
+			the_widget('Communities_Menu_Widget', $instance, $sidebar_args);
+
+		 }
+	?>
 </ul>
- -->
-
- <?php //echo "<pre>";print_r($wp_query);echo "</pre>"; ?>
 
 <?php get_template_part('parts/footer'); ?>
