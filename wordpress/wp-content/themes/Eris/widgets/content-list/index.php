f7cc17c73f7f4efd1beaf1972baeabf42204955a
jcorradino <jcorra4@searshc.com>
2012-08-22 19:03:28 -0500

    adding category selector to content list widget, fixing content list widget stuff, adding single view

--- wordpress/wp-content/themes/Eris/widgets/content-list/index.php
+++ wordpress/wp-content/themes/Eris/widgets/content-list/index.php
@@ -1,12 +1,14 @@
-    <header class="content-header">
-        <h3>
-            Related Articles
-        </h3>
-    </header>
-
+<?php 
+    get_template_part('parts/header', 'widget');
+    $category = get_category_by_slug(is_widget()->category);
+?>
     <section class="content-body content-list clearfix">
-        <?php loop_by_type('contentlist'); ?>
-        
-        <div class="more-link"><a href="#">See All {Category Name} Articles</a></div>
+        <?php loop('post', 'contentlist'); ?>
+
+        <?php if($category) : ?>
+            <div class="more-link"><a href="<?php echo get_category_link($category->term_id); ?>">See All <?php echo $category->name; ?> Articles</a></div>
+        <?php endif; ?>
 
     </section>
+    
+<?php get_template_part('parts/footer', 'widget') ;?>
---
+++ wordpress/wp-content/themes/Eris/widgets/content-list/single.php
@@ -0,0 +1,1 @@
+<?php get_template_part('widgets/content-list/index'); ?>
