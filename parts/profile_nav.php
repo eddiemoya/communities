f40eb8116521e7e435e21a32ab5c7772bd976761
Carl Albrecht-Buehler <calbre1@searshc.com>
2012-08-28 11:46:23 -0500

    Added some additional logic for profile.

--- parts/profile_nav.php
+++ parts/profile_nav.php
@@ -14,10 +14,11 @@
             "name" => "Questions",
             "url" => $url_no_qs . '?post-type=question'
         ),
+        /*
         "" => array(
             "name" => "Comments",
             "url" => $url_no_qs . '?post-type='
-        ),
+        ),*/
          "comment" => array(
             "name" => "Comments",
             "url" => $url_no_qs . '?post-type=comment'
@@ -94,35 +95,35 @@
     if( $_REQUEST['post-type'] != 'aboutme' && !empty( $available_tabs ) ): 
 ?>
 <nav class="bar clearfix">
-  <ul class="clearfix">
-    <?php
+    <ul class="clearfix">
+<?php
     if($current_tab == 'Community Activity') {
-    	
-	    foreach ($a_navigation as $lone_nav_id => $lone_nav) {
-	    	
-	     if ( in_array($lone_nav_id, $available_tabs) ) {
-	     	
-	        if ( $lone_nav_id == $current_nav ) {
-	        	
-	          echo '<li class="active">' . $lone_nav["name"] . '</li>';
-	        }
-	        
-	        else {
-	        	
-	          echo '<li><a href="' . $lone_nav["url"] . '">' . $lone_nav["name"] . '</a></li>';
-	          
-	        }
-	      }
-	    }
+
+        foreach ($a_navigation as $lone_nav_id => $lone_nav) {
+
+            if ( in_array($lone_nav_id, $available_tabs) ) {
+
+                if ( $lone_nav_id == $current_nav ) {
+
+                    echo '<li class="active">' . $lone_nav["name"] . '</li>';
+                }
+
+                else {
+
+                    echo '<li><a href="' . $lone_nav["url"] . '">' . $lone_nav["name"] . '</a></li>';
+
+                }
+            }
+        }
     }
-    ?>
-  </ul>
+?>
+    </ul>
 </nav>
 <?php endif; ?>
 
 <?php
     # catch the circumstance where the user has not yet done anything on the site.
-    if( $_REQUEST['post-type'] != 'aboutme' && empty( $available_tabs ) ):
+    if( $_REQUEST['post-type'] != 'aboutme' && empty( $available_tabs ) && $profile_type == 'myprofile' ):
 ?>
     <section class="no-activity">
         Get out there and show us what you're all about!
