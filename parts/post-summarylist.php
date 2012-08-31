3a6d273e463c473be987b25b4079aa1670c2cf1e
jcorradino <jcorra4@searshc.com>
2012-08-31 12:42:39 -0500

    changing the question list to the summary list, modifying the query

---
+++ parts/post-summarylist.php
@@ -0,0 +1,33 @@
+<?php
+    if(is_widget()->list_style == "show-category") :
+        $header_display = "";
+        foreach(wp_get_post_terms($post->ID, 'category') as $cat) :
+            if($cat->slug != "uncategorized") :
+                $header_display = ucwords($cat->name);
+            endif;
+        endforeach;
+    elseif(is_widget()->list_style == "post-type") :
+        $header_display = ucwords(get_post_type_object(get_post_type())->labels->singular_name);
+    endif;
+?>
+<article class="content-item span12">
+    <div class="post-data span12">
+        <?php if($header_display != "" || !empty(is_widget()->show_date)) : ?>
+            <section class="clearfix">
+                <?php if($header_display != "") : ?>
+                    <p class="post-category left"><?php echo $header_display; ?></p>
+                <?php endif; ?>
+                <?php if(is_widget()->show_date == "on") : ?>
+                    <p class="post-date right"><?php the_date(); ?></p>
+                <?php endif; ?>
+            </section>
+        <?php endif; ?>
+        <section class="clearfix">
+            <p class="post-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></p>
+            <p class="comments-count"><?php comments_number( 'no answers', '1 answer', '% answers' ); ?></p>
+            <?php if(!empty(is_widget()->show_share)) : ?>
+                <?php get_partial( 'parts/share', array( "version" => is_widget()->share_style, "url" => get_post_permalink( $post->ID ) ) ); ?>
+            <?php endif; ?>
+        </section>
+    </div>
+</article>
