e109eb0a7ba5d51c411b94751b3b8becc07552d1
Sebastian Frohm <sebastian.frohm@gmail.com>
2012-09-07 15:56:28 -0500

    Fixing minor issues in validator

--- wordpress/crons/validator.php
+++ wordpress/crons/validator.php
@@ -17,18 +17,22 @@
     $readableErrorFileHandle = fopen($readableErrorFile, 'a+');
 
     if(file_exists($nextDataSetFile)) {
-        $batchHandle = fopen($nextDataSetFile, 'w+');
+        $batchHandle = fopen($nextDataSetFile, 'a+');
 
         $nextDataSet = fread($batchHandle, 1024);
 
         $nextDataSet = (int)trim($nextDataSet);
+
+        ftruncate($batchHandle, 0);
     } else {
         $batchHandle = fopen($nextDataSetFile, 'w+');
 
         $nextDataSet = 0;
     }
 
-    echo 'The next batch is '.$nextDataSet."\n";
+    $finalPosition = ($nextDataSet >= 0) ? $nextDataSet + 10000 : 10000;
+
+    echo 'The next batch set starting point is array spot '.$nextDataSet."\n";
 
     if(file_exists($readFile)) {
         echo 'Starting file load at '.date('h:i:s M d, Y').'...'."\n";
@@ -40,7 +44,7 @@
 
         $startTimeAfterFile = microtime(true);
 
-        for($i = $nextDataSet; $i < 10000; $i++) {
+        for($i = $nextDataSet; $i < $finalPosition; $i++) {
             $ourError = '';
 
             if(!isset($xml->user[$i]->email) || !preg_match('/^.+@.+?\.[a-zA-Z]{2,}$/', $xml->user[$i]->email)) {
@@ -127,37 +131,43 @@
         $nextDataSet += 10000;
         
         fwrite($batchHandle, $nextDataSet);
-        
-        // Create the XML with all data issues
-        $xml = new SimpleXMLElement('<users/>');
 
-        foreach($badData as $key=>$val) {
-            $user = $xml->addChild('user');
+        if(isset($badData) && !empty($badData)) {
+            // Create the XML with all data issues
+            if(file_exists($badDataFile)) {
+                $xml = new SimpleXMLElement($badDataFile);
+            } else {
+                $xml = new SimpleXMLElement('<users/>');
+            }
 
-            $user->addChild('posInArray', $val->posInArray);
-            $user->addChild('id', $val->id);
-            $user->addChild('screen_name', $val->screen_name);
-            $user->addChild('email', $val->email);
-            $user->addChild('display_name', $val->display_name);
-            $user->addChild('first_name', $val->first_name);
-            $user->addChild('last_name', $val->last_name);
-            $user->addChild('guid', $val->guid);
+            foreach($badData as $key=>$val) {
+                $user = $xml->addChild('user');
+
+                $user->addChild('posInArray', $val->posInArray);
+                $user->addChild('id', $val->id);
+                $user->addChild('screen_name', $val->screen_name);
+                $user->addChild('email', $val->email);
+                $user->addChild('display_name', $val->display_name);
+                $user->addChild('first_name', $val->first_name);
+                $user->addChild('last_name', $val->last_name);
+                $user->addChild('guid', $val->guid);
+            }
+
+            // Saving pretty XML
+            $dom = new DOMDocument('1.0');
+
+            $dom->preserveWhiteSpace = false;
+            $dom->formatOutput = true;
+            $dom->loadXML($xml->asXML());
+
+            $dom->save($badDataFile);
         }
-
-        // Saving pretty XML
-        $dom = new DOMDocument('1.0');
-
-        $dom->preserveWhiteSpace = false;
-        $dom->formatOutput = true;
-        $dom->loadXML($xml->asXML());
-
-        $dom->save($badDataFile);
 
         $endTime = microtime(true);
         echo $endTime - $startTimeAfterFile;
-        echo ' is the elapsed time to validate out '.$i.' users'."\n";
+        echo ' is the elapsed time to validate out 10000 users'."\n";
         echo $endTime - $startTime;
-        echo ' is the elapsed time to load the file and validate out '.$i.' users';
+        echo ' is the elapsed time to load the file and validate out 10000 users';
         exit(0);
     } else {
         echo 'there is no file to read!';
