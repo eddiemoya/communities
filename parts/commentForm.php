f32601e498349d84853710144d61bc674a3a207b
Tim Steele <tsteel5@searshc.com>
2012-09-11 14:35:40 -0500

    Major Style updates

--- parts/commentForm.php
+++ parts/commentForm.php
@@ -1,3 +1,4 @@
+<div class="ugc-comment-answer_form span12">
 <?php
 	global $current_user;
     get_currentuserinfo();
@@ -11,18 +12,21 @@
 
     if(!is_user_logged_in()) {
 ?>
-        <div class="commentForm" xmlns="http://www.w3.org/1999/html">
+	<div class="trigger discussion">
+		<a href="#" shc:gizmo="moodle" shc:gizmo:options="{moodle: {width:480, target:ajaxdata.ajaxurl, type:'POST', data:{action: 'get_template_ajax', template: 'page-login'}}}">Leave <?php echo $comment_type_text; ?> <span class="smaller">&#9660;</span></a>
+	</div>
+<!--         <div class="commentForm" xmlns="http://www.w3.org/1999/html">
             <div class="top clearfix">
                 <a class="leaveComment" shc:gizmo="moodle" shc:gizmo:options="{moodle: {width:480, target:ajaxdata.ajaxurl, type:'POST', data:{action: 'get_template_ajax', template: 'page-login'}}}">Leave <?php echo $comment_type_text; ?> <span class="smaller">&#9660;</span></a>
             </div>
-        </div>
+        </div> -->
 <?php
     } else {
         $fields =  array();
 
         $args = array(
             'fields'               => apply_filters( 'comment_form_default_fields', $fields ),
-            'comment_field'        => '<textarea id="comment" name="comment" rows="8" aria-required="true" shc:gizmo:form="{required:true}">'. $comment_value .'</textarea>',
+            'comment_field'        => '<textarea id="comment-answer_textarea" class="input_textarea discussion" name="comment">'. $comment_value .'</textarea>',
             'must_log_in'          => null,
             'logged_in_as'         => null,
             'comment_notes_before' => null,
@@ -42,56 +46,59 @@
 
         if ( comments_open() ) :
     ?>
-    <div class="commentForm" xmlns="http://www.w3.org/1999/html">
-        <div class="top clearfix">
-            <span class="leaveComment">Leave <?php echo $comment_type_text;?> <span class="smaller">&#9660;</span></span>
-        </div>
-        
-        <?php
+    <div class="trigger discussion">
+			<a href="#">Leave <?php echo $comment_type_text;?> <span class="smaller">&#9660;</span></a>
+		</div>
+
+      <?php
         if ( get_option( 'comment_registration' ) && !is_user_logged_in() ) :
             echo $args['must_log_in'];
             do_action( 'comment_form_must_log_in_after' );
             else : ?>
-            <form id="leave-response" action="<?php echo site_url( '/wp-comments-post.php' ); ?>" method="post" shc:gizmo="transFormer">
             
-                <?php
-                # If a user doesn't have a screen name, prompt them to enter one
-                if(get_user_meta($current_user->ID, 'sso_guid') && ! has_screen_name($current_user->ID)):
-                    
-                    # If there is a screen name error, show it
-                    if(isset($_GET['comm_err']) && ($_GET['cid'] == 0)): 
-                ?>
-                   <div>
-                       <?php echo stripslashes(urldecode($_GET['comm_err']));?>
-                   </div>
-                   <?php endif; ?>
-                    <label for="screen-name" class="required">Screen Name</label>
-                    <input type="text" class="input_text" name="screen-name" id="screen-name" value="<?php echo $screen_name_value;?>" shc:gizmo:form="{required:true, special: 'screen-name', message: 'Screen name invalid. Screen name is already in use or does not follow the screen name guidelines.'}"/>
-                <?php endif;?>
-               
-               
-                <?php
-                
-                
-                do_action( 'comment_form_top' );
-
-                if ( is_user_logged_in() ) :
-                    echo apply_filters( 'comment_form_logged_in', $args['logged_in_as'], $commenter, $user_identity );
-                endif;
-
-                echo apply_filters( 'comment_form_field_comment', $args['comment_field'] );
-                echo $args['comment_notes_after'];
-                ?>
-                <p class="form-submit">
-                    <input type="submit" id="<?php echo esc_attr( $args['id_submit'] ); ?>" class="<?php echo theme_option("brand"); ?>_button" value="<?php echo esc_attr( $args['label_submit'] ); ?>" />
-                    <input type="reset" class="<?php echo theme_option("brand"); ?>_button azure" value="Cancel" />
-                    <?php comment_id_fields( $post->ID ); ?>
-                </p>
-                <input type='hidden' name='comment_type' value='<?php echo $comment_type; ?>' />
-                <?php do_action( 'comment_form', $post->ID ); ?>
-            </form>
-        <?php endif; ?>
-    </div>
+            <?php 
+            	# If there is a screen name error, show it
+            	if(isset($_GET['comm_err']) && ($_GET['cid'] == 0)): 
+            ?>
+            <div class="form-errors">
+               <?php echo stripslashes(urldecode($_GET['comm_err']));?>
+        		</div>
+        		<?php endif; ?>
+        		
+            <form action="<?php echo site_url( '/wp-comments-post.php' ); ?>" method="post" shc:gizmo="transFormer">
+							<ul class="form-fields">
+								<?php
+	                # If a user doesn't have a screen name, prompt them to enter one
+	                if(get_user_meta($current_user->ID, 'sso_guid') && ! has_screen_name($current_user->ID)):
+	              ?>
+	              <li class="clearfix">	
+	                <label for="screen-name" class="required">Screen Name</label>
+	                <input type="text" class="input_text" name="screen-name" id="screen-name" value="<?php echo $screen_name_value;?>" shc:gizmo:form="{required:true, special: 'screen-name', message: 'Screen name invalid. Screen name is already in use or does not follow the screen name guidelines.'}"/>
+	             	</li>
+	              <?php endif;?>
+								<li class="clearfix">
+									<?php
+		                do_action( 'comment_form_top' );
+		
+		                if ( is_user_logged_in() ) :
+		                    echo apply_filters( 'comment_form_logged_in', $args['logged_in_as'], $commenter, $user_identity );
+		                endif;
+		
+		                echo apply_filters( 'comment_form_field_comment', $args['comment_field'] );
+		                echo $args['comment_notes_after'];
+	                ?>
+								</li>
+								<li class="clearfix">
+									<button type="submit" class="<?php echo theme_option("brand"); ?>_button"><?php echo esc_attr( $args['label_submit'] ); ?></button>
+									<button class="<?php echo theme_option("brand"); ?>_button azure">Cancel</button>
+                  <?php comment_id_fields( $post->ID ); ?>
+                  <input type='hidden' name='comment_type' value='<?php echo $comment_type; ?>' />
+                	<?php do_action( 'comment_form', $post->ID ); ?>
+								</li>
+							</ul>
+						</form>
+      <?php endif; ?>
+</div>
     <?php
             do_action( 'comment_form_after' );
         else :
