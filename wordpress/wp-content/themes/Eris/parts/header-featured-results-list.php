0a7a6d870ad4eb4e4855e3f9f99746db6fb75c5c
jcorradino <jcorra4@searshc.com>
2012-09-12 17:06:03 -0500

    removing adding new header for featured results list

---
+++ wordpress/wp-content/themes/Eris/parts/header-featured-results-list.php
@@ -0,0 +1,16 @@
+<article class="content-container featured-results-list">
+
+        <?php if (is_widget()->show_title && !empty(is_widget()->widget_title)) : ?>
+
+            <header class="content-header">
+                <h3><?php echo is_widget()->widget_title; ?></h3>
+                <?php if(is_widget()->show_subtitle && is_widget()->widget_subtitle != "") : ?>
+                    <h4><?php echo is_widget()->widget_subtitle; ?></h4>
+                <?php endif; ?>
+            </header>
+
+        <?php endif; ?>
+
+
+        <!-- << echo (is_single()) ? 'section' : 'ol'; ?> class="content-body clearfix"> -->
+        <section class="content-body clearfix">
