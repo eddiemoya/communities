4528fa37788925ae6d255a82f05122c523c6af4d
Eddie Moya <eddie.moya@gmail.com>
2012-09-14 02:53:04 -0500

    simplifying the holy clustertits out of the results list - this probably breaks a bunch of shit - but whatever this breaks needs to get consolodated into this anhow

--- parts/footer-widget.php
+++ parts/footer-widget.php
@@ -1,3 +1,6 @@
         <!-- </// echo (is_single()) ? 'section' : 'ol'; ?>><!-- content-body -->
     </section>
 <!-- </article> content-container -->
+        <section class="pagination">
+             <?php echo posts_nav_link(); ?>
+        </section>
--- widgets/results-list/archive.php
+++ widgets/results-list/archive.php
@@ -7,43 +7,14 @@
 
 if(!is_ajax())
     get_template_part('parts/header', 'results-list');
-
- if (!have_posts()) : ?>
-
-        <section class="no-results">
-            <p class="header">Sorry, We could not find any matches for "<?php echo $s; ?>"</p>
-            <p>Please try your search again</p>
-            
-            <p class="header">Search Tips:</p>
-            <ul class="bullets">
-                <li>Double check your search for typos or spelling errors.</li>
-                <li>Try one of the suggested keywords displayed when you type in the search field.</li>
-                <li>Try using a different word or more general word or phrase.</li>
-                <li>If you searched an acronym, try using the full name.</li>
-            </ul>
-        </section>
-
-<?php endif;?> 
-        <!--     # Only show pagination if there are enough posts //if ( $wp_query->post_count > $wp_query->query_vars['posts_per_page'] ) : -->
-        <section class="pagination">
-             <?php echo posts_nav_link(); ?>
-        </section>
-
-<?php loop('post-results-list'); ?>
+    $post_type = get_query_var('post_type');
+    loop('results-list', array($post_type, 'post'),  'parts', 'parts/no-results.php');
     
-  <?  // if(is_widget()->template == "featured") :
+  // if(is_widget()->template == "featured") :
     //     get_partial("parts/post-featured-post", array("widget" => is_widget()));
     // else :
         
    // endif;
-  ?>
-    
 
-        <!-- # Only show pagination if there are enough posts //if ( $wp_query->post_count > $wp_query->query_vars['posts_per_page'] ) : -->
-        <section class="pagination">
-             <?php echo posts_nav_link(); ?>
-        </section>
-<?php
-
-    if(!is_ajax())
-        get_template_part('parts/footer', 'widget');
+if(!is_ajax())
+    get_template_part('parts/footer', 'widget');
