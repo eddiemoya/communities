c973ce9e8c26e3e3dfab0aa929ea74f2786c9b5f
Eddie Moya <eddie.moya@gmail.com>
2012-09-18 00:58:37 -0500

    the root file for the featured widget is now much simpler

--- wordpress/wp-content/themes/Eris/widgets/featured-post/single.php
+++ wordpress/wp-content/themes/Eris/widgets/featured-post/single.php
@@ -3,9 +3,7 @@
 get_template_part('parts/header', 'widget') ;
     //global $excerptLength; $excerptLength = 200;
 
-    $post_type = get_query_var('post_type');
-    $post_type = (is_array($post_type)) ? $post_type[0] : $post_type;
-    loop(array($post_type, 'post'), 'featured', 'parts');
+    loop(array($wp_query->post->post_type, 'post'), null, 'widgets/featured-post');
 
 
 get_template_part('parts/footer', 'widget');
