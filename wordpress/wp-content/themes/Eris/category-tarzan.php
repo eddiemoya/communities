31dcfe6938673e9f6d023c48c2a9f5a56c512b56
Tim Steele <tsteel5@searshc.com>
2012-09-18 09:02:23 -0500

    Apply stash

---
+++ wordpress/wp-content/themes/Eris/category-tarzan.php
@@ -0,0 +1,415 @@
+<?php
+/**
+ * Template Name: batman
+ * @package WordPress
+ * @subpackage White Label
+ */
+
+get_template_part('parts/header');
+
+//loop();
+?>
+
+	<section class="span12">
+		
+		<section class="span8">
+			
+			<section class="span12 featured-members content-container">
+				
+				<hgroup class="content-header">
+					<h3>Meet the Community Team</h3>
+		      <h4>Whatever your question or issue, we're here to help</h4>
+				</hgroup>
+				
+				<section class="content-body clearfix">
+					
+					<ul class="member-list">
+						
+						<li class="span4"> <!-- BEGIN MEMBER LISTING -->
+							<div class="member-wrapper clearfix">
+								<ul class="member_details">
+									
+									<li class="member_avatar span6">
+										<a href="#">
+											<img src="http://localhost:100/wp-content/themes/Eris/assets/img/avatar.jpg?" />
+											<span class="badge">Subscriber</span>
+										</a>
+									</li>
+									
+									<li class="member_profile span6">
+										<ul>
+											<li class="member_screen-name"><a href="">TimothySteele</a></li>
+											<li class="member_location">Chicago, IL</li>
+											<li class="member_specialties">
+												Specializes in:
+												<ul>
+													<li><a href="#">Specialty 1</a></li>
+													<li><a href="#">Specialty 2</a></li>
+													<li><a href="#">Specialty 3</a></li>
+												</ul>
+											</li>
+										</ul>
+									</li>
+									
+									<li class="member_posts span12">
+										<ul>
+											<li class="member_last-posted">Last Posted on September 17, 2012</li>
+											<li class="member_answers">8 answers</li>
+											<li class="member_posts">3 blog posts</li>
+											<li class="member_comments">6 comments</li>
+										</ul>
+									</li>
+									
+								</ul> <!-- END MEMBER DETAILS -->
+							</div> <!-- END MEMBER WRAPPER -->
+						</li> <!-- END MEMBER LISTING -->
+						
+						<li class="span4"> <!-- BEGIN MEMBER LISTING -->
+							<div class="member-wrapper clearfix">
+								<ul class="member_details">
+									
+									<li class="member_avatar span6">
+										<a href="#">
+											<img src="http://localhost:100/wp-content/themes/Eris/assets/img/avatar.jpg?" />
+											<span class="badge">Subscriber</span>
+										</a>
+									</li>
+									
+									<li class="member_profile span6">
+										<ul>
+											<li class="member_screen-name"><a href="">TimothySteele</a></li>
+											<li class="member_location">Chicago, IL</li>
+											<li class="member_specialties">
+												Specializes in:
+												<ul>
+													<li><a href="#">Specialty 1</a></li>
+													<li><a href="#">Specialty 2</a></li>
+													<li><a href="#">Specialty 3</a></li>
+												</ul>
+											</li>
+										</ul>
+									</li>
+									
+									<li class="member_posts span12">
+										<ul>
+											<li class="member_last-posted">Last Posted on September 17, 2012</li>
+											<li class="member_answers">8 answers</li>
+											<li class="member_posts">3 blog posts</li>
+											<li class="member_comments">6 comments</li>
+										</ul>
+									</li>
+									
+								</ul> <!-- END MEMBER DETAILS -->
+							</div> <!-- END MEMBER WRAPPER -->
+						</li> <!-- END MEMBER LISTING -->
+						
+						<li class="span4"> <!-- BEGIN MEMBER LISTING -->
+							<div class="member-wrapper clearfix">
+								<ul class="member_details">
+									
+									<li class="member_avatar span6">
+										<a href="#">
+											<img src="http://localhost:100/wp-content/themes/Eris/assets/img/avatar.jpg?" />
+											<span class="badge">Subscriber</span>
+										</a>
+									</li>
+									
+									<li class="member_profile span6">
+										<ul>
+											<li class="member_screen-name"><a href="">TimothySteele</a></li>
+											<li class="member_location">Chicago, IL</li>
+											<li class="member_specialties">
+												Specializes in:
+												<ul>
+													<li><a href="#">Specialty 1</a></li>
+													<li><a href="#">Specialty 2</a></li>
+													<li><a href="#">Specialty 3</a></li>
+												</ul>
+											</li>
+										</ul>
+									</li>
+									
+									<li class="member_posts span12">
+										<ul>
+											<li class="member_last-posted">Last Posted on September 17, 2012</li>
+											<li class="member_answers">8 answers</li>
+											<li class="member_posts">3 blog posts</li>
+											<li class="member_comments">6 comments</li>
+										</ul>
+									</li>
+									
+								</ul> <!-- END MEMBER DETAILS -->
+							</div> <!-- END MEMBER WRAPPER -->
+						</li> <!-- END MEMBER LISTING -->
+						
+					</ul>
+					
+				</section>
+				
+			</section>
+			
+			
+			<section class="span12 featured-members content-container">
+		    <hgroup class="content-header">
+		        <h3>Meet the Community Team</h3>
+		        <h4>Whatever your question or issue, we're here to help</h4>
+		    </hgroup>
+    		<section class="content-body">
+        	<div class="member clearfix">
+        		
+						<div class="crest span2 labeled">
+    					<a href="http://localhost:100/?author=98"><img alt="" class="photo" src="/wp-content/uploads/userphoto/98.jpeg?1345825782"></a>
+        			<h4><a href="http://localhost:100/?author=98">Expert</a></h4>
+    			
+    				</div>
+    			
+	    			<article class="info span8">
+	        		
+	        		<h4><a href="http://localhost:100/?author=98">user</a></h4>
+	        		
+	        		<address>&nbsp;</address>
+	            <h5>Specializes in</h5>
+	            
+	            <ul>
+		            <li><a href="http://localhost:100/category/content-blurb-demo/">Content Blurb Demo</a></li>
+		            <li><a href="http://localhost:100/category/post-a-question/">post-a-question</a></li>
+		            <li><a href="http://localhost:100/category/post-a-question-demo/test-subcat/">Test subcat</a></li>
+	            </ul>
+	          </article>
+	    			<div class="stats">
+	          	<ul>
+	              <li>0 answers</li>
+	              <li>0 posts</li>
+	              <li>1 comment</li>
+	            </ul>
+	          </div>
+					</div>
+				
+					<div class="member clearfix">
+	    
+						<div class="crest span2 labeled">
+	    				<a href="http://localhost:100/author/boobies_rule/"><img alt="" class="photo" src="/7.png?1345662144"></a>
+	    
+	    				<h4><a href="http://localhost:100/author/boobies_rule/"></a></h4>
+	    			</div>
+	    			
+	    			<article class="info span8">
+	        		<h4><a href="http://localhost:100/author/boobies_rule/">boobies_rule</a></h4>
+	        		<address>&nbsp;</address>
+	            <h5>Specializes in</h5>
+	            <ul>
+	              <li><a href="http://localhost:100/category/content-blurb-demo/">Content Blurb Demo</a></li>
+	              <li><a href="http://localhost:100/category/resultlist/">Resultlist</a></li>
+	              <li><a href="http://localhost:100/category/results-list-widget-demo/">Results List Widget Demo</a></li>
+	           	</ul>
+	         	</article>
+	         	
+	    			<div class="stats">
+	            <p>Last posted on <time pubdate="pubdate" datetime="2012-08-22">Aug 22, 2012</time>.</p>
+	            <ul>
+	              <li>0 answers</li>
+	              <li>7 posts</li>
+	              <li>0 comments</li>
+	            </ul>
+	          </div>
+					</div>
+				
+					<div class="member clearfix">
+		    
+						<div class="crest span2 labeled">
+		    			<a href="http://localhost:100/?author=60"><img alt="" class="photo" src="http://localhost:100/wp-content/themes/Eris/assets/img/avatar.jpg?"></a>
+		    			<h4><a href="http://localhost:100/?author=60">Subscriber</a></h4>
+		    		</div>
+		    		
+		    		<article class="info span8">
+		        	<h4><a href="http://localhost:100/?author=60">AGVance</a></h4>
+		        	<address>&nbsp;</address>
+		          <h5>Specializes in</h5>
+		          <ul>
+		            <li><a href="http://localhost:100/category/content-blurb-demo/">Content Blurb Demo</a></li>
+		            <li><a href="http://localhost:100/category/results-list-widget-demo/">Results List Widget Demo</a></li>
+		          </ul>
+		        </article>
+		    		<div class="stats">
+		          <ul>
+		            <li>0 answers</li>
+		            <li>0 posts</li>
+		            <li>0 comments</li>
+		          </ul>
+		        </div>
+					</div>       
+					<span class="left"><a href="http://localhost:100/experts-list/">See More</a></span> 
+    		</section>
+			</section>
+			
+			
+		</section> <!-- END SPAN8 -->
+	
+		<section class="span4">
+			
+			<section class="span12 featured-members content-container">
+				
+				<hgroup class="content-header">
+					<h3>Meet the Community Team</h3>
+		      <h4>Whatever your question or issue, we're here to help</h4>
+				</hgroup>
+				
+				<section class="content-body clearfix">
+					
+					<ul class="member-list">
+						
+						<li class="span4"> <!-- BEGIN MEMBER LISTING -->
+							<div class="member-wrapper clearfix">
+								<ul class="member_details">
+									
+									<li class="member_avatar span6">
+										<a href="#">
+											<img src="http://localhost:100/wp-content/themes/Eris/assets/img/avatar.jpg?" />
+											<span class="badge">Subscriber</span>
+										</a>
+									</li>
+									
+									<li class="member_profile span6">
+										<ul>
+											<li class="member_screen-name"><a href="">TimothySteele</a></li>
+											<li class="member_location">Chicago, IL</li>
+										</ul>
+									</li>
+									
+									<li class="member_posts span12">
+										<ul>
+											<li class="member_last-posted">Last Posted on September 17, 2012</li>
+										</ul>
+									</li>
+									
+								</ul> <!-- END MEMBER DETAILS -->
+							</div> <!-- END MEMBER WRAPPER -->
+						</li> <!-- END MEMBER LISTING -->
+						
+						<li class="span4"> <!-- BEGIN MEMBER LISTING -->
+							<div class="member-wrapper clearfix">
+								<ul class="member_details">
+									
+									<li class="member_avatar span6">
+										<a href="#">
+											<img src="http://localhost:100/wp-content/themes/Eris/assets/img/avatar.jpg?" />
+											<span class="badge">Subscriber</span>
+										</a>
+									</li>
+									
+									<li class="member_profile span6">
+										<ul>
+											<li class="member_screen-name"><a href="">TimothySteele</a></li>
+											<li class="member_location">Chicago, IL</li>
+										</ul>
+									</li>
+									
+									<li class="member_posts span12">
+										<ul>
+											<li class="member_last-posted">Last Posted on September 17, 2012</li>
+									</li>
+									
+								</ul> <!-- END MEMBER DETAILS -->
+							</div> <!-- END MEMBER WRAPPER -->
+						</li> <!-- END MEMBER LISTING -->
+						
+						<li class="span4"> <!-- BEGIN MEMBER LISTING -->
+							<div class="member-wrapper clearfix">
+								<ul class="member_details">
+									
+									<li class="member_avatar span6">
+										<a href="#">
+											<img src="http://localhost:100/wp-content/themes/Eris/assets/img/avatar.jpg?" />
+											<span class="badge">Subscriber</span>
+										</a>
+									</li>
+									
+									<li class="member_profile span6">
+										<ul>
+											<li class="member_screen-name"><a href="">TimothySteele</a></li>
+											<li class="member_location">Chicago, IL</li>
+										</ul>
+									</li>
+									
+									<li class="member_posts span12">
+										<ul>
+											<li class="member_last-posted">Last Posted on September 17, 2012</li>
+										</ul>
+									</li>
+									
+								</ul> <!-- END MEMBER DETAILS -->
+							</div> <!-- END MEMBER WRAPPER -->
+						</li> <!-- END MEMBER LISTING -->
+						
+					</ul>
+					
+				</section>
+				
+			</section>
+			
+			
+			
+			
+			<section class="span12 featured-members content-container">
+		    
+		    <hgroup class="content-header">
+		        <h3>Meet the Community Team</h3>
+		        <h4>Whatever your question or issue, we're here to help</h4>
+		    </hgroup>
+    
+		    <section class="content-body">
+					<div class="member clearfix">
+		    
+		    		<div class="crest span3 labeled">
+		    			<a href="http://localhost:100/?author=98"><img alt="" class="photo" src="/wp-content/uploads/userphoto/98.jpeg?1345825782"></a>
+		    			<h4><a href="http://localhost:100/?author=98">Expert</a></h4>
+		    		</div>
+		    		
+		    		<article class="info span8">
+		        	<h4><a href="http://localhost:100/?author=98">user</a></h4>
+		        	<address>&nbsp;</address>
+		        </article>
+		    		
+		    		<div class="stats"></div>
+		    		
+					</div>
+					<div class="member clearfix">
+		    
+						<div class="crest span2 labeled">
+		    			<a href="http://localhost:100/?author=60"><img alt="" class="photo" src="http://localhost:100/wp-content/themes/Eris/assets/img/avatar.jpg?"></a>
+		    
+		    			<h4><a href="http://localhost:100/?author=60">Subscriber</a></h4>
+		    		</div>
+		    		<article class="info span8">
+		        	<h4><a href="http://localhost:100/?author=60">AGVance</a></h4>
+		        	<address>&nbsp;</address>
+		        </article>
+		    		<div class="stats"></div>
+		    	</div>
+		    	<div class="member clearfix">
+		    
+						<div class="crest span2 labeled">
+		    			<a href="http://localhost:100/author/boobies_rule/"><img alt="" class="photo" src="/7.png?1345662144"></a>
+		    
+		    			<h4><a href="http://localhost:100/author/boobies_rule/"></a></h4>
+		    		</div>
+		    		<article class="info span8">
+		        	<h4><a href="http://localhost:100/author/boobies_rule/">boobies_rule</a></h4>
+		        	<address>&nbsp;</address>
+		        </article>
+		    		<div class="stats">
+		        	<p>Last posted on <time pubdate="pubdate" datetime="2012-08-22">Aug 22, 2012</time>.</p>
+		        </div>
+					</div>       
+					<span class="left"><a href="http://localhost:100/experts-list/">See More</a></span> 
+		    </section>
+			</section>
+			
+		</section>
+		
+	</section>
+	
+<?php
+get_template_part('parts/footer');
+
+?>
