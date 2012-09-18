5f776e25f22f8079e57ee56b1616f81b35cdc668
Eddie Moya <eddie.moya@gmail.com>
2012-09-18 00:59:04 -0500

    featured widget: question template

---
+++ widgets/featured-post/question.php
@@ -0,0 +1,47 @@
+<?php $c = get_the_category(); ?>
+
+<section class="content-body clearfix">
+
+    <?php if (is_widget('show_category') || is_widget('show_date')) : ?>
+        <section class="content-details clearfix">
+
+            <?php if (is_widget('show_category')) : $c = get_the_category(); ?>
+                <span class="content-category">
+                    <a href="<?php echo get_category_link($c[0]); ?>" title="<?php echo $c[0]->name; ?>">
+                        <?php echo $c[0]->cat_name; ?>
+                    </a>
+                </span>
+            <?php endif; ?>
+
+            <?php if(is_widget('show_date')) : ?>
+                <time class="content-date" pubdate datetime="<?php the_time('Y-m-d'); ?>">
+                    <?php the_time('F j, Y'); ?>
+                </time>
+            <?php endif; ?>
+
+        </section> <!-- content-details -->
+
+    <?php endif; //is_widget->show_category, show_date ?>
+    <?php //get_partial( 'parts/space_date_time', array( "timestamp" => get_the_time('U'), "remove_hms" => true ) ); ?>
+
+    <?php if (is_widget('show_thumbnail') && function_exists('get_category_image_url') &&  $image = get_category_image_url((int)$c[0]->term_id, false)) : ?>
+        <div class="category-image">
+            <img src="<?php echo $image; ?>" />
+        </div>
+    <?php endif; ?>
+
+    <h1 class="content-headline">
+        Q: <a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a>
+    </h1>
+
+    <?php if(is_widget('show_comment_count')): ?>
+        <ul class="clearfix">
+            <li class="content-comments"><?php custom_comment_count('answer'); ?> answers</li>
+        </ul>
+    <?php endif; ?>
+
+
+    <section class="post-actions">
+        <?php get_partial( 'parts/share', array( "version" => is_widget('share_style'), "url" => get_post_permalink( $post->ID ) ) ); ?>
+    </section>
+</section>
