4f1d2e2ef5cb6cf8f682ae8b08ff7ae8fa7335d4
jcorradino <jcorra4@searshc.com>
2012-09-20 15:58:13 -0500

    adding some basic caching to the sitemap magic

--- wordpress/wp-content/plugins/simple-multisite-sitemaps/sitemap-template.php
+++ wordpress/wp-content/plugins/simple-multisite-sitemaps/sitemap-template.php
@@ -30,35 +30,86 @@
 |                                                                    |
 \--------------------------------------------------------------------/
 */
-header( 'HTTP/1.0 200 OK' );
-header( 'Content-Type: text/xml; charset=' . get_option( 'blog_charset' ), true );
-echo '<?xml version="1.0" encoding="'.get_option( 'blog_charset' ).'"?'.'>'; 
-?>
-
-<urlset	xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
-	    xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd"
-	    xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-
-<?php 
 global $blog_id;
-
-$query_args = array(
-	'post_type'     => array( 'post', 'page', 'question', 'guide' ),
-	'post_status'   => 'publish',
-	'orderby'       => 'date',
-	'showposts'     => -1
-);
-query_posts( $query_args );
-
-if ( have_posts()) : while (have_posts() ) : the_post();
+if($_GET["refresh"] == true) :
+	ob_start();
+	echo '<?xml version="1.0" encoding="'.get_option( 'blog_charset' ).'"?'.'>'; 
 ?>
-	<url>
-		<loc><?php echo get_blog_permalink( $blog_id, $post->ID ); ?></loc> 
-		<lastmod><?php echo mysql2date( 'Y-m-d\TH:i:s+00:00', get_post_modified_time('Y-m-d H:i:s', true), false ); ?></lastmod> 
-		<changefreq>weekly</changefreq> 
-		<priority>0.6</priority>
-	</url>
+
+	<urlset	xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+		    xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd"
+		    xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+
 <?php 
-endwhile; endif;
-?> 
-</urlset>
+	$alinks = array(
+		'post'     => site_url('posts'),
+		'question' => get_post_type_archive_link('question'),
+		'guide'    => get_post_type_archive_link('guide')
+	);
+	$terms = array(
+		"question" => get_terms_by_post_type('category', 'question'),
+		"post"     => get_terms_by_post_type('category', 'post'),
+		"guide"    => get_terms_by_post_type('category', 'guide'),
+	);
+	$labels = array (
+		'question' => "Q&A's",
+		'post'     => 'Blog Posts',
+		'guide'    => 'Guides'
+	);
+
+	$query_args = array(
+		'post_type'   => array( 'post', 'page', 'guide', 'question' ),
+		'post_status' => 'publish',
+		'orderby'     => 'date',
+		'showposts'		=> -1
+	);
+
+	query_posts( $query_args );
+
+	foreach(get_categories() as $category) :
+		$url = get_category_link($category->term_id);
+?>
+		<url>
+			<loc><?php echo $url; ?></loc>
+			<changefreq>weekly</changefreq> 
+			<priority>0.6</priority>
+		</url>
+<?php
+	endforeach;
+
+	foreach($labels as $post_type => $label) :
+		foreach($terms[$post_type] as $term) :
+			$url = esc_url( get_category_link($term->term_id) ).$post_type;
+?>
+				<url>
+					<loc><?php echo $url; ?></loc>
+					<changefreq>weekly</changefreq> 
+					<priority>0.6</priority>
+				</url>
+<?php
+		endforeach;
+	endforeach;
+
+	if ( have_posts()) : 
+		while (have_posts() ) : the_post();
+?>
+				<url>
+					<loc><?php echo (function_exists("get_blog_permalink") && get_blog_permalink( $blog_id, $post->ID )) ? get_blog_permalink( $blog_id, $post->ID ) : get_permalink($post->ID); ?></loc> 
+					<lastmod><?php echo mysql2date( 'Y-m-d\TH:i:s+00:00', get_post_modified_time('Y-m-d H:i:s', true), false ); ?></lastmod> 
+					<changefreq>weekly</changefreq> 
+					<priority>0.6</priority>
+				</url>
+<?php 
+		endwhile; 
+	endif; 
+?>
+	</urlset>
+<?php
+	$sitemap = ob_get_clean();
+	ob_end_clean();
+	file_put_contents(plugin_dir_path(__FILE__).$blog_id."sitemap.xml", $sitemap);
+else:
+	header( 'HTTP/1.0 200 OK' );
+	header( 'Content-Type: text/xml; charset=' . get_option( 'blog_charset' ), true );
+	echo file_get_contents(plugin_dir_path(__FILE__).$blog_id."sitemap.xml");
+endif;
