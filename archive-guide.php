9e8012d4db71bf61d9572298b422f87262d2cfb6
Eddie Moya <eddie.moya@gmail.com>
2012-09-20 12:23:47 -0500

    fixing post type archive sections

--- archive-guide.php
+++ archive-guide.php
@@ -1,30 +1,24 @@
 <?php get_template_part('parts/header'); ?>
 
-<ul class="span8">
-<?php
-    if (have_posts()) :
-        while (have_posts()) :
-            get_partial("widgets/featured-post/single", array("is_widget_override" => true));
-        endwhile;
-    endif;
-?>
-</ul>
+<section class="dropzones span12">
 
-<ul class="span4 widget_dropzone_widget dropzone">
-	<?php 
-		if(!dynamic_sidebar('Posts Sidebar')){
-	
-			$instance = array('title' => 'About', 'text' => '<p><strong>Buying Guides</strong> give you the inside scoop on all types of products. use them to make the best purchase every single time.</p>');
-			the_widget('Content_Blurb', $instance, $sidebar_args);
+	<section class="span8 widget_dropzone_widget dropzone">
+		<section class="dropzone-inner-wrapper">
+			<?php if(!dynamic_sidebar('Buying Guide Sidebar')) : ?> 
 
-			$instance = array('nav_menu' => $nav_menus[6]->term_id, 'title' => 'First Time Here?', 'sub-title' => "Here's our best stuff");
-			the_widget('Communities_Menu_Widget', $instance, $sidebar_args);
+			<?php endif; ?> 
+		</section>
+	</section>
 
-			$instance = array('nav_menu' => $nav_menus[7]->term_id, 'title' => 'Related Stories');
-			the_widget('Communities_Menu_Widget', $instance, $sidebar_args);
+	<section class="span4 widget_dropzone_widget dropzone">
+		<section class="dropzone-inner-wrapper">
+			<?php if(!dynamic_sidebar('Buying Guide Sidebar')) : ?>
 
-		 }
-	?>
-</ul>
+			<?php endif; ?>
+		</section>
+	</section>
+
+
+</section>
 
 <?php get_template_part('parts/footer'); ?>
--- archive-question.php
+++ archive-question.php
@@ -1,42 +1,24 @@
 <?php get_template_part('parts/header'); ?>
 
-<?php 
-	global $post_question_data;
-	$data = $post_question_data;
-	
-	$nav_menus = get_terms( 'nav_menu', array( 'hide_empty' => true ) );
-	$sidebar_args = array(
-	    'before_widget' => '<article class="span12 content-container widget dropzone-widget sidebar-widget widget %2$s" id="%1$s">',
-	    'after_widget' => '</article>',
-	    'before_title' => '<header class="content-header"><h3 class="widgettitle">',
-	    'after_title' => '</h3></header>'
-	);
- ?>
+<section class="dropzones span12">
 
-<section class="span8 widget_dropzone_widget dropzone">
-	<?php if(!dynamic_sidebar('Q&A Content Area')) : ?> 
+	<section class="span8 widget_dropzone_widget dropzone">
+		<section class="dropzone-inner-wrapper">
+			<?php if(!dynamic_sidebar('Q&A Content Area')) : ?> 
 
-		<article class="span12 widget_post_a_question content-container widget post-your-question">
-			<?php include ( get_template_directory().'/parts/forms/post-a-question-step-'.$data['step'].'.php' ); ?>
-		</article>
+			<?php endif; ?> 
+		</section>
+	</section>
 
-		<article class="span12 widget_results-list content-container widget results-list">
-			<?php get_template_part('widgets/results-list/archive'); ?>
-		</article> 
+	<section class="span4 widget_dropzone_widget dropzone">
+		<section class="dropzone-inner-wrapper">
+			<?php if(!dynamic_sidebar('Q&A Sidebar')) : ?>
 
-	<?php endif; ?> 
+			<?php endif; ?>
+		</section>
+	</section>
 
-</section>
 
-<section class="span4 widget_dropzone_widget dropzone">
-	<?php 
-		if(!dynamic_sidebar('Q&A Sidebar')){
-
-			$instance = array('nav_menu' => $nav_menus[3]->term_id, 'title' => 'Related Links');
-			the_widget('Communities_Menu_Widget', $instance, $sidebar_args);
-
-		 }
-	?>
 </section>
 
 <?php get_template_part('parts/footer'); ?>
