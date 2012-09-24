b2b994777649bdef55beedb9f5be2abf7c5e6c20
Tim Steele <tsteel5@searshc.com>
2012-09-24 12:30:40 -0500

    Fuck you wp-config

--- wordpress/wp-config.php
+++ wordpress/wp-config.php
@@ -13,37 +13,37 @@
  *
  * @package WordPress
  */	
-	// /** The name of the database for WordPress */
-	// define('DB_CHARSET', 'utf8');
-// 	
-	// /** The name of the database for WordPress */
-	// define('DB_NAME', 'communities');
-// 	
-	// /** MySQL database username */
-	// define('DB_USER', 'root');
-// 	
-	// /** MySQL database password */
-	// define('DB_PASSWORD', 'tim97669');
-// 	
-	// /** MySQL hostname */
-	// define('DB_HOST', '127.0.0.1:3306');
-// 	
+	/** The name of the database for WordPress */
+	define('DB_CHARSET', 'utf8');
 	
 	/** The name of the database for WordPress */
 	define('DB_NAME', 'communities');
 	
 	/** MySQL database username */
-	define('DB_USER', 'communities_dba');
+	define('DB_USER', 'root');
 	
 	/** MySQL database password */
-	define('DB_PASSWORD', 'password1');
+	define('DB_PASSWORD', 'tim97669');
 	
 	/** MySQL hostname */
-	define('DB_HOST', 'barable.com');
-	// define('WP_ALLOW_MULTISITE', true);
+	define('DB_HOST', '127.0.0.1:3306');
+	
 	
-	/** Database Charset to use in creating database tables. */
-	define('DB_CHARSET', 'utf8');
+	// /** The name of the database for WordPress */
+	// define('DB_NAME', 'communities');
+// 	
+	// /** MySQL database username */
+	// define('DB_USER', 'communities_dba');
+// 	
+	// /** MySQL database password */
+	// define('DB_PASSWORD', 'password1');
+// 	
+	// /** MySQL hostname */
+	// define('DB_HOST', 'barable.com');
+	// // define('WP_ALLOW_MULTISITE', true);
+// 	
+	// /** Database Charset to use in creating database tables. */
+	// define('DB_CHARSET', 'utf8');
 	
 /**#@+
  * Authentication Unique Keys and Salts.
