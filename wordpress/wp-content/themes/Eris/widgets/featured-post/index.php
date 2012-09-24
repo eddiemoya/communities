6d014482f8f12d8417bfa5c26d5330479da51cb2
jcorradino <jcorra4@searshc.com>
2012-09-24 17:12:41 -0500

    fixing expert counter

--- wordpress/wp-content/themes/Eris/widgets/featured-post/index.php
+++ wordpress/wp-content/themes/Eris/widgets/featured-post/index.php
@@ -8,7 +8,7 @@
     if (have_posts()) { while (have_posts()) { 
     the_post(); 
     $answer_count = (function_exists('get_custom_comment_count')) ? get_custom_comment_count('answer') : '';
-    $expert_count = (function_exists('get_expert_comment_count')) ? get_expert_comment_count($post->ID) : '';
+    $expert_count = (function_exists('get_expert_comment_count')) ? get_expert_comment_count(get_the_ID()) : '';
     $answer_count = $answer_count - $expert_count;
     $answer_count_string = ($answer_count > 500) ? "500+ answers" : $answer_count . " " . _n( ' answer', ' answers', $answer_count );
     $expert_count_string = ($expert_count > 500) ? "500+ community team answers" : $expert_count . " " . _n( ' community team answer', ' community team answers', $expert_count );    
