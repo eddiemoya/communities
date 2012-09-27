2a464e1b66203251d8dec77e61268f41a5a1275e
Eddie Moya <eddie.moya@gmail.com>
2012-09-27 15:42:20 -0500

    removing results list class from search page results list

--- wordpress/wp-content/themes/Eris/search.php
+++ wordpress/wp-content/themes/Eris/search.php
@@ -6,7 +6,7 @@
 
 get_template_part('parts/header'); ?>
 
-<article class="span12 widget_results-list content-container widget results-list">
+<article class="span12 content-container widget">
 	<?php get_template_part('widgets/results-list/archive'); ?>
 </article> 
 
