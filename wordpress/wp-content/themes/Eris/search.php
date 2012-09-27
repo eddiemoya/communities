932f6937ab9d915bc2bc722b67fc8bdc9cd7e5df
Eddie Moya <eddie.moya@gmail.com>
2012-09-27 17:18:25 -0500

    changing the way search results list has the class applied; making it so pagiation on results list only happens iif have_posts()

--- wordpress/wp-content/themes/Eris/search.php
+++ wordpress/wp-content/themes/Eris/search.php
@@ -3,10 +3,11 @@
  * @package WordPress
  * @subpackage White Label
  */
+ $class = (have_posts()) ? "results-list widgets_results-list" : "";
 
 get_template_part('parts/header'); ?>
 
-<article class="span12 content-container widget">
+<article class="span12 content-container widget <?php echo $class; ?>">
 	<?php get_template_part('widgets/results-list/archive'); ?>
 </article> 
 
