71ff23d3229246604dfe25bfd52888387b119a64
Tim Steele <tsteel5@searshc.com>
2012-09-28 12:29:30 -0500

    Fix HR issue

--- wordpress/wp-config.php
+++ wordpress/wp-config.php
@@ -14,29 +14,7 @@
  * @package WordPress
  */
 
-if(USE_LOCAL_DB) {
-	
-	/** The name of the database for WordPress */
-	define('DB_CHARSET', 'utf8');
-	
-	/** The name of the database for WordPress */
-	define('DB_NAME', 'comm_local');
-	
-	/** MySQL database username */
-	define('DB_USER', 'wp_user');
-	
-	/** MySQL database password */
-	define('DB_PASSWORD', 'philos10');
-	
-	/** MySQL hostname */
-	// define('WP_ALLOW_MULTISITE', true);
-	define('DB_HOST', 'localhost');
-	
-	/** Database Charset to use in creating database tables. */
-	define('DB_CHARSET', 'utf8');
 
-	
-} else {
 	/** The name of the database for WordPress */
 	define('DB_NAME', 'communities');
 	
@@ -52,7 +30,6 @@ if(USE_LOCAL_DB) {
 	
 	/** Database Charset to use in creating database tables. */
 	define('DB_CHARSET', 'utf8');
-}
 	
 /**#@+
  * Authentication Unique Keys and Salts.
