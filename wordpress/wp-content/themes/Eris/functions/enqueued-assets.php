9eec6a0b6729807b99f6e2bb1984882688dba8a7
Tim Steele <tsteel5@searshc.com>
2012-09-28 09:08:51 -0500

    Omniture script adding

--- wordpress/wp-content/themes/Eris/functions/enqueued-assets.php
+++ wordpress/wp-content/themes/Eris/functions/enqueued-assets.php
@@ -39,7 +39,7 @@ function enqueue_scripts() {
         wp_register_script('transFormer', get_template_directory_uri() . '/assets/js/widgets/shcJSL.transFormer.js', array(), '1.0');
         wp_register_script('flagger', get_template_directory_uri() . '/assets/js/widgets/shcJSL.flagger.js', array(), '1.0');
         wp_register_script('addthis', 'http://s7.addthis.com/js/250/addthis_widget.js', array(), '1.0');
-				
+        wp_register_script('omniture', get_template_directory_uri() . '/assets/js/vendor/omniture.' . theme_option("brand") . 'sCode.js', array(), '1.0');
 				// NOT FOR PRODUCTION
 				//wp_register_script('debug', get_template_directory_uri() . '/assets/js/vendor/debug.js', array(), '1.0');
 
@@ -55,6 +55,7 @@ function enqueue_scripts() {
         wp_enqueue_script('transFormer');
         wp_enqueue_script('flagger');
         wp_enqueue_script('addthis');
+        wp_enqueue_script('omniture');
 
 		wp_localize_script('jquery', 'ajaxdata', $data);		
         
