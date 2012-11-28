c71979e8bd00005ce6beb76fdcbb02b770c73b19
Dan Crimmins <dbcrimmins@gmail.com>
2012-11-28 11:44:42 -0600

    more stuff

--- wordpress/wp-content/themes/Eris/functions/general.php
+++ wordpress/wp-content/themes/Eris/functions/general.php
@@ -5,7 +5,19 @@
         echo $option['uastring'];
     }
     
-    
+
+/**
+ * select_it() - This function is used exclusively with WP Polls. It accepts input string of label and 
+ * checkbox or radio button as used and formatted in polls and adds the checked attribute to the input.
+ * 
+ * @param string $input
+ * @return string - input with the checked attribute added.
+ * @author Dan Crimmins
+ * 
+ * @see comm_display_pollvote() -- located in filters-hooks.php. This is the only file where this function
+ * is being used.
+ * 
+ */
 function select_it($input) {
 	
 	$pos = (strrpos($input, '">') !== false) ? (strrpos($input, '">') + 1) : false;
@@ -20,7 +32,3 @@ function select_it($input) {
 	return $input;
 }
 
-function is_selected($poll_cookie) {
-	
-	
-}
