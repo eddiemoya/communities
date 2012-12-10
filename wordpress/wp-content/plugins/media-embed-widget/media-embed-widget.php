da2e323e0f73153958b0d3d7e3a88cc35fb3a176
Jason Corradino <jason@ididntbreak.it>
2012-12-10 12:42:42 -0600

    adding a media widget

---
+++ wordpress/wp-content/plugins/media-embed-widget/media-embed-widget.php
@@ -0,0 +1,352 @@
+<?php
+/*
+Plugin Name: Media Embed Widget
+Description: Handles oEmbed items on section fronts
+Version: 0.1
+Author: Jason Corradino, Eddie Moya
+Author URI: http://ididntbreak.it, http://eddiemoya.com/
+ */
+
+/**
+ * IMPORTANT: Change the class name for each widget
+ */    
+class Media_Embed_Widget extends WP_Widget {
+      
+    /**
+     * Name for this widget type, should be human-readable - the actual title it will go by.
+     * 
+     * @var string [REQUIRED]
+     */
+    var $widget_name = 'Media Widget';
+   
+    /**
+     * Root id for all widgets of this type. Will be automatically generate if not set.
+     * 
+     * @var string [OPTIONAL]. FALSE by default.
+     */
+    var $id_base = 'Media_Embed_Widget';
+    
+    /**
+     * Shows up under the widget in the admin interface
+     * 
+     * @var string [OPTIONAL]
+     */
+    private $description = 'Media Embed Widget';
+
+    /**
+     * CSS class used in the wrapping container for each instance of the widget on the front end.
+     * 
+     * @var string [OPTIONAL]
+     */
+    private $classname = 'media-embed';
+    
+    /**
+     *
+     * @var type 
+     */
+    private $width = '250';
+    
+    
+    /**
+     * Never used - does nothing.
+     * @var type 
+     */
+    private $height = '200';
+    
+    
+    /**
+     * Be careful to consider PHP versions. If running PHP4 class name as the contructor instead.
+     * 
+     * @author Eddie Moya
+     * @return void
+     */
+    public function Media_Embed_Widget(){
+        $widget_ops = array(
+            'description' => $this->description,
+            'classname' => $this->classname
+        );
+        
+        $control_options = array(
+            'height' => $this->height,
+            'width' => $this->width
+        );
+
+        parent::WP_Widget($this->id_base, $this->widget_name, $widget_ops, $control_options);
+    }
+    
+    /**
+     * Self-registering widget method.
+     * 
+     * This can be called statically.
+     * 
+     * @author Eddie Moya
+     * @return void
+     */
+    public function register_widget() {
+        add_action('widgets_init', create_function( '', 'register_widget("' . __CLASS__ . '");' ));
+    }
+    
+    /**
+     * The front end of the widget. 
+     * 
+     * Do not call directly, this is called internally to render the widget.
+     * 
+     * @author [Widget Author Name]
+     * 
+     * @param array $args       [Required] Automatically passed by WordPress - Settings defined when registering the sidebar of a theme
+     * @param array $instance   [Required] Automatically passed by WordPress - Current saved data for the widget options.
+     * @return void 
+     */
+    public function widget($args, $instance) {
+        global $wp_query;
+        
+		echo $instance["code"];
+
+        echo $after_widget;
+    }
+
+    /**
+     *
+     * @param type $tag
+     * @param type $class
+     * @return type 
+     */
+    function add_class($string, $class) {
+        return str_replace('$span', $class, $string);
+        
+    }
+
+    /**
+     * Data validation. 
+     * 
+     * Do not call directly, this is called internally to render the widget
+     * 
+     * @author [Widget Author Name]
+     * 
+     * @uses esc_attr() http://codex.wordpress.org/Function_Reference/esc_attr
+     * 
+     * @param array $new_instance   [Required] Automatically passed by WordPress
+     * @param array $old_instance   [Required] Automatically passed by WordPress
+     * @return array|bool Final result of newly input data. False if update is rejected.
+     */
+    public function update($new_instance, $old_instance){
+        
+        /* Lets inherit the existing settings */
+        $instance = $old_instance;
+
+        /**
+         * Sanitize each option - be careful, if not all simple text fields,
+         * then make use of other WordPress sanitization functions, but also
+         * make use of PHP functions, and use logic to return false to reject
+         * the entire update. 
+         * 
+         * @see http://codex.wordpress.org/Function_Reference/esc_attr
+         */
+        foreach($new_instance as $key => $value){
+			if ($key != "code")
+            	$instance[$key] = esc_attr($value);
+            
+        }
+        
+        //Handle unchecked checkboxes
+        foreach($instance as $key => $value){
+            if($value == 'on' && !isset($new_instance[$key])){
+                $instance[$key] = '';
+            }
+
+        }
+
+		if ($instance["url"] != $old_instance["url"]) {
+			$instance["code"] = wp_oembed_get($instance["url"]);
+			$instance['video_tiny'] = wp_oembed_get($instance["url"], array('width'=>225));
+		}
+	
+        return $instance;
+    }
+    
+    /**
+     * Generates the form for this widget, in the WordPress admin area.
+     * 
+     * The use of the helper functions form_field() and form_fields() is not
+     * neccessary, and may sometimes be inhibitive or restrictive.
+     * 
+     * @author Eddie Moya
+     * 
+     * @uses wp_parse_args() http://codex.wordpress.org/Function_Reference/wp_parse_args
+     * @uses self::form_field()
+     * @uses self::form_fields()
+     * 
+     * @param array $instance [Required] Automatically passed by WordPress
+     * @return void 
+     */
+    public function form($instance){
+        
+        /* Setup default values for form fields - associtive array, keys are the field_id's */
+        $defaults = array(
+            'title' => '', 
+            'style' => 'normal',
+            'show_title' => 'on',
+            'widget_name' => $this->classname
+            );
+        
+        /* Merge saved input values with default values */
+        $instance = wp_parse_args($instance, $defaults);
+       
+        $this->form_field('title', 'text', 'Title', $instance);
+
+		echo $instance["video_tiny"];
+
+        $fields = array(
+            array(
+                'field_id' => 'url',
+                'type' => 'text',
+                'label' => 'URL'
+            )
+        );
+        $this->form_fields($fields, $instance, true);
+    }
+    
+
+    /**
+     * Helper function - does not need to be part of widgets, this is custom, but 
+     * is helpful in generating multiple input fields for the admin form at once. 
+     * 
+     * This is a wrapper for the singular form_field() function.
+     * 
+     * @author Eddie Moya
+     * 
+     * @uses self::form_fields()
+     * 
+     * @param array $fields     [Required] Nested array of field settings
+     * @param array $instance   [Required] Current instance of widget option values.
+     * @return void
+     */
+    private function form_fields($fields, $instance, $group = false){
+        
+        if($group) {
+            echo "<p>";
+        }
+            
+        foreach($fields as $field){
+            
+            extract($field);
+            $label = (!isset($label)) ? null : $label;
+            $options = (!isset($options)) ? null : $options;
+            $this->form_field($field_id, $type, $label, $instance, $options, $group);
+        }
+        
+        if($group){
+             echo "</p>";
+        }
+    }
+    
+    /**
+     * Helper function - does not need to be part of widgets, this is custom, but 
+     * is helpful in generating single input fields for the admin form at once. 
+     *
+     * @author Eddie Moya
+     * 
+     * @uses get_field_id() (No Codex Documentation)
+     * @uses get_field_name() http://codex.wordpress.org/Function_Reference/get_field_name
+     * 
+     * @param string $field_id  [Required] This will be the CSS id for the input, but also will be used internally by wordpress to identify it. Use these in the form() function to set detaults.
+     * @param string $type      [Required] The type of input to generate (text, textarea, select, checkbox]
+     * @param string $label     [Required] Text to show next to input as its label.
+     * @param array $instance   [Required] Current instance of widget option values. 
+     * @param array $options    [Optional] Associative array of values and labels for html Option elements.
+     * 
+     * @return void
+     */
+    private function form_field($field_id, $type, $label, $instance, $options = array(), $group = false){
+  
+        if(!$group)
+             echo "<p>";
+            
+        $input_value = (isset($instance[$field_id])) ? $instance[$field_id] : '';
+        switch ($type){
+            
+            case 'text': ?>
+            
+                    <label for="<?php echo $this->get_field_id( $field_id ); ?>"><?php echo $label; ?>: </label>
+                    <input type="text" id="<?php echo $this->get_field_id( $field_id ); ?>" class="widefat" style="<?php echo (isset($style)) ? $style : ''; ?>" class="" name="<?php echo $this->get_field_name( $field_id ); ?>" value="<?php echo $input_value; ?>" />
+                <?php break;
+            
+            case 'select': ?>
+                    <label for="<?php echo $this->get_field_id( $field_id ); ?>"><?php echo $label; ?>: </label>
+                    <select id="<?php echo $this->get_field_id( $field_id ); ?>" class="widefat" name="<?php echo $this->get_field_name($field_id); ?>">
+                        <?php
+                            foreach ( $options as $value => $label ) :  ?>
+                        
+                                <option value="<?php echo $value; ?>" <?php selected($value, $input_value) ?>>
+                                    <?php echo $label ?>
+                                </option><?php
+                                
+                            endforeach; 
+                        ?>
+                    </select>
+                    
+				<?php break;
+                
+            case 'textarea':
+                
+                $rows = (isset($options['rows'])) ? $options['rows'] : '16';
+                $cols = (isset($options['cols'])) ? $options['cols'] : '20';
+                
+                ?>
+                    <label for="<?php echo $this->get_field_id( $field_id ); ?>"><?php echo $label; ?>: </label>
+                    <textarea class="widefat" rows="<?php echo $rows; ?>" cols="<?php echo $cols; ?>" id="<?php echo $this->get_field_id($field_id); ?>" name="<?php echo $this->get_field_name($field_id); ?>"><?php echo $input_value; ?></textarea>
+                <?php break;
+            
+            case 'radio' :
+                /**
+                 * Need to figure out how to automatically group radio button settings with this structure.
+                 */
+                ?>
+                    
+                <?php break;
+            
+
+            case 'hidden': ?>
+                    <input id="<?php echo $this->get_field_id( $field_id ); ?>" type="hidden" style="<?php echo (isset($style)) ? $style : ''; ?>" class="widefat" name="<?php echo $this->get_field_name( $field_id ); ?>" value="<?php echo $input_value; ?>" />
+                <?php break;
+
+            
+            case 'checkbox' : ?>
+                    <input type="checkbox" class="checkbox" id="<?php echo $this->get_field_id($field_id); ?>" name="<?php echo $this->get_field_name($field_id); ?>"<?php checked( (!empty($instance[$field_id]))); ?> />
+                	<label for="<?php echo $this->get_field_id( $field_id ); ?>"><?php echo $label; ?></label>
+                <?php
+        }
+        
+        if(!$group)
+             echo "</p>";
+            
+       
+    }
+
+	private function get_wp_base_path()
+	{
+	    $base = dirname(__FILE__);
+	    $path = false;
+
+	    if (@file_exists(dirname(dirname($base))."/wp-config.php"))
+	    {
+	        $path = dirname(dirname($base));
+	    }
+	    else
+	    if (@file_exists(dirname(dirname(dirname($base)))."/wp-config.php"))
+	    {
+	        $path = dirname(dirname(dirname($base)));
+	    }
+	    else
+	    $path = false;
+
+	    if ($path != false)
+	    {
+	        $path = str_replace("\\", "/", $path);
+	    }
+	    return $path;
+	}
+}
+
+Media_Embed_Widget::register_widget();
+
