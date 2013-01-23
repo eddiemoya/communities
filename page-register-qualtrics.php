0bf96c652a07914aaee592d57e923aaddaac1898
Tim Steele <tsteel5@searshc.com>
2013-01-23 15:20:56 -0600

    Finish up Qualtrics registration page

--- page-register-qualtrics.php
+++ page-register-qualtrics.php
@@ -32,7 +32,7 @@ $message = array(
 			<p>
 				Thank you for your valuable input&ndash;and your business.
 			</p>
-			<p>
+			<p class='signature'>
 				Sears Customer Experience Team
 			</p>",
 		"medium" => 
@@ -44,7 +44,7 @@ $message = array(
 			<p>
 				Thank you for your time&ndash;and your business.
 			</p>
-			<p>
+			<p class='signature'>
 				Sears Customer Experience Team
 			</p>",
 			"high" =>
@@ -56,7 +56,7 @@ $message = array(
 			<p>
 				Thank you for your valuable input&ndash;and your business.
 			</p>
-			<p>
+			<p class='signature'>
 				Sears Customer Experience Team
 			</p>"),
 	"kmart" => array(
@@ -69,7 +69,7 @@ $message = array(
 			<p>
 				Thank you for your valuable input&ndash;and your business.
 			</p>
-			<p>
+			<p class='signature'>
 				Kmart Customer Experience Team
 			</p>",
 		"medium" => 
@@ -81,7 +81,7 @@ $message = array(
 			<p>
 				Thank you for your time&ndash;and your business.
 			</p>
-			<p>
+			<p class='signature'>
 				Kmart Customer Experience Team
 			</p>",
 			"high" =>
@@ -93,35 +93,19 @@ $message = array(
 			<p>
 				Thank you for your valuable input&ndash;and your business.
 			</p>
-			<p>
+			<p class='signature'>
 				Kmart Customer Experience Team
 			</p>")
 );
 ?>
-
-<style type="text/css">
-	#content .qualtics-alert .content-body {
-		border:solid 10px;
-	}
-	
-	.qualtics-alert .content-body img {
-		float:left;
-	}
-	
-	.sears #content .qualtics-alert .content-body {
-		border-color:#0297d6;
-	}
-	
-	.kmart #content .qualtics-alert .content-body {
-		border-color:#b10f1e;
-	}
-</style>
 
 <?php get_template_part('parts/header'); ?>
     
     <section class="span12">
         
       <article class="widget content-container qualtics-alert span12">
+				
+				<link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri(); ?>/assets/css/qualtrics.css" />
 				
 				<section class="content-body clearfix">
 
@@ -152,33 +136,172 @@ $message = array(
 
     </section>
     <section class="span4">
-    	<article class="widget content-container qualtics-welcome">
-      	
-      	<header class="content-header">
-        	<h3>Welcome to the</h3>
-        	<h4>Community</h4>
-				</header>
-				
-				<section class="content-body clearfix">
+    	<div class="shadow-border-right">
+    		<article class="widget content-container qualtics-welcome span12">
+	      	
+	      	<header class="content-header">
+	        	<h3>Welcome to the</h3>
+	        	<h4>Community</h4>
+					</header>
 					
-					<p>
-						Here you have a voice. We encourage you to continue to share your thoughts and ideas. 
-					</p>
-					
-					<p>
-						Here you&rsquo;ll find valuable information on topics important to you. We&rsquo;re here to make your every day life a little easier. 
-					</p>
-					
-					<p>
-						If you have a question, our customer care staff, product experts and community members are here to help. 
-					</p>
-					
- 				</section>
-
-			</article>
+					<section class="content-body clearfix">
+						
+						<p>
+							Here you have a voice. We encourage you to continue to share your thoughts and ideas. 
+						</p>
+						
+						<p>
+							Here you&rsquo;ll find valuable information on topics important to you. We&rsquo;re here to make your every day life a little easier. 
+						</p>
+						
+						<p>
+							If you have a question, our customer care staff, product experts and community members are here to help. 
+						</p>
+						
+	 				</section>
+	
+				</article>
+    	</div>
     </section>
 		<section class="span8">
-			<?php get_template_part('parts/register'); ?>
+			<article class="content-container register span12">
+			    <section class="content-body clearfix">
+			        <h6 class="content-headline">Register</h6>
+
+			
+			        <form class="form_register" id="register-form" method="post" action="<?php echo '?ssoregister&origin=' . $origin; ?>" shc:gizmo="transFormer">
+			            <ul class="form-fields">
+			                <li>
+			                    <dl class="clearfix">
+			                        <dt class="span3"><label for="loginId">Email:</label></dt>
+			                        <dd class="span9"><input type="text" name="loginId" autocomplete="off" class="input_text" id="loginId" shc:gizmo:form="{required:true, trim:true, pattern: /^.+@.+?\.[a-zA-Z]{2,}$/, message: 'Please enter a valid email address'}" value="<?php if (isset($_GET['loginId'])) {echo $_GET['loginId'];} ?>"/></dd>
+			                    </dl>
+			                </li>
+			
+			                <li>
+			                    <dl class="clearfix">
+			                        <dt class="span3"><label for="login_confirm-email">Confirm Email:</label></dt>
+			                        <dd class="span9">
+			                            <input type="text" name="login_confirm-email" autocomplete="off" class="input_text" id="login_confirm-email" shc:gizmo:form="{required:true, trim:true, custom: function(self) {if (self.value.toLowerCase() != ($('#loginId').attr('value')).toLowerCase()) return false; else return true;}, message: 'Your email does not match. Please check and try again.'}" value="" />
+			                        </dd>
+			                    </dl>
+			                </li>
+			
+			                <li>
+			                    <dl class="clearfix">
+			                        <dt class="span3"><label for="logonPassword">Password:</label></dt>
+			                        <dd class="span9">
+			                            <input
+			                            type="password"
+			                            name="logonPassword"
+			                            autocomplete="off"
+			                            class="input_text input_password"
+			                            id="logonPassword"
+			                            shc:gizmo:form="{required:true, pattern: /^\w*(?=\w{8,})(?=\w*\d)(?=\w*[a-zA-Z])(?!\w*_)\w*$/, message: 'Please enter a valid password.'}"
+			                            shc:gizmo="tooltip"
+			                            shc:gizmo:options="
+			                            {
+			                                tooltip: {
+			                                    displayData: {
+			                                        element: 'passInfo'
+			                                    },
+			                                    events: {
+			                                        click: {
+			                                            active: false
+			                                        },
+			                                        blur: {
+			                                            active: true
+			                                        },
+			                                        focus: {
+			                                            active: true
+			                                        }
+			                                    },
+			                                    arrowPosition: 'left'
+			                                }
+			                            }"
+			                            />
+			                        </dd>
+			                    </dl>
+			                </li>
+			
+			                <li>
+			                    <dl class="clearfix">
+			                        <dt class="span3"><label for="zipcode">ZIP Code:</label></dt>
+			                        <dd class="span9"><input type="text" name="zipcode" autocomplete="off" class="input_text input_password" id="zipcode" shc:gizmo:form="{required:true, trim:true, pattern: /(^\d{5})(-\d{4})?$/, message: 'Please enter a valid ZIP code'}" value="<?php if (isset($_GET['Zipcode'])) {echo $_GET['Zipcode'];} ?>" /></dd>
+			                    </dl>
+			                </li>
+			
+			                <li class="clearfix">
+			                    <dl>
+			                        <dd class="span3">&nbsp;</dd>
+			                        <dd class="span9">
+			                            <p>
+			                                <input type="checkbox" name="offers" id="offers" value="True" class="input_checkbox" checked="checked"/> I would like to receive offers, updates and sale alerts from Sears
+			                            </p>
+			                        </dd>
+			                    </dl>
+			                </li>
+			
+			                <li class="clearfix">
+			                    <dl>
+			                        <dd class="span3">&nbsp;</dd>
+			                        <dd class="span9">
+			                            <button type="submit" class="<?php echo theme_option("brand"); ?>_button">Register</button>
+			                        </dd>
+			                    </dl>
+			                </li>
+			
+			                <li class="clearfix">
+			                    <dl>
+			                        <dd class="span3">&nbsp;</dd>
+			                        <dd class="span9">
+			                            <p>
+			                                By clicking &quot;Register&quot;, I agree to the <a href="<?php echo (theme_option("brand") == "sears")? "http://www.sears.com/shc/s/nb_10153_12605_NB_CStermsofservice":"http://www.kmart.com/csterms/nb-100000000000005?adCell=WF"; ?>" target="_blank" title="Terms of Use">Terms of Use</a> and <a href="<?php echo (theme_option("brand") == "sears")? "http://www.sears.com/shc/s/nb_10153_12605_NB_CSprivacy":"http://www.kmart.com/csprivacy/nb-100000000000006?adCell=WF"; ?>" target="_blank" title="Privacy Policy">Privacy Policy</a>.
+			                            </p>
+			                        </dd>
+			                    </dl>
+			                </li>
+			            </ul>
+			
+			        </form>
+			
+			        <ul>
+			            <li class="clearfix">
+			                <dl>
+			                    <dd class="span3">&nbsp;</dd>
+			                    <dd class="span9">
+			                        <p class="bold">
+			                            Existing Customer? <a href="/login/" title="Sign In" shc:gizmo="moodle" shc:gizmo:options="{moodle: {width:480, target:ajaxdata.ajaxurl, type:'POST', data:{action: 'get_template_ajax', template: 'page-login'}}}">Sign In</a>
+			                        </p>
+			                    </dd>
+			                </dl>
+			            </li>
+			        </ul>
+			        <div id="passInfo" class="info hide">
+			            <p class="bold">Your password must have:</p>
+			            <ul>
+			                <li>6 or more characters total</li>
+			                <li>At least one letter</li>
+			                <li>At least one number</li>
+			                <li>No space</li>
+			                <li>No special characters such as ! or ?</li>
+			            </ul>
+			            <p>All passwords are cAsE sEnSiTiVe.</p>
+			        </div>
+			
+			        <section id="login-open-id" class="open-id" shc:gizmo="openID">
+			            <span class="or">OR</span>
+			            <p>use your account from</p>
+			            <ul class="open-id-services clearfix">
+			                <li class="open-id_service open-id_facebook"><a href="#" shc:openID="facebook">Facebook</a></li>
+			                <li class="open-id_service open-id_yahoo"><a href="#" shc:openID="yahoo">Yahoo!</a></li>
+			                <li class="open-id_service open-id_google"><a href="#" shc:openID="google">Google</a></li>
+			                <!-- <li class="open-id_service open-id_twitter"><a href="#" shc:openID="twitter">Twitter</a></li> -->
+			            </ul>
+			        </section>
+			
+			    </section>
+			</article>
 		</section>
 <?php
     get_template_part('parts/footer');
