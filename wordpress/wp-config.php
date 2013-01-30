20e08f16c779f151594bf7f494116827698c6836
Tim Steele <tsteel5@searshc.com>
2013-01-30 09:45:35 -0600

    Fixed the configuration for wordpress to work on my local environment

--- wordpress/wp-config.php
+++ wordpress/wp-config.php
@@ -19,23 +19,23 @@
 
 
 /** The name of the database for WordPress */
-
-define('DB_NAME', 'comm_local');
+define('DB_NAME', 'communities');
 
 /** MySQL database username */
-define('DB_USER', 'wp_user');
+define('DB_USER', 'root');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'philos10');
+define('DB_PASSWORD', 'tim97669');
 
 /** MySQL hostname */
-define('DB_HOST', 'localhost');
+define('DB_HOST', '127.0.0.1:3306');
 
 /** Database Charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8');
 
 /** The Database Collate type. Don't change this if in doubt. */
 define('DB_COLLATE', '');
+
 
 
 /**#@+
