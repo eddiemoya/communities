7c956df6df0670f99d93117d61098e2310a54bb9
Eddie Moya <eddie.moya@gmail.com>
2013-02-08 11:44:06 -0600

    removing border-left from top-left dropzone on single posts

--- parts/post.php
+++ parts/post.php
@@ -17,7 +17,7 @@
 	<?php if ($slug = get_first_available_slug_from_list_of_terms_dont_use_this_horrible_function("single-{$post->post_type}-top-left", get_the_category())) { ?>
 	
 	
-		<section class="dropzone-inner-wrapper border-left">
+		<section class="dropzone-inner-wrapper">
 
 			<?php display_dropzone($slug); ?>
 			
