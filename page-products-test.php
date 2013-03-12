193c99f3c8476473896fb4380312ec2a33bbe259
Dan Crimmins <dbcrimmins@gmail.com>
2013-03-12 09:30:22 -0500

    Added caching and pagination

--- page-products-test.php
+++ page-products-test.php
@@ -1,7 +1,13 @@
 <?php
 
+
 echo '<h2>Ratings & Reviews Test Page</h2><br><br>';
 
+$rr = RR_User_Reviews::factory(10007132);
+
+$results = $rr->results;
+
+
 echo '<pre>';
-var_dump(RR_User_Reviews::factory(10007132)->results);
+var_dump($rr);
 echo '</pre>';
