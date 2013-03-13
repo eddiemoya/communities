6d08bec9af5ed0dcc7c4ba9f63a650b5884e619e
Dan Crimmins <dbcrimmins@gmail.com>
2013-03-13 09:07:13 -0500

    Added product_uri, catalogid, storeid to products result obj. Added edit_link, storeid, catalogid, product_uri properties to review objects

--- wordpress/wp-content/themes/Eris/page-products-test.php
+++ wordpress/wp-content/themes/Eris/page-products-test.php
@@ -11,3 +11,13 @@ $results = $rr->results;
 echo '<pre>';
 var_dump($rr);
 echo '</pre>';
+
+
+/*$product = Products_Api_Request::factory(array('api' => 'detail',
+												'term' 	=> '02631512000P'))
+								->response();
+								
+								
+echo '<pre>';
+var_dump($product);
+exit;*/
