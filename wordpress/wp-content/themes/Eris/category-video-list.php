6a73dc29553cdbae8e7aa24626815bebbb3947fc
tim-steele <tim.steele@whoistimsteele.com>
2013-03-18 10:59:34 -0500

    added video code

---
+++ wordpress/wp-content/themes/Eris/category-video-list.php
@@ -0,0 +1,154 @@
+<?php
+/**
+ * Template Name: batman
+ * @package WordPress
+ * @subpackage White Label
+ */
+
+get_template_part('parts/header');
+
+//loop();
+?>
+
+	<section class="span12">
+		
+		<section class="span8">
+			<!-- START VIDEO LIST -->
+			
+			<section class="dropzone-inner-wrapper ">        
+				<article class="widget content-container span12 page-title">
+					
+					<section class="content-body">
+						<h1>Appliances</h1>
+						<h2>Get the most out of the machines that make life easier. </h2>
+					</section>
+				</article>
+				
+				<!-- VIDEO RESULTS LIST -->
+        		<article class="widget content-container span12 results-list">
+
+					<section class="content-body">
+						<article class="content-container post">
+
+    						<section class="content-body clearfix">
+
+            					<div class="featured-image span6">
+            						<img title="Hidden refrigerator" alt="hidden refrigerator" class="attachment-post-thumbnail wp-post-image" src="https://www.sears.com/community/wp-content/uploads/2013/03/Hidden-refrigerator.jpg">
+            					</div>
+    
+
+        						<div class="span6">
+
+            						<div class="content-details clearfix">
+                                    	<span class="content-category"><a title="Appliances" href="https://www.sears.com/community/category/appliances/">Appliances</a></span>
+                						<time datetime="2013-03-04" pubdate="" class="content-date">March 4, 2013</time>
+									</div> <!-- content-details -->
+
+            						<h1 class="content-headline"><a href="https://www.sears.com/community/appliances/five-new-trends-in-kitchen-design/">Five New Trends in Kitchen Design</a></h1> <!-- content-headline -->
+
+						            <ul>
+						                <li class="content-author">By: <a href="https://www.sears.com/community/author/SHC-MichaelM/">SHC-MichaelM</a></li>
+						            </ul>
+									
+									<p class="content-excerpt">Walk into a kitchen 30 or 40 years ago, and you’d immediately notice the difference in design and functionality than our modern kitchens. If you’re thinking of remodeling all or part of your kitchen, you need to stay up on the latest trends. Here are five of the hottest trends in kitchen design right now.</p>
+									<p class="content-excerpt">1. Dual-fuel kitchen ranges. Anyone who’s cooked with a gas stove knows that they heat things faster and more thoroughly, which is why professional chefs use gas burners in their kitchens…. <a href="https://www.sears.com/community/appliances/five-new-trends-in-kitchen-design/" class="moretag">See More</a></p>
+
+	            					<p class="content-tags"></p>
+
+						            <ul>
+						                <li class="content-comments">0 comments</li> 
+						                <li class="content-share">
+						                	<ul class="addthis dropmenu">
+						    					<li><span class="sharebutton">Share</span>
+											        <ul class="sharemenulinks">
+											            <li><a addthis:title="Five New Trends in Kitchen Design" addthis:url="https://www.sears.com/community/appliances/five-new-trends-in-kitchen-design/" class="addthis_button_facebook at300b" title="Facebook" href="#"><span class="at16nc at300bs at15nc at15t_facebook at16t_facebook"><span class="at_a11y">Share on facebook</span></span>Facebook</a></li>
+											            <li><a addthis:title="Five New Trends in Kitchen Design" addthis:url="https://www.sears.com/community/appliances/five-new-trends-in-kitchen-design/" class="addthis_button_twitter at300b" title="Tweet" href="#"><span class="at16nc at300bs at15nc at15t_twitter at16t_twitter"><span class="at_a11y">Share on twitter</span></span>Twitter</a></li>
+											            <li><a title="Post this to your ShopYourWay account" style="cursor:pointer;" onclick="javascript:window.open(this.href,'ShopYourWay','width=650,height=350'); return false;" href="https://shopyourway.com/sharer/share?title=Five+New+Trends+in+Kitchen+Design&amp;link=https%3A%2F%2Fwww.sears.com%2Fcommunity%2Fappliances%2Ffive-new-trends-in-kitchen-design%2F&amp;sourceSiteUrl=&amp;sourceSiteAlias=&amp;content=&amp;imageUrl="><img alt="ShopYourWay Post" src="https://static.shopyourway.com/static/share-buttons/small-light.png">ShopYourWay</a></li>
+											            <li><a addthis:url="https://www.sears.com/community/appliances/five-new-trends-in-kitchen-design/" class="addthis_button_email at300b" title="Email" href="#"><span class="at16nc at300bs at15nc at15t_email at16t_email"><span class="at_a11y">Share on email</span></span>Email</a></li>
+											        </ul>
+						    					</li>
+											</ul>
+										</li>
+						             </ul>
+
+        						</div> <!-- featured- -->
+
+							    </section> <!-- featured-post -->
+							</article>
+
+							<article class="content-container post">
+
+							    <section class="content-body clearfix">
+
+           							<div class="featured-image span6">
+            							<img title="gas stove flame" alt="gas stove flame" class="attachment-post-thumbnail wp-post-image" src="https://www.sears.com/community/wp-content/uploads/2013/03/gas-stove-flame.jpg">
+            						</div>
+    
+
+        							<div class="span6">
+
+            							<div class="content-details clearfix">
+                                    		<span class="content-category"><a title="Appliances" href="https://www.sears.com/community/category/appliances/">Appliances</a></span>
+                							<time datetime="2013-03-04" pubdate="" class="content-date">March 4, 2013</time>
+										</div> <!-- content-details -->
+
+							            <h1 class="content-headline">
+							                <a href="https://www.sears.com/community/appliances/10-smart-home-fire-prevention-tips/">10 Smart Home Fire Prevention Tips</a>
+							            </h1> <!-- content-headline -->
+
+							            <ul>
+							                <li class="content-author">By: <a href="https://www.sears.com/community/author/SHC-MichaelM/">SHC-MichaelM</a></li>
+							            </ul>
+
+							            <p class="content-excerpt">Home fires are scary events that can destroy your home and its contents as well as put your family in mortal danger. Most people know some basic home fire safety rules, but there are many huge fire dangers present in most homes that most of us don’t really think about. Below is a list of 10 of the most common fire dangers in households and what you can do to prevent them.</p>
+										<p class="content-excerpt">1. Dryer lint. After each load of laundry you should thoroughly clean the dryer lint out of the removable screen in your… <a href="https://www.sears.com/community/appliances/10-smart-home-fire-prevention-tips/" class="moretag">See More</a></p>
+
+							            <p class="content-tags"></p>
+
+							            <ul>
+							                <li class="content-comments">0 comments</li> 
+							                <li class="content-share">
+							                	<ul class="addthis dropmenu">
+												    <li>
+												        <span class="sharebutton">Share</span>
+												        <ul class="sharemenulinks">
+												            <li><a addthis:title="10 Smart Home Fire Prevention Tips" addthis:url="https://www.sears.com/community/appliances/10-smart-home-fire-prevention-tips/" class="addthis_button_facebook at300b" title="Facebook" href="#"><span class="at16nc at300bs at15nc at15t_facebook at16t_facebook"><span class="at_a11y">Share on facebook</span></span>Facebook</a></li>
+												            <li><a addthis:title="10 Smart Home Fire Prevention Tips" addthis:url="https://www.sears.com/community/appliances/10-smart-home-fire-prevention-tips/" class="addthis_button_twitter at300b" title="Tweet" href="#"><span class="at16nc at300bs at15nc at15t_twitter at16t_twitter"><span class="at_a11y">Share on twitter</span></span>Twitter</a></li>
+												            <li><a title="Post this to your ShopYourWay account" style="cursor:pointer;" onclick="javascript:window.open(this.href,'ShopYourWay','width=650,height=350'); return false;" href="https://shopyourway.com/sharer/share?title=10+Smart+Home+Fire+Prevention+Tips&amp;link=https%3A%2F%2Fwww.sears.com%2Fcommunity%2Fappliances%2F10-smart-home-fire-prevention-tips%2F&amp;sourceSiteUrl=&amp;sourceSiteAlias=&amp;content=&amp;imageUrl="><img alt="ShopYourWay Post" src="https://static.shopyourway.com/static/share-buttons/small-light.png">ShopYourWay</a></li>
+												            <li><a addthis:url="https://www.sears.com/community/appliances/10-smart-home-fire-prevention-tips/" class="addthis_button_email at300b" title="Email" href="#"><span class="at16nc at300bs at15nc at15t_email at16t_email"><span class="at_a11y">Share on email</span></span>Email</a></li>
+												        </ul>
+												    </li>
+												</ul>
+											</li>
+										</ul>
+							
+							        </div> <!-- featured- -->
+
+							    </section> <!-- featured-post -->
+							</article>
+
+    					</section>
+
+						<section class="pagination">
+							<a href="https://www.sears.com/community/category/appliances/post/page/2/">Next Page »</a>
+						</section>
+					</article>
+					
+					<div class="clearfix"></div>
+				
+				</section>
+			
+			<!-- END VIDEO LIST -->
+		</section>
+	
+		<section class="span4">
+			
+
+		</section>
+		
+	</section>
+	
+<?php
+get_template_part('parts/footer');
+
+?>
---
+++ wordpress/wp-content/themes/Eris/category-video-single.php
@@ -0,0 +1,247 @@
+<?php
+/**
+ * Template Name: batman
+ * @package WordPress
+ * @subpackage White Label
+ */
+
+get_template_part('parts/header');
+
+//loop();
+?>
+
+	<section class="span12">
+		
+		<section class="span8">
+			<!-- VIDEO ARTICLE -->
+			
+			<article class="content-container post span12">
+				<section class="content-body clearfix">
+					
+					<div class="content-video">
+						<!-- YOU HAVE TO INCLUDE PARAMETER 'wmode=opaque' OR VIDEO OVERLAPS SITE -->
+						<iframe width="560" height="315" src="http://www.youtube.com/embed/kfVsfOSbJY0?wmode=opaque" frameborder="0" allowfullscreen></iframe>
+					</div>
+					
+					<div class="content-details clearfix">
+						<span class="content-category"><a title="" href="https://www.sears.com/community/category/appliances/">Appliances</a></span>
+						<time datetime="2012-10-24" pubdate="" class="content-date">October 24, 2012</time>
+					</div>
+				
+					<h1 class="content-headline">8 Tips for Installing a Washing Machine</h1>
+				
+					<p>
+						So, you bought a <a target="_blank" href="http://www.sears.com/appliances-washers/c-1101269">new washing machine </a>and you’re ready to install it and put it right to work. Before you do, make sure your laundry room’s floor and pipes are ready too.
+					</p>
+					
+					<h2>A Stable Floor</h2>
+					<p>
+						Whatever washing machine you choose, the water’s stop-and-go action generates a lot of force at the machine’s feet. That force transfers to the floor beneath, which is why washers were traditionally installed on strong, force-dampening concrete basement floors. Today’s laundry rooms, however, are often upstairs where wood subfloor and joists have to bear the force. You can still have a second-floor laundry center, but check the floor structure first:
+					</p>
+					<ol class="bullets">
+						<li>It should be extra stiff to resist vibration. If the floor is weak or spongy, the washer won’t operate properly. Adding thicker plywood subfloor or blocking helps.</li>
+						<li>If you have a tile floor, install an extra-thick mortar base beneath it.</li>
+						<li>Your floor covering matters too; if it’s not abrasion resistant, the floor could suffer. The feet of an aggressively bouncing machine with an unbalanced load can rip apart sheet vinyl floors.</li>
+					</ol>
+					
+					<h2>Plumbing</h2>
+					<p>
+						Washers have simple plumbing, but that doesn’t mean the components aren’t important. Proper <a target="_blank" href="http://www.sears.com/tools-plumbing-tools/c-1221021417">plumbing</a> is key to a washer’s functionality. Don’t hurry through installation or brush off small problems &mdash; you could end up with a huge mess.
+					</p>
+					<ol class="bullets">
+						<li>Most washers come with relatively low-quality supply hoses that can swell under pressure and then burst. Replace them with braided steel hoses to reduce the risk of catastrophic leaks.</li>
+						<li>If it’s hard to reach the shut-off faucets for the hoses, install faucets within reach so you can easily control the water. Then get in the habit of shutting off the water when you’re not doing laundry.</li>
+						<li>Inadequately supported supply pipes can rattle as the water turns on and shuts off during the wash cycle. Keep them quiet by clamping the pipes securely to the wall or to one another &mdash; just don’t clamp them so tightly that they can’t expand and contract as the water temperature changes. Wrap the <a target="_blank" href="http://www.sears.com/tools-hand-tools-clamps/s-1028720">clamps</a> with electrical tape to stop them from grabbing the pipes during these fluctuations, or place a small piece of insulation between the clamp and the pipe.</li>
+						<li>Install a drain in the floor of all laundry rooms above basement level. If you can’t install a drain, place a tray or pan below the machine to catch leaks. The best of both worlds is a tray with an integral drain line.</li>
+						<li>Used wash water usually drains into a vertical drain called a standpipe. Connect the pipe before the trap to keep sewer odors out of the standpipe. The drainpipe should be less than 8-ft. tall so water drains easily.</li>
+					</ol>
+					<p>
+						<em><strong>Do you have any washing machine installation tips you’d like to share with the community. Write them in the Comments section below.</strong></em>
+					</p>
+				
+					<div class="clearfix">&nbsp;</div>
+				
+					<p class="content-author">
+						By: <a href="https://www.sears.com/community/author/SHC-MichaelM/">SHC-MichaelM</a>				
+					</p>
+				
+	   				<div class="addthis_toolbox addthis_default_style">
+					    <a addthis:title="8 Tips for Installing a Washing Machine" addthis:url="https://www.sears.com/community/appliances/8-tips-for-installing-a-washing-machine/" class="addthis_button_facebook_like at300b"><div .uuot_ncgdxa.like"="" class="fb-like data-ref= fb_edge_widget_with_comment fb_iframe_widget" data-layout="button_count" data-show_faces="false" data-action="like" data-width="90" data-font="arial" data-href="https://www.sears.com/community/appliances/8-tips-for-installing-a-washing-machine/" data-send="false" fb-xfbml-state="rendered"><span style="height: 20px; width: 75px;"><iframe scrolling="no" id="fdd0c0fae85be" name="f1aace1176608a8" style="border: medium none; overflow: hidden; height: 20px; width: 75px;" title="Like this content on Facebook." class="fb_ltr" src="https://www.facebook.com/plugins/like.php?api_key=172525162793917&amp;locale=en_US&amp;sdk=joey&amp;channel_url=https%3A%2F%2Fs-static.ak.facebook.com%2Fconnect%2Fxd_arbiter.php%3Fversion%3D19%23cb%3Df389e52e3fdcbfe%26origin%3Dhttps%253A%252F%252Fwww.sears.com%252Ff125b8f8b70a60c%26domain%3Dwww.sears.com%26relation%3Dparent.parent&amp;href=https%3A%2F%2Fwww.sears.com%2Fcommunity%2Fappliances%2F8-tips-for-installing-a-washing-machine%2F&amp;node_type=link&amp;width=90&amp;font=arial&amp;layout=button_count&amp;colorscheme=light&amp;action=like&amp;show_faces=false&amp;send=false&amp;extended_social_context=false"></iframe></span></div></a>
+					    <a addthis:title="8 Tips for Installing a Washing Machine" addthis:url="https://www.sears.com/community/appliances/8-tips-for-installing-a-washing-machine/" class="addthis_button_tweet at300b"><iframe scrolling="no" frameborder="0" allowtransparency="true" src="https://platform.twitter.com/widgets/tweet_button.1363148939.html#_=1363383294625&amp;count=horizontal&amp;counturl=https%3A%2F%2Fwww.sears.com%2Fcommunity%2Fappliances%2F8-tips-for-installing-a-washing-machine%2F&amp;id=twitter-widget-0&amp;lang=en&amp;original_referer=https%3A%2F%2Fwww.sears.com%2Fcommunity%2Fappliances%2F8-tips-for-installing-a-washing-machine%2F&amp;size=m&amp;text=8%20Tips%20for%20Installing%20a%20Washing%20Machine&amp;url=https%3A%2F%2Fwww.sears.com%2Fcommunity%2Fappliances%2F8-tips-for-installing-a-washing-machine%2F%23.UUOT_hTE-N0.twitter" class="twitter-share-button twitter-count-horizontal" style="width: 107px; height: 20px;" title="Twitter Tweet Button" data-twttr-rendered="true"></iframe></a>
+					    <a title="Post this to your ShopYourWay account" onclick="openSYWWindow(this.href,'ShopYourWay','width=650,height=350'); return false;" href="https://shopyourway.com/sharer/share?title=8+Tips+for+Installing+a+Washing+Machine&amp;link=https%3A%2F%2Fwww.sears.com%2Fcommunity%2Fappliances%2F8-tips-for-installing-a-washing-machine%2F&amp;sourceSiteUrl=&amp;sourceSiteAlias=&amp;content=&amp;imageUrl="><img alt="ShopYourWay Post" src="https://www.sears.com/community/wp-content/themes/Eris/assets/img/shopyourway_large.png"></a>
+					    <a addthis:url="https://www.sears.com/community/appliances/8-tips-for-installing-a-washing-machine/" class="addthis_button_email at300b" title="Email" href="#"><img alt="Email icon" src="https://www.sears.com/community/wp-content/themes/Eris/assets/img/email.png"></a>
+						<div class="atclear"></div>
+					</div>
+		    		<p class="content-comments">
+		    			4 comments		    
+		    		</p>
+				
+				</section>
+			</article>
+			
+	    	<div class="ugc-comment-answer_form span12">
+				<div class="trigger discussion">
+					<a shc:gizmo:options="{moodle: {width:480, target:ajaxdata.ajaxurl, type:'POST', data:{action: 'get_template_ajax', template: 'page-login'}}}" shc:gizmo="moodle" href="#">Leave a comment <span class="smaller">▼</span></a>
+				</div>
+			</div>    
+			
+			<section class="span12 content-container comments">
+				<section class="content-body">
+                	
+                	<ol id="allComments">
+                
+						<li id="comment-reply-24738" class="comment clearfix">
+    
+							<ul class="member_details span2">
+
+							    <li class="member_avatar span6">
+							        <a href="https://www.sears.com/community/author/AdamO/"><img alt="" class="photo" src="https://www.sears.com/community/wp-content/uploads/userphoto/1339245.jpg?1350546974"></a>
+							    </li>
+
+            					<li class="member_profile span6">
+            						<ul>
+	                                    <li class="member_screen-name"><a href="https://www.sears.com/community/author/AdamO/">AdamO</a></li>
+	                                    <li class="member_location">San Jose, CA</li>
+                            		</ul>
+       							 </li>
+
+    						</ul>
+    						
+    						<div class="span10">
+    							<article class="content-container">
+									<section class="content-body clearfix">
+					
+										<div class="content-details clearfix">
+											<time datetime="2012-10-24" pubdate="" class="content-date">October 24, 2012<span class="time-stamp">9:28pm</span></time>
+										</div>
+					
+					          			<p>
+					          				The drain hose of new washing machines is often installed on the machine, with its end tucked inside of the cabinet.
+					          			</p>
+										<p>
+											Operating the machine, without having removed the tucked-in end and placing it over an existing laundry room drain pipe will result in ALL of the water being pumped out, through the washing machine, onto the laundry room floor.
+										</p>
+					
+									</section>
+								</article> <!-- END ARTICLE CONTENT CONTAINER -->
+            					
+           						<div class="clearfix"></div>
+           						 
+        						<div class="ugc-comment-answer_form clearfix">
+        							<div class="trigger discussion">
+										<a shc:gizmo:options="{moodle: {width:480, target:ajaxdata.ajaxurl, type:'POST', data:{action: 'get_template_ajax', template: 'page-login'}}}" shc:gizmo="moodle" href="#">Reply</a>
+									</div>
+				        		</div> <!-- END UGC COMMENT ANSWER FORM --> 
+    						</div> <!-- END SPAN10 -->
+    					</li>
+
+						<li id="comment-reply-24737" class="comment clearfix">
+    
+							<ul class="member_details span2">
+
+							    <li class="member_avatar span6">
+							        <a href="https://www.sears.com/community/author/AdamO/"><img alt="" class="photo" src="https://www.sears.com/community/wp-content/uploads/userphoto/1339245.jpg?1350546974"></a>
+							    </li>
+
+						        <li class="member_profile span6">
+						        	<ul>
+						                <li class="member_screen-name"><a href="https://www.sears.com/community/author/AdamO/">AdamO</a></li>
+						                <li class="member_location">San Jose, CA</li>
+						           	</ul>
+						    	</li>
+
+    						</ul>
+    						
+    						<div class="span10">
+    							<article class="content-container">
+									<section class="content-body clearfix">
+					
+										<div class="content-details clearfix">
+											<time datetime="2012-10-24" pubdate="" class="content-date">October 24, 2012<span class="time-stamp">9:25pm</span></time>
+										</div>
+						
+						          		<p>
+						          			Washing machine hot and cold standard or steel-braided hoses work MUCH better when the rubber flat washers are installed in "both" female ends of the hoses.
+						          		</p>
+										<p>
+											Without the washers, there will be "leaking" no matter how tight the hose ends are turned.
+										</p>
+					
+									</section>
+								</article> <!-- END ARTICLE CONTENT CONTAINER -->
+            
+    							<div class="clearfix"></div>
+        						<div class="ugc-comment-answer_form clearfix">
+        							<div class="trigger discussion">
+										<a shc:gizmo:options="{moodle: {width:480, target:ajaxdata.ajaxurl, type:'POST', data:{action: 'get_template_ajax', template: 'page-login'}}}" shc:gizmo="moodle" href="#">Reply</a>
+									</div>
+				        		</div> <!-- END UGC COMMENT ANSWER FORM --> 
+    						</div> <!-- END SPAN10 -->
+            				
+            				<ol class="children">
+            
+								<li id="comment-reply-27477" class="comment clearfix">
+    
+									<ul class="member_details span2">
+
+									    <li class="member_avatar span6">
+									        <a href="https://www.sears.com/community/author/TestManiac/"><img alt="" class="photo" src="https://www.sears.com/community/wp-content/uploads/userphoto/1373431.png?1362690962"></a>
+									    </li>
+
+							            <li class="member_profile span6">
+							            	<ul>
+							                    <li class="member_screen-name"><a href="https://www.sears.com/community/author/TestManiac/">TestManiac</a></li>
+							                    <li class="member_location">Chicago, IL</li>
+							                </ul>
+							        	</li>
+
+    								</ul>
+    								
+    								<div class="span10">
+								    	<article class="content-container">
+											<section class="content-body clearfix">
+													
+												<div class="content-details clearfix">
+													<time datetime="2013-03-12" pubdate="" class="content-date">March 12, 2013<span class="time-stamp">3:51pm</span></time>
+												</div>
+													
+												<p class="responseTo">
+								            		In response to AdamO            
+								            	</p>
+								        	    
+								        	    <p>Thanks for the tips.</p>
+													
+											</section>
+										</article> <!-- END ARTICLE CONTENT CONTAINER -->
+            							
+            							<div class="clearfix"></div>
+        								
+        								<div class="ugc-comment-answer_form clearfix">
+        									<div class="trigger discussion">
+												<a shc:gizmo:options="{moodle: {width:480, target:ajaxdata.ajaxurl, type:'POST', data:{action: 'get_template_ajax', template: 'page-login'}}}" shc:gizmo="moodle" href="#">Reply</a>
+											</div>
+				        				</div> <!-- END UGC COMMENT ANSWER FORM --> 
+    								</div> <!-- END SPAN10 -->
+    							
+    							</li>
+        					</ol>
+    					</li>
+                	</ol> <!-- END ALL COMMENTS -->
+            	</section> <!-- END CONTENT BODY -->
+
+			</section> <!-- END COMMENTS CONTENT CONTAINER -->
+			
+			<!-- END VIDEO ARTICLE -->
+		</section>
+	
+		<section class="span4">
+			
+
+		</section>
+		
+	</section>
+	
+<?php
+get_template_part('parts/footer');
+
+?>
