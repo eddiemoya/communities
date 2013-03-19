39a78b8c9d2a1f7188543323d9b323ec8201bab6
tim-steele <tim.steele@whoistimsteele.com>
2013-03-19 10:25:05 -0500

    added HTML for recent reviews

---
+++ wordpress/wp-content/themes/Eris/page-dev-recent-reviews.php
@@ -0,0 +1,181 @@
+<?php
+/**
+ * Template Name: batman
+ * @package WordPress
+ * @subpackage White Label
+ */
+
+get_template_part('parts/header');
+
+//loop();
+?>
+
+	<section class="span12">
+		
+		<section class="span8">
+			
+			
+			
+		</section>
+	
+		<section class="span4">
+			
+			<!-- RECENT REVIEWS WIDGET -->
+			
+			<article class="span12 content-container recent_reviews">
+
+				<hgroup class="content-header">
+			  		<h3>Recent Reviews</h3>
+			  	</hgroup>
+
+				<section class="content-body clearfix">
+					<ul class="recent-reviews-list">
+
+						<li class="clearfix">
+							<ul>
+								<li class="recent-review_title">
+									<!--
+										Title limited to 73 characters. Need to add horizontal ellipse.
+										Link to all reviews page
+									-->
+									<a href="#">Really Impressed</a>
+								</li>
+								<li class="recent-review_reviewer">Steve Jones, Los Angeles, CA</li>
+								<li class="recent-review_review">
+									<!-- 
+										We will have to add the quotes and horizontal ellipse.
+										Text limited to 80 characters.
+										'more' links to all reviews page.
+									-->
+									&quot;Completely plagiarize tactical niches after unique growth strategies. Intrinsicly&hellip;&quot; <a href="#">more</a>
+								</li>
+								<li class="recent-review_name-rating">
+									<span class="recent-review_name">Kenmore French Door</span>
+									<span class="recent-review_rating">
+										
+									</span>
+								</li>								
+							</ul>
+						</li>
+
+						<li class="clearfix">
+							<ul>
+								<li class="recent-review_title">
+									<!--
+										Title limited to 73 characters. Need to add horizontal ellipse.
+										Link to all reviews page
+									-->
+									<a href="#">Nice features on Amana&hellip;</a>
+								</li>
+								<li class="recent-review_reviewer">Glen Matlock, Naples, FL</li>
+								<li class="recent-review_review">
+									<!-- 
+										We will have to add the quotes and horizontal ellipse.
+										Text limited to 80 characters.
+										'more' links to all reviews page.
+									-->
+									&quot;Completely plagiarize tactical niches after unique growth strategies. Intrinsicly&hellip;&quot; <a href="#">more</a>
+								</li>
+								<li class="recent-review_name-rating">
+									<span class="recent-review_name">Amana Microwave</span>
+									<span class="recent-review_rating">
+										
+									</span>
+								</li>								
+							</ul>
+						</li>
+						
+						<li class="clearfix">
+							<ul>
+								<li class="recent-review_title">
+									<!--
+										Title limited to 73 characters. Need to add horizontal ellipse.
+										Link to all reviews page
+									-->
+									<a href="#">Does the light stay on when&hellip;</a>
+								</li>
+								<li class="recent-review_reviewer">Paul Cook, New York, NY</li>
+								<li class="recent-review_review">
+									<!-- 
+										We will have to add the quotes and horizontal ellipse.
+										Text limited to 80 characters.
+										'more' links to all reviews page.
+									-->
+									&quot;Completely plagiarize tactical niches after unique growth strategies. Intrinsicly&hellip;&quot; <a href="#">more</a>
+								</li>
+								<li class="recent-review_name-rating">
+									<span class="recent-review_name">Kenmore French Door</span>
+									<span class="recent-review_rating">
+										
+									</span>
+								</li>								
+							</ul>
+						</li>
+						
+					</ul>
+		
+				</section>			
+
+			</article>
+
+			<!-- END RECENT REVIEWS WIDGET -->
+			
+			<!-- RECENT ACTIVITIES WIDGET -->
+			
+			<article class="span12 content-container recent_activity">
+
+				<hgroup class="content-header">
+			  		<h3>Recent Activities</h3>
+			  	</hgroup>
+
+				<section class="content-body clearfix">
+					<ul class="recent-activity-list">
+
+						<li class="clearfix">
+							<div class="recent-activity_avatar">
+								<img alt="" class="photo" src="https://www.sears.com/community/wp-content/uploads/userphoto/137.jpg?1348781895">						</div>
+							<div class="recent-activity_data">
+								<ul>
+									<li class="recent-activity_activity"><a href="https://www.sears.com/community/author/SHC-BillW/">SHC-BillW</a> Answered this: </li>
+									<li class="recent-activity_category-time">
+										<time datetime="2013-03-18" pubdate="" class="content-date">Mar 18 3:26pm</time>
+										<a class="category" href="https://www.sears.com/community/category/appliances/">Appliances</a>
+									</li>
+									<li class="recent-activity_title"><a href="https://www.sears.com/community/questions/i-have-a-sears-washing-machine-is-draining-but-not-spinning-the-manual-says-check-the-fuse-this-is-of-the-house-or-is-there-a-fuse-on-the-washer-itself/">I have a Sears washing machine is draining, but not spinning.  The manual says c...</a></li>
+									<li class="recent-activity_excerpt">Hi ocicat,  The answer to the question you poised is not easy to answer. If the ...</li>								
+								</ul>
+							</div>
+						</li>
+
+						<li class="clearfix">
+							<div class="recent-activity_avatar">
+								<img alt="" class="photo" src="https://www.sears.com/community/wp-content/themes/Eris/assets/img/avatar.jpg?">						</div>
+							<div class="recent-activity_data">
+								<ul>
+									<li class="recent-activity_activity"><a href="https://www.sears.com/community/author/Roadlizard7/">Roadlizard7</a> Answered this: </li>
+									<li class="recent-activity_category-time">
+										<time datetime="2013-03-18" pubdate="" class="content-date">Mar 18 2:32pm</time>
+										<a class="category" href="https://www.sears.com/community/category/appliances/">Appliances</a>
+									</li>
+									<li class="recent-activity_title"><a href="https://www.sears.com/community/questions/are-gas-dryers-more-efficient-than-electric-dryers/">Are gas dryers more efficient than electric dryers?</a></li>
+									<li class="recent-activity_excerpt">Given the current glut of natural gas, and the fact that burning natural gas is ...</li>								
+								</ul>
+							</div>
+						</li>
+
+					</ul>
+		
+				</section>			
+
+			</article>
+
+			<!-- END RECENT ACTIVITIES WIDGET -->
+
+		</section>
+		
+	</section>
+	
+<?php
+get_template_part('parts/footer');
+
+?>
