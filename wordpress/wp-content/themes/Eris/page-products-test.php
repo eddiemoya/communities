598e653ff11a12bc13aec2150a6a53fb2b52475e
Dan Crimmins <dbcrimmins@gmail.com>
2013-03-27 11:25:51 -0500

    finished core product updater

--- wordpress/wp-content/themes/Eris/page-products-test.php
+++ wordpress/wp-content/themes/Eris/page-products-test.php
@@ -3,8 +3,8 @@
 
 echo '<h2>Ratings & Reviews Test Page</h2><br><br>';
 
-$rr = RR_User_Reviews::factory(35497172)
-						->get();
+/*$rr = RR_User_Reviews::factory(35497172)
+						->get();*/
 
 /*$rr = $reviews = RR_Recent_Reviews::factory()
        								->limit(3)
@@ -20,20 +20,37 @@ $rr = RR_User_Reviews::factory(35497172)
 
 //$results = $rr->results;
 
+/*$rr = get_posts(array('post_type'		=> 'product',
+						'post_status'	=> array('publish', 'draft'),
+						'posts_per_page' => 2,
+						'paged'	=> 4));*/
 
-echo '<pre>';
+
+
+
+/*echo '<pre>';
 var_dump($rr);
-echo '</pre>';
+echo '</pre>';*/
+
+try {
+		Products_Updater::factory()
+						->update();
+						
+} catch(Exception $e) {
+	
+	echo $e->getMessage();
+}
+
 
 //part string in elem 5
-/*$pieces = explode('/', 'http://www.kmart.com/shc/s/p_10151_10104_021W004989196001P');
+/*$piece = explode('/', 'http://www.kmart.com/shc/s/p_10151_10104_021W004989196001P');
 $review_str = str_replace('p_', 'allmodreviews_', $pieces[5]);
 
 echo $review_str;*/
 
 
 /*$product = Products_Api_Request::factory(array('api' => 'detail',
-												'term' 	=> '02631512000P'))
+												'term' 	=> 'ghfhgfh'))
 								->response();
 								
 								
