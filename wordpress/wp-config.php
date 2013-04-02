6af507217df48e22b1a12942069ebe7b35195286
tim-steele <tim.steele@whoistimsteele.com>
2013-04-02 10:07:16 -0500

    Fixed wp-config to work with my DB

--- wordpress/wp-config.php
+++ wordpress/wp-config.php
@@ -14,17 +14,15 @@
  *
  * @package WordPress
  */
-
-
-
+ 
 /** The name of the database for WordPress */
-define('DB_NAME', 'comm_local');
+define('DB_NAME', 'communities');
 
 /** MySQL database username */
-define('DB_USER', 'wp_user');
+define('DB_USER', 'root');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'philos10');
+define('DB_PASSWORD', '');
 
 /** MySQL hostname */
 define('DB_HOST', 'localhost');
@@ -34,8 +32,6 @@ define('DB_CHARSET', 'utf8');
 
 /** The Database Collate type. Don't change this if in doubt. */
 define('DB_COLLATE', '');
-
-
 
 /**#@+
  * Authentication Unique Keys and Salts.
