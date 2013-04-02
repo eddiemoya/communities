9ed61079514c0e4bb969f9c4184cbd83ccd64aed
tim-steele <tim.steele@whoistimsteele.com>
2013-04-02 13:46:50 -0500

    Added product carousel pages/images

---
+++ wordpress/wp-content/themes/Eris/page-product-carousel.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * Template Name: batman
+ * @package WordPress
+ * @subpackage White Label
+ */
+
+get_template_part('parts/header');
+
+//loop();
+?>
+
+	<section class="span12">
+		
+		<section class="span8">
+			
+			<!-- PRODUCT CAROUSEL -->
+			
+			<section class="carousel span12">
+				
+				<div class="carousel-container">
+					
+					<ul class="carousel-items">
+						
+						<li>
+							
+						</li>
+						
+					</ul>
+					
+				</div>
+				
+			</section>
+
+			<!-- END PRODUCT CAROUSEL -->
+			
+		</section>
+	
+		<section class="span4">
+
+		</section>
+		
+	</section>
+	
+<?php
+get_template_part('parts/footer');
+
+?>
