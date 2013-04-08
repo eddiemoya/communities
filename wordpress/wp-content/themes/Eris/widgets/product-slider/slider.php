df0f74fa0a53b1abdaf0410b62db0683cb77e756
Jason Corradino <jason@ididntbreak.it>
2013-04-08 14:30:04 -0500

    slider widget front end sandbox nonsense

---
+++ wordpress/wp-content/themes/Eris/widgets/product-slider/slider.php
@@ -0,0 +1,55 @@
+<?php if ($instance["widget_title"] != "") : ?>
+	<hgroup class="content-header">
+		<h3><?php echo $instance["widget_title"]; ?></h3>
+	</hgroup>
+<?php endif; ?>
+<section class="content-body product-slider-widget-content clearfix length<?php echo $instance["product_count"]; ?>">
+	<div class="left-arrow"></div>
+	<div class="product-slider-container">
+		<div class="product">
+			<a href="#"><img src="http://s.shld.net/is/image/Sears/spin_prod_256367401?&wid=115&op_sharpen=1" /></a>
+			<p class="product-title"><a href="#">LG 55" LED 1080p HDTV 55LS4500</a></p>
+			<span class="product-stars star-selected"></span>
+			<span class="product-stars star-selected"></span>
+			<span class="product-stars star-selected"></span>
+			<span class="product-stars star-deselected"></span>
+			<span class="product-stars star-deselected"></span>
+			<p class="product-price">$699.98 <span class="old-price">$999.99</span></p>
+		</div>
+		<div class="product">
+			<a href="#"><img src="http://s.shld.net/is/image/Sears/spin_prod_674486201?&wid=115&op_sharpen=1" /></a>
+			<p class="product-title"><a href="#">LG 47" LED Cinema 3D HDTV 47LM4600</a></p>
+			<span class="product-stars star-selected"></span>
+			<span class="product-stars star-selected"></span>
+			<span class="product-stars star-deselected"></span>
+			<span class="product-stars star-deselected"></span>
+			<span class="product-stars star-deselected"></span>
+			<p class="product-price">$599.99</p>
+		</div>
+		<div class="product">
+			<a href="#"><img src="http://s.shld.net/is/image/Sears/spin_prod_662972601?&wid=115&op_sharpen=1" /></a>
+			<p class="product-title"><a href="#">Samsung 46" Class 1080p 120Hz LED HDTV UN46EH6000</a></p>
+			<span class="product-stars star-selected"></span>
+			<span class="product-stars star-selected"></span>
+			<span class="product-stars star-selected"></span>
+			<span class="product-stars star-selected"></span>
+			<span class="product-stars star-deselected"></span>
+			<p class="product-price">$499.88 <span class="old-price">$869.99</span</p>
+		</div>
+		<div class="product">
+			<a href="#"><img src="http://s.shld.net/is/image/Sears/spin_prod_546783501?&wid=115&op_sharpen=1" /></a>
+			<p class="product-title"><a href="#">Sharp 70" Class Aquos® 1080p 120Hz LED HDTV - LC70LE650U</a></p>
+			<span class="product-stars star-selected"></span>
+			<span class="product-stars star-deselected"></span>
+			<span class="product-stars star-deselected"></span>
+			<span class="product-stars star-deselected"></span>
+			<span class="product-stars star-deselected"></span>
+			<p class="product-price">$599.99</p>
+		</div>
+	</div>
+	<div class="right-arrow"></div>
+</section>
+
+
+
+
