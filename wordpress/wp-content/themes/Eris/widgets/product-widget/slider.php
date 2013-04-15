d6d4b66fab3b4766411d3e54bc0a51d5d78f2a8e
Matt Day <mday1@searshc.com>
2013-04-15 15:57:45 -0500

    Fixed front-loading issue

--- wordpress/wp-content/themes/Eris/widgets/product-widget/slider.php
+++ wordpress/wp-content/themes/Eris/widgets/product-widget/slider.php
@@ -2,7 +2,16 @@
 	$count = 1;
 	$span = ($pw_template == "products-vertical") ? "span12" : "span12";
 	$layout = ($pw_template == "products-vertical") ? "vertical" : "horizontal";
+
 	$total = count($data->products);
+	$prodRows = $data->products;
+	$flNumber = 1;
+
+	for($i=0; $i<$flNumber; $i++)
+	{
+		$ar = array_pop($prodRows);
+		array_unshift($prodRows, $ar);
+	}
 ?>
 
 <?php if ($instance["pw_title"] != "") { ?>
@@ -13,8 +22,8 @@
 <section class="content-body product-slider-widget-content clearfix viewport_size_<?php echo($pw_fields_to_show); ?> <?php echo($span); ?>" shc:gizmo="carousel" shc:gizmo:options="{viewportsize:<?php echo($pw_fields_to_show); ?>,itemcount:<?php echo($total); ?>,autoSlideInterval:2000}">
 	<div class="left-arrow"></div>
 	<div class="product-slider-container layout_<?php echo($layout); ?>">
-		<?php foreach($data->products as $d) { ?>
-		<div class="product <?php if($count <= 3) { ?>inactive-left<?php } else if($count <= 6) { ?>active<?php } else { ?>inactive-right <?php } ?>">
+		<?php foreach($prodRows as $d) { ?>
+		<div class="product <?php if($count <= $flNumber) { ?>inactive-left<?php } else if($count <= $flNumber + $pw_fields_to_show) { ?>active<?php } else { ?>inactive-right <?php } ?>">
 			<a href="<?php echo($d->guid); ?>"><img src="<?php echo((!empty($d->meta->imageurls)) ? $d->meta->imageurls[0] : ""); ?>&wid=115&op_sharpen=1" /></a>
 			<p class="product-title"><a href="<?php echo($d->guid); ?>"><?php echo($d->post_title); ?></a></p>
 			<?php for($i=1; $i<=5; $i++) { ?>
