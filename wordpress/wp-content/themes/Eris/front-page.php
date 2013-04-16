9fa5491a75088c1097b6d05f1b6b8b776ba84353
Jason Corradino <jason@ididntbreak.it>
2013-04-16 09:59:04 -0500

    removing random widgets hard coded in homepage

--- wordpress/wp-content/themes/Eris/front-page.php
+++ wordpress/wp-content/themes/Eris/front-page.php
@@ -1,16 +1,13 @@
 <?php //echo '<!-- SECTION TEMPLATE -->';
 get_template_part('parts/header');
 
-// global $excerptLength;
+global $excerptLength;
 
-// $excerptLength = 95;
+$excerptLength = 95;
 
-// if(function_exists('display_dropzones')){
-//     display_dropzones();
-// }
-
-get_template_part('widgets/product-slider/slider');
-get_template_part('widgets/product-slider/slider');
+if(function_exists('display_dropzones')){
+    display_dropzones();
+}
 
 get_template_part('parts/footer');
 
