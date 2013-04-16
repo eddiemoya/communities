10cb5e05df69f5e11a2807d2b4e64b7dd9e616da
Eddie Moya <eddie.moya@gmail.com>
2013-04-15 22:31:49 -0500

    modifying product widget template and scss files to add a viewport element to product widget

--- widgets/product-widget/slider.php
+++ widgets/product-widget/slider.php
@@ -1,34 +1,54 @@
 <?php
-	$count = 1;
-	$span = ($pw_template == "products-vertical") ? "span12" : "span12";
-	$layout = ($pw_template == "products-vertical") ? "vertical" : "horizontal";
-	$animation = $instance['pw_animation_duration'];
-	$total = count($data->products);
 
-	$prodRows = $data->products;
-	$flNumber = 1;
+$count = 1;
+$span = ($pw_template == "products-vertical") ? "span12" : "span12";
+$layout = ($pw_template == "products-vertical") ? "vertical" : "horizontal";
+$animation = $instance['pw_animation_duration'];
+$animation = (!empty($animation)) ?  ",autoSlideInterval:$animation": "" ;
+$total = count($data->products);
 
-	for($i=0; $i<$flNumber; $i++)
-	{
-		$ar = array_pop($prodRows);
-		array_unshift($prodRows, $ar);
-	}
-	
-?>
+$prodRows = $data->products;
+$flNumber = 2;
 
-<?php if ($instance["pw_title"] != "") { ?>
+
+
+for($i=0; $i<$flNumber; $i++)
+{
+	$ar = array_pop($prodRows);
+	array_unshift($prodRows, $ar);
+}
+
+
+
+
+ if ($instance["pw_title"] != "") { ?>
 	<hgroup class="content-header">
 		<h3><?php echo $instance["pw_title"]; ?></h3>
 	</hgroup>
 <?php } ?>
 
 <section class="content-body product-slider-widget-content clearfix viewport_size_<?php echo($pw_fields_to_show); ?> <?php echo($span); ?>" shc:gizmo="carousel" 
-	shc:gizmo:options="{viewportsize:<?php echo($pw_fields_to_show); ?>,itemcount:<?php echo($total); ?>,autoSlideInterval:<?php echo $animation; ?>}">
+	shc:gizmo:options="{viewportsize:<?php echo($pw_fields_to_show); ?>,itemcount:<?php echo($total); ?><?php echo $animation; ?>}">
 
 	<div class="left-arrow"></div>
 	<div class="product-slider-container layout_<?php echo($layout); ?>">
+
+		<div class="product-slider-viewport span12">
+
+
 		<?php foreach($prodRows as $d) { ?>
-		<div class="product <?php if($count <= $flNumber) { ?>inactive-left<?php } else if($count <= $flNumber + $pw_fields_to_show) { ?>active<?php } else { ?>inactive-right <?php } ?>">
+
+		<?php
+			if($count <= $flNumber) { 
+				$class = "inactive-left";
+			} else if($count <= $flNumber + $pw_fields_to_show+2) { 
+				$class = "active" ;
+			} else { 
+				$class = "inactive-right";
+			}
+		?>
+
+		<div class="product <?php echo $class; ?> ">
 			<a href="<?php echo($d->guid); ?>"><img src="<?php echo((!empty($d->meta->imageurls)) ? $d->meta->imageurls[0] : ""); ?>&wid=115&op_sharpen=1" /></a>
 			<p class="product-title"><a href="<?php echo($d->guid); ?>"><?php echo($d->post_title); ?></a></p>
 			<?php for($i=1; $i<=5; $i++) { ?>
@@ -43,6 +63,8 @@
 			</p>
 		</div>
 		<?php $count++;} ?>
+
+	</div>
 	</div>
 	<div class="right-arrow"></div>
 </section>
