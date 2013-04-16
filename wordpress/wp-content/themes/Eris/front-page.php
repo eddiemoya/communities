9a767346f76fce4dda17bbd601cc831f7fce4973
Eddie Moya <eddie.moya@gmail.com>
2013-04-16 17:32:22 -0500

    removing front end testing for slider on the front page

--- wordpress/wp-content/themes/Eris/front-page.php
+++ wordpress/wp-content/themes/Eris/front-page.php
@@ -9,8 +9,8 @@ if(function_exists('display_dropzones')){
     display_dropzones();
 }
 
-get_template_part('widgets/product-slider/slider');
-get_template_part('widgets/product-slider/slider');
+// get_template_part('widgets/product-slider/slider');
+// get_template_part('widgets/product-slider/slider');
 
 get_template_part('parts/footer');
 
