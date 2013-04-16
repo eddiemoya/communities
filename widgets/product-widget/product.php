646abf70f1685c085656670de85c58bce44b97e1
Dan Crimmins <dbcrimmins@gmail.com>
2013-04-16 17:23:42 -0500

    changed url on image to PDP and diplay of saleprice

--- widgets/product-widget/product.php
+++ widgets/product-widget/product.php
@@ -1,11 +1,9 @@
 <?php 
-	//$product_url = $d->guid;
-	//$product_url = "http://www.sears.com/{$d->post_name}/p-{$d->meta->partnumber}";
 	$product_url = (((stripos(get_bloginfo('name'), 'sears')) !== false) ? 'http://www.sears.com/' : 'http://www.kmart.com/') . $d->meta->product_uri;
 ?>
 
 <div class="product <?php echo $class; ?>" data-pid="<?php echo $d->ID; ?>">
-	<a href="<?php echo($d->guid); ?>"><img src="<?php echo((!empty($d->meta->imageurls)) ? $d->meta->imageurls[0] : ""); ?>?&wid=115&op_sharpen=1" /></a>
+	<a href="<?php echo $product_url; ?>"><img src="<?php echo((!empty($d->meta->imageurls)) ? $d->meta->imageurls[0] : ""); ?>?&wid=115&op_sharpen=1" /></a>
 	<p class="product-title">
 		<a href="<?php echo $product_url; ?>">
 			<?php echo($d->post_title); ?>
@@ -15,7 +13,7 @@
 		<span class="product-stars star-<?php echo(($i > $d->meta->rating) ? "de" : ""); ?>selected"></span>
 	<?php } ?>
 	<p class="product-price">
-		<?php if(!empty($d->meta->saleprice)) { ?>
+		<?php if(!empty($d->meta->saleprice) && ($d->meta->saleprice != $d->meta->regularprice)) { ?>
 			$<?php echo($d->meta->saleprice); ?> <span class="old-price">$<?php echo($d->meta->regularprice); ?></span>
 		<?php } else { ?>
 			$<?php echo($d->meta->regularprice); ?>
