03b31e12d69067e87f8074e4afe1ce4ee26df995
Matt Day <mday1@searshc.com>
2013-04-18 11:56:16 -0500

    Integrated front end taxonomy widget

--- widgets/taxonomy_widget/taxonomy-featured.php
+++ widgets/taxonomy_widget/taxonomy-featured.php
@@ -1,7 +1,16 @@
-<div>
-	<h4><a href="/link/to/<?php echo($data['info']->slug); ?>"><?php echo($data['info']->name); ?></a></h4>
-	<h6>Taxonomy: <?php echo($data['info']->taxonomy); ?></h6>
-	<?php if(!empty($data['info']->description)) { ?>
-		<div><?php echo($data['info']->description); ?></div>
-	<?php } ?>
+<div class="featured-image span3">
+	<img title="<?php echo($data['info']->name); ?>" alt="<?php echo($data['info']->name); ?>" src="<?php echo((!empty($data['imgs'])) ? $data['imgs'][0]->guid : "http://path/to/noimg.png"); ?>">        
+</div>
+
+<div class="span9 featured-content">
+
+    <h1 class="content-headline">
+        <a href="<?php echo($data['post']->guid); ?>" title="<?php echo($data['post']->post_title); ?>"><?php echo($data['post']->post_title); ?></a>
+    </h1>
+
+	<!--
+		You will have to add the <sarcasm>always helpful, and very informative</sarcasm> 'see more' link on the back end.
+	-->
+
+	<?php echo($data['post']->post_content); ?>
 </div>
