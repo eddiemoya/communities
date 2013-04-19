cdc3360384ae99482ae2177f7ac7b5780988be7d
Matt Day <mday1@searshc.com>
2013-04-19 15:26:40 -0500

    Added templates for grid/list

---
+++ wordpress/wp-content/themes/Eris/widgets/taxonomy_widget/taxonomy-grid.php
@@ -0,0 +1,39 @@
+<header class="content-header">
+	<h3>Departments</h3>
+</header>
+
+<section class="content-body clearfix">
+<?php $ctr = 0; ?>
+<ul class="span12">
+<?php foreach($disp as $k => $d) { ?>
+	<li class="span3<?php if(($ctr % 4) == 0) { ?> alpha<?php } ?>">
+		<a href="<?php echo($d['link']); ?>" title="<?php echo($k); ?>"><img src="<?php echo($d['img']); ?>" alt="<?php echo($k); ?>" /></a>
+		<h5><a href="<?php echo($d['link']); ?>" title="<?php echo($k); ?>"><?php echo($k); ?></a></h5>
+	</li>
+<?php $ctr++; } ?>
+</ul>
+	
+<?php if(!empty($drop) && $dpdn) { ?>
+	<div class="span12 taxonomy-drop-down-menu">
+		<select class="input_select" name="department" shc:gizmo="drop-menu">
+			<option value="">--Click for <?php echo($ftxt); ?></option>
+			<?php foreach($drop as $k => $d) { ?>
+				<option value="<?php echo($d['link']); ?>"><?php echo($k); ?></option>
+			<?php } ?>
+		</select>
+	</div>
+<?php $ctr = 0; ?>
+	<?php if(!empty($nsts)) { ?>
+	<noscript>
+		<ul class="span12">
+			<?php foreach($nsts as $k => $d) { ?>
+			<li class="span3<?php if(($ctr % 4) == 0) { ?> alpha<?php } ?>">
+				<a href="<?php echo($d['link']); ?>" title="<?php echo($k); ?>"><img src="<?php echo($d['img']); ?>" alt="<?php echo($k); ?>" /></a>
+				<h5><a href="<?php echo($d['link']); ?>" title="<?php echo($k); ?>"><?php echo($k); ?></a></h5>
+			</li>
+			<?php $ctr++;} ?>
+		</ul>
+	</noscript>
+	<?php } ?>
+<?php } ?>
+</section>
