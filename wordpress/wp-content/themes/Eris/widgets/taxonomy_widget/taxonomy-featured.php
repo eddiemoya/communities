7b6ccb30b8d22f34db164b9a710a5402ac9aaf3a
Matt Day <mday1@searshc.com>
2013-04-29 09:44:02 -0500

    Added featured taxonomy widget template

--- wordpress/wp-content/themes/Eris/widgets/taxonomy_widget/taxonomy-featured.php
+++ wordpress/wp-content/themes/Eris/widgets/taxonomy_widget/taxonomy-featured.php
@@ -1,18 +1,19 @@
+<header class="content-header">
+	<h3>Featured Category</h3>
+</header>
 <section class="content-body clearfix">
-<div class="featured-image span3">
-	<img title="<?php echo($data['info']->name); ?>" alt="<?php echo($data['info']->name); ?>" src="<?php echo((!empty($data['imgs'])) ? $data['imgs'][0]->guid : "http://path/to/noimg.png"); ?>">        
-</div>
 
-<div class="span9 featured-content">
+	<div class="featured-image span6">
+		<img title="<?php echo($data['info']->name); ?>" alt="<?php echo($data['info']->name); ?>" class="attachment-large wp-post-image" src="<?php echo((!empty($data['imgs'])) ? $data['imgs'][0]->guid : "http://path/to/noimg.png"); ?>">
+	</div>
 
-    <h1 class="content-headline">
-        <a href="<?php echo($data['link']); ?>" title="<?php echo($data['info']->name); ?>"><?php echo($data['info']->name); ?></a>
-    </h1>
+	<div class="featured-content span6">
 
-	<!--
-		You will have to add the <sarcasm>always helpful, and very informative</sarcasm> 'see more' link on the back end.
-	-->
-
+	<div class="content-details clearfix">
+		<span class="content-category"><a title="<?php echo($data['info']->name); ?>" href="<?php echo($data['link']); ?>"><?php echo($data['info']->name); ?></a></span>
+		<time datetime="<?php echo(strftime('%Y-%m-%d', time())); ?>" pubdate="" class="content-date"><?php echo(strftime('%B %d, %Y', time())); ?></time>
+	</div>
 	<?php echo($data['info']->description); ?>
-</div>
+	
+	</div>
 </section>
