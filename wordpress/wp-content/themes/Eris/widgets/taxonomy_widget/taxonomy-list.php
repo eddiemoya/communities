f4f075086dd9e29a6d7c4355ff9bbfbbbeb34548
Matt Day <mday1@searshc.com>
2013-04-29 15:34:16 -0500

    Fixed formatting issue for taxonomy widget

--- wordpress/wp-content/themes/Eris/widgets/taxonomy_widget/taxonomy-list.php
+++ wordpress/wp-content/themes/Eris/widgets/taxonomy_widget/taxonomy-list.php
@@ -1,27 +1,22 @@
-<header class="content-header">
-	<h3>Departments</h3>
-</header>
-
-<section class="content-body clearfix">
-
-<ul class="span12">
 <?php foreach($disp as $k => $d) { ?>
-	<div class="featured-image span3">
-		<img title="<?php echo($k); ?>" alt="<?php echo($k); ?>" src="<?php echo($d['img']); ?>">        
-	</div>
+	<article class="content-container department">	
+		<section class="content-body clearfix">
+			<div class="featured-image span3">
+				<img title="<?php echo($k); ?>" alt="<?php echo($k); ?>" src="<?php echo($d['img']); ?>">        
+			</div>
 
-	<div class="span9 featured-content">
+			<div class="span9 featured-content">
 
-	    <h1 class="content-headline">
-	        <a href="<?php echo($d['link']); ?>" title="<?php echo($k); ?>"><?php echo($k); ?></a>
-	    </h1>
+			    <h1 class="content-headline">
+			        <a href="<?php echo($d['link']); ?>" title="<?php echo($k); ?>"><?php echo($k); ?></a>
+			    </h1>
 
-		<!--
-			You will have to add the <sarcasm>always helpful, and very informative</sarcasm> 'see more' link on the back end.
-		-->
+				<!--
+					You will have to add the <sarcasm>always helpful, and very informative</sarcasm> 'see more' link on the back end.
+				-->
 
-		<?php echo($d['desc']); ?>
-	</div>
+				<?php echo($d['desc']); ?>
+			</div>
+		</section>				
+	</article>
 <?php } ?>
-</ul>
-</section>
