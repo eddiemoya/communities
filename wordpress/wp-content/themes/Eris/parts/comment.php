d4b17903be9ac16b538be2ff597604c9f1c79389
mday <mday@mday-VirtualBox.(none)>
2013-06-14 15:33:03 -0500

    Modifications for templating

--- wordpress/wp-content/themes/Eris/parts/comment.php
+++ wordpress/wp-content/themes/Eris/parts/comment.php
@@ -1,5 +1,6 @@
 <?php
     $removed_text = "<p>This {$comment_type} has been removed.</p>";
+    $brand = theme_option("brand") ? theme_option("brand") : 'sears';
 ?>
 <li class="comment clearfix<?php echo $container_class; ?>" id="<?php echo $comment_type.'-reply-'.$comment->comment_ID ?>">
     <?php 
@@ -15,14 +16,17 @@
                         <?php echo date( "F j, Y", $date ); ?>
                         <span class="time-stamp"><?php echo date( "g:ia", $date ); ?></span>
                     </time>
-                </div>
+                </div><?php //print_pre($comment); ?>
                 <?php if ($parent_author != "") : ?>
                     <p class="responseTo">In response to <?php echo $parent_author; ?></p>
                 <?php endif; ?>
                 <?php echo ($comment->comment_approved == 1) ? wpautop($comment->comment_content) : $removed_text; ?>
-                <?php get_partial('parts/vote', array('id' => $comment->comment_ID, 'type' => "comment", 'actions' => $comment->actions)); ?>
-                <?php get_partial('parts/follow', array('id' => $comment->comment_ID, 'type' => "comment")); ?>
-                <?php get_partial('parts/share', array('id' => $comment->comment_ID, 'type' => "comment")); ?>
+                <form class="actions clearfix" id="comment-actions-<?php echo($comment->comment_ID); ?>" method="post">
+					<?php get_partial('parts/vote', array('id' => $comment->comment_ID, 'type' => "comments", 'sub_type' => 'comment', 'actions' => $comment->actions, 'logged_in' => is_user_logged_in())); ?>
+					<?php get_partial('parts/follow', array('id' => $comment->comment_ID, 'type' => "comments")); ?>
+					<?php get_partial('parts/share', array('id' => $comment->comment_ID, 'type' => "comments")); ?>
+					<?php get_partial('parts/flag', array('id' => $comment->comment_ID, 'type' => "comments", 'sub_type' => "comment", 'actions' => $comment->actions, 'brand' => $brand)); ?>
+                </form>
             </section>
         </article> <!-- END ARTICLE CONTENT CONTAINER -->
     
--- wordpress/wp-content/themes/Eris/parts/follow.php
+++ wordpress/wp-content/themes/Eris/parts/follow.php
@@ -1,1 +1,4 @@
-<button type="button" name="button1" value="follow" id="follow-question-<?php echo $id; ?>" title="Follow this <?php echo $type; ?>" class="follow">Follow</button>
+<button type="button" name="button1" value="follow" id="follow-question-<?php echo $id; ?>" title="Follow this <?php echo $type; ?>" class="follow"
+shc:gizmo:options="{moodle:{width:480,target:ajaxdata.ajaxurl,type:'POST',data:{action:'get_template_ajax',template:'page-login'}}}"
+shc:gizmo="moodle"
+>Follow</button>
