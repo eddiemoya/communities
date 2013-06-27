489e46929802d44099fa341852ef4ea8f8acb4c6
Eddie Moya <eddie.moya@gmail.com>
2013-06-26 19:20:13 -0500

    updating single.php with calls to widgetpress and wp_node

--- wordpress/wp-content/themes/Eris/single.php
+++ wordpress/wp-content/themes/Eris/single.php
@@ -3,8 +3,28 @@
  * @package WordPress
  * @subpackage White Label
  */
-get_template_part('parts/header');
 
-loop('post');
+get_template_part('parts/header');
+		
+			$term = wp_get_object_terms($post->ID, $post->post_type);
+			$term = $term[0];
+			 $node = new WP_Node($term->term_id, $term->taxonomy);
+
+			query_posts(
+				array(
+					'post_type' => 'post',
+					'tax_query' => array(
+						array(
+							'taxonomy' => $term->taxonomy,
+							'terms' => $term->term_id,
+							'field' => 'id'
+						)
+					)
+					)
+				);
+			WidgetPress_Controller_Widgets::display_dropzones($node->post->ID);
+			wp_reset_query();
+			
+
 
 get_template_part('parts/footer');
