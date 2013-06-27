2db9bb7ebdf51221125561ba944e961469a8dd7a
Eddie Moya <eddie.moya@gmail.com>
2013-06-26 23:20:46 -0500

    putting the single requests for node objects into the hands of the template hierarchy

--- wordpress/wp-content/themes/Eris/single-category.php
+++ wordpress/wp-content/themes/Eris/single-category.php
@@ -2,26 +2,28 @@
 
 get_template_part('parts/header');
 
-			global $post;
-			//print_pre($post);
-			$term = wp_get_object_terms($post->ID, $post->post_type);
-			$term = $term[0];
-			 $node = new WP_Node($term->term_id, $term->taxonomy);
+	$term = wp_get_object_terms($post->ID, $post->post_type);
+	$term = $term[0];
+	$node = new WP_Node($term->term_id, $term->taxonomy);
 
-			query_posts(
+	$layout_setting = $node->get_meta_data('section_front_layout');
+
+	print_pre($layout_setting);
+	$layout_id = ($layout_setting > 0 ) ? $layout_setting : $node->post->ID;
+
+	query_posts(
+		array(
+			'tax_query' => array(
 				array(
-					'post_type' => 'post',
-					'tax_query' => array(
-						array(
-							'taxonomy' => $term->taxonomy,
-							'terms' => $term->term_id,
-							'field' => 'id'
-						)
-					)
-					)
-				);
-			WidgetPress_Controller_Widgets::display_dropzones($node->post->ID);
-			wp_reset_query();
+					'taxonomy' => $term->taxonomy,
+					'terms' => $term->term_id,
+					'field' => 'id'
+				)
+			)
+			)
+		);
+	WidgetPress_Controller_Widgets::display_dropzones($layout_id);
+	wp_reset_query();
 			
 
 
