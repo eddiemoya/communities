203732abadb16c78a659b27719e2e374a8679186
Eddie Moya <eddie.moya@gmail.com>
2013-06-28 11:20:33 -0500

    adding sf_filter query var and rewrite rules. this needs to be in section fronts but fuck it.

--- wordpress/wp-content/themes/Eris/functions/rewrite-rules.php
+++ wordpress/wp-content/themes/Eris/functions/rewrite-rules.php
@@ -5,12 +5,24 @@ add_filter('category_rewrite_rules', 'category_post_type_endpoints');
 add_filter('post_rewrite_rules', 'posts_endpoint');
 add_filter('post_format_rewrite_rules', 'post_format_endpoint');
 
+add_filter('query_vars', 	 'add_vars');
+
+	function add_vars($qvars) {
+		//$qvars[] = 'meta_key';
+		$qvars[] = 'sf_filter';
+
+
+		return $qvars;
+	}
+
+
 
 add_filter('rewrite_rules_array', 'rewrite_rules');
 
 function rewrite_rules($rules){
-	//print_pre($rules);
-	return $rules;
+	//$newrules['category/(.+?)/video$'] = 'index.php?post_type=category&name=$matches[1]';
+$newrules['category/(.+?)/(guide|question|post|video)/?$'] = 'index.php?post_type=category&name=$matches[1]&sf_filter=$matches[2]';
+	return $newrules + $rules;
 }
 /**
  * Add endpoint for posttype archive for the POST post type.
@@ -32,7 +44,9 @@ function post_format_endpoint($rules){
 function posts_endpoint($rules){
 
 	$newrules = array();
+	
 	$newrules['posts?/?$'] = 'index.php?post_type=post';
+
 	//$newrules['video?/?$'] = 'index.php?post_format=video';
 
 	//echo "<pre>";print_r($newrules + $rules);echo "</pre>";
@@ -44,17 +58,18 @@ function posts_endpoint($rules){
  */
 function category_post_type_endpoints($rules){
 
- //    $newrules = array();
-	// $newrules['category/(.+?)/(guide|question|post)s?/?$'] = 'index.php?category_name=$matches[1]&post_type=$matches[2]';
- //    $newrules['category/(.+?)/(guide|question|post)s?/page/?([0-9]{1,})/?$'] = 'index.php?category_name=$matches[1]&post_type=$matches[2]&paged=$matches[3]';
- //    $newrules['category/(.+?)/videos?/?$'] = 'index.php?category_name=$matches[1]&post_format=video';
+    $newrules = array();
+	$newrules['category/(.+?)/(guide|question|post|video)/?$'] = 'index.php?post_type=category&name=$matches[1]&sf_filter=$matches[2]';
+
+    // $newrules['category/(.+?)/(guide|question|post)s?/page/?([0-9]{1,})/?$'] = 'index.php?category_name=$matches[1]&post_type=$matches[2]&paged=$matches[3]';
+    // $newrules['category/(.+?)/videos?/?$'] = 'index.php?category_name=$matches[1]&post_format=video';
 	//print_pre($newrules + $rules);
 	//return $newrules + $rules;
 
 	#The new Section Fronts plugin requires that we remove the default rewrite rules for a given taxonomy.
 	#This allows the request to "fall through"  to the post types rewrite rule. The request for taxonomy is
 	#then recreated.
-	return array();
+	return $newrules + $rules;
 
 }
 
