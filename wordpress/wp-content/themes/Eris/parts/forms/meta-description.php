5a56d43b482cb8c42d556e4ba9019240cbb993bb
Eddie Moya <eddie.moya@gmail.com>
2013-07-02 15:43:11 -0500

    new partial template for admin form field for meta description

---
+++ wordpress/wp-content/themes/Eris/parts/forms/meta-description.php
@@ -0,0 +1,9 @@
+		<tr class="form-field">
+			<th scope="row" valign="top"><label for="<?php echo $label; ?>"><?php echo $label; ?></label></th>
+			<td>
+				<textarea name="<?php echo $name;?>" id="<?php echo $css_id;?>" rows="5" cols="50" style="width: 97%;"><?php 
+				echo $value; 
+				?></textarea><br />
+
+			</td>
+		</tr>
