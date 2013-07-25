2fe248ed3b04c69042649c8ccda75673b4b3877b
root <root@mday-VirtualBox.(none)>
2013-07-25 22:04:01 +0000

    Fixed fix for Pagination fix

--- wordpress/wp-content/themes/Eris/widgets/results-list/archive.php
+++ wordpress/wp-content/themes/Eris/widgets/results-list/archive.php
@@ -5,13 +5,17 @@ if(!is_ajax())
 	get_template_part('parts/header', 'results-list');
 }
 
-$search_terms = (!empty($_GET['s'])) ? trim($_GET['s']) : "";
+$_GET['s'] = trim($_GET['s']);
 
 $post_type = get_query_var('post_type');
 $post_type = (is_array($post_type)) ? $post_type[0] : $post_type;
 $post_type = (isset($_GET['s'])) ? 'question' : $post_type;
 
-if(!empty($search_terms))
+if(isset($_GET['s']) && empty($_GET['s']))
+{
+	get_template_part('parts/no-results');
+}
+else
 {
 	loop(array($post_type, 'post'), null, 'widgets/results-list', 'parts/no-results');
 	
@@ -21,10 +25,6 @@ if(!empty($search_terms))
 		echo posts_nav_link();
 		echo '</section>';
 	}
-}
-else
-{
-	get_template_part('parts/no-results');
 }
 
 if(!is_ajax())
