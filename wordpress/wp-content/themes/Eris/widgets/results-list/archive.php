b6aaa1f46c644e95417e5f89726e29d2da3ce082
mday <mday@mday-VirtualBox.(none)>
2013-07-26 20:44:40 +0000

    Fix for fix for fix for pagination fix

--- wordpress/wp-content/themes/Eris/widgets/results-list/archive.php
+++ wordpress/wp-content/themes/Eris/widgets/results-list/archive.php
@@ -5,13 +5,13 @@ if(!is_ajax())
 	get_template_part('parts/header', 'results-list');
 }
 
-$_GET['s'] = trim($_GET['s']);
+$search_terms = isset($_GET['s'])) ? trim($_GET['s']) : "";
 
 $post_type = get_query_var('post_type');
 $post_type = (is_array($post_type)) ? $post_type[0] : $post_type;
 $post_type = (isset($_GET['s'])) ? 'question' : $post_type;
 
-if(isset($_GET['s']) && empty($_GET['s']))
+if(isset($_GET['s']) && empty($search_terms))
 {
 	get_template_part('parts/no-results');
 }
