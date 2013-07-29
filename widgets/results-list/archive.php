044e774806d669e9873591cce4689725d2c0cebd
mday <mday@mday-VirtualBox.(none)>
2013-07-29 16:51:49 +0000

    Fix for final fix

--- widgets/results-list/archive.php
+++ widgets/results-list/archive.php
@@ -1,36 +1,35 @@
 <?php 
 
-if(!is_ajax()) {
-	
-    get_template_part('parts/header', 'results-list');
+if(!is_ajax()) 
+{
+	get_template_part('parts/header', 'results-list');
 }
 
-    $post_type = get_query_var('post_type');
-    $post_type = (is_array($post_type)) ? $post_type[0] : $post_type;
+$search_terms = isset($_GET['s']) ? trim($_GET['s']) : "";
 
-    $post_type = (isset($_GET['s'])) ? 'question' : $post_type;
+$post_type = get_query_var('post_type');
+$post_type = (is_array($post_type)) ? $post_type[0] : $post_type;
+$post_type = (isset($_GET['s'])) ? 'question' : $post_type;
 
+if(isset($_GET['s']) && empty($search_terms))
+{
+	get_template_part('parts/no-results');
+}
+else
+{
+	loop(array($post_type, 'post'), null, 'widgets/results-list', 'parts/no-results');
+	
+	if(have_posts())
+	{
+		echo '<section class="pagination">';
+		echo posts_nav_link();
+		echo '</section>';
+	}
+}
 
-    if(isset($_GET['s']) && trim($_GET['s']) == '') {
+if(!is_ajax())
+{
+	get_template_part('parts/footer', 'widget');
+}
 
-
-    	get_template_part('parts/no-results');
-
-    } else {
-
-    	loop(array($post_type, 'post'), null, 'widgets/results-list', 'parts/no-results');
-    }
-
-
-if(!is_ajax()){
-    get_template_part('parts/footer', 'widget');
-
-
-
-}?>
-
-<?php if(have_posts()) : ?>
-<section class="pagination">
-     <?php echo posts_nav_link(); ?>
-</section>
-<?php endif; ?>
+?>
