1f99fc4b15aa70dfa8da7d1e4f6680996b74fc83
Eddie Moya <eddie.moya@gmail.com>
2013-08-01 16:51:52 -0500

    updating meta description and moving sections template around

---
+++ wordpress/wp-content/themes/Eris/section-front.php
@@ -0,0 +1,53 @@
+<?php
+
+get_template_part('parts/header');
+	
+	$term = wp_get_object_terms($post->ID, $post->post_type);
+	$term = $term[0];
+	$node = new WP_Node($term->term_id, $term->taxonomy);
+
+	$filter = get_query_var('sf_filter');
+	
+	$layout_id = $node->get_meta_data("sf_{$filter}_template");
+	//$layout_id = ($layout_setting > 0 ) ? $layout_setting : $node->post->ID;
+
+	$tax_query[] = array(
+		'taxonomy' => $node->term->taxonomy,
+		'terms' => $node->term->term_id,
+		'field' => 'id'
+	);
+
+	$post_type = '';
+	switch($filter){
+		case 'post': 
+		case 'guide':
+		case 'question':
+			$post_type = $filter; 
+			break;
+		case 'category':
+			$post_type = array('post', 'guide', 'question');
+			break;
+		case 'video':
+			$tax_query[] = array(
+				'taxonomy' => 'post_format',
+				'terms' => $filter,
+				'field' => 'slug'
+			);
+			$tax_query['relation'] = 'AND';
+			$post_type = array('post', 'guide', 'question');
+		break;
+	}
+
+	$query = array(
+		'post_type' => $post_type,
+		'tax_query' => $tax_query
+	);
+
+	query_posts($query);
+	
+	WidgetPress_Controller_Widgets::display_dropzones($layout_id);
+	wp_reset_query();
+			
+
+
+get_template_part('parts/footer');
