cdcf14707079eebaffdd60a0b0e5f302ace9125f
Eddie Moya <eddie.moya@gmail.com>
2013-08-01 17:03:51 -0500

    fixing default post rewrite slug

--- wordpress/wp-content/themes/Eris/functions/post-types.php
+++ wordpress/wp-content/themes/Eris/functions/post-types.php
@@ -164,3 +164,23 @@ add_filter('wpdz_dropzones', 'custom_excerpt_length', 9);
 //     $wp_post_types['page']->menu_position = 4;
 //     echo "<pre>";print_r($wp_post_types['page']->menu_position);echo "</pre>";
 // }
+
+
+add_action( 'init', 'default_post_type', 1 );
+function default_post_type() {
+
+    register_post_type( 'post', array(
+        'labels' => array(
+            'name_admin_bar' => _x( 'Post', 'add new on admin bar' ),
+        ),
+        'public'  => true,
+        '_builtin' => false, 
+        '_edit_link' => 'post.php?post=%d', 
+        'capability_type' => 'post',
+        'map_meta_cap' => true,
+        'hierarchical' => false,
+        'rewrite' => array( 'slug' => 'post' ),
+        'query_var' => false,
+        'supports' => array( 'title', 'editor', 'author', 'thumbnail', 'excerpt', 'trackbacks', 'custom-fields', 'comments', 'revisions', 'post-formats' ),
+    ) );
+}
