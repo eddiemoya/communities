3144cece11b4493fd3abdc451812118c91f23424
Dan Crimmins <dbcrimmins@gmail.com>
2013-09-04 14:35:01 -0500

    added all templates for forums to Eris, completed all mods except to replies templates

---
+++ wordpress/wp-content/themes/Eris/pagination-forums.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Pagination for pages of forums
+ *
+ * @package bbPress
+ * @subpackage Theme
+ */
+
+?>
+
+<div class="bbp-pagination">
+	<div class="bbp-pagination-links">
+
+		<?php bbp_forums_pagination(bbp_get_forum_id());?>
+
+	</div>
+</div>
+
