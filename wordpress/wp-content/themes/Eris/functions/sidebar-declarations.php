7e9334c316ab5b45a4533f713037a213c51871d0
Dan Crimmins <dbcrimmins@gmail.com>
2013-09-06 08:55:09 -0500

    added sidebars, heading breadcrumbs

--- wordpress/wp-content/themes/Eris/functions/sidebar-declarations.php
+++ wordpress/wp-content/themes/Eris/functions/sidebar-declarations.php
@@ -3,7 +3,7 @@
  * Register Widgetized Areas 
  */
 if (function_exists('register_sidebar')) {
-    register_sidebar(array(
+   /* register_sidebar(array(
         'name' => 'Q&A Sidebar',
         'description' => 'Sidebar',
         'before_widget' => '<article class="span12 content-container widget dropzone-widget widget">',
@@ -52,6 +52,46 @@ if (function_exists('register_sidebar')) {
         'after_widget' => '</article>',
         'before_title' => '<header class="content-header"><h3 class="widgettitle">',
         'after_title' => '</h3></header>'
+    ));*/
+	
+	/**
+	 * Forums Sidebars
+	 */
+	
+	register_sidebar(array(
+        'name' => 'Forums Archive Sidebar',
+        'description' => 'Sidebar',
+        'before_widget' => '<div class="span12 content-container widget dropzone-widget widget">',
+        'after_widget' => '</div>',
+        'before_title' => '<h3 class="widgettitle">',
+        'after_title' => '</h3>'
+    ));
+    
+    register_sidebar(array(
+        'name' => 'Forum Topics Sidebar',
+        'description' => 'Sidebar',
+        'before_widget' => '<div class="span12 content-container widget dropzone-widget widget">',
+        'after_widget' => '</div>',
+        'before_title' => '<h3 class="widgettitle">',
+        'after_title' => '</h3>'
+    ));
+    
+    register_sidebar(array(
+        'name' => 'Forum Threads Sidebar',
+        'description' => 'Sidebar',
+        'before_widget' => '<div class="span12 content-container widget dropzone-widget widget">',
+        'after_widget' => '</div>',
+        'before_title' => '<h3 class="widgettitle">',
+        'after_title' => '</h3>'
+    ));
+    
+    register_sidebar(array(
+        'name' => 'Forum Thread Archive Sidebar',
+        'description' => 'Sidebar',
+        'before_widget' => '<div class="span12 content-container widget dropzone-widget widget">',
+        'after_widget' => '</div>',
+        'before_title' => '<h3 class="widgettitle">',
+        'after_title' => '</h3>'
     ));
     
 }
