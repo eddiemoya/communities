9b51b7873f994e3aaae57a7a2f46d4b47650dd22
Matt Strick <matthew.strick@searshc.com>
2013-09-16 23:59:31 -0500

    more misc. styling for the forums section.

--- wordpress/wp-content/themes/Eris/forums-head.php
+++ wordpress/wp-content/themes/Eris/forums-head.php
@@ -10,7 +10,7 @@
 
 <div id="forums-head">
 	<?php if(bbp_is_forum_archive()):?>
-	
+
 		<h1><?php echo get_option('_bbp_forum_archive_heading', 'Forums Header Text');?></h1>
 		<p><?php echo get_option('_bbp_forum_archive_subheading', 'Forums Sub-header Text');?></p>
 		
