824e9d22c7bfab6e6315dee81b7f1356ccd972dc
Matt Strick <matthew.strick@searshc.com>
2013-09-17 17:31:21 -0500

    more styling for the tooltip.

--- wordpress/wp-content/themes/Eris/assets/js/forums-tooltip.js
+++ wordpress/wp-content/themes/Eris/assets/js/forums-tooltip.js
@@ -1,15 +1,26 @@
 // Welcome Tooltip work
 (function ($) {
-	// Open the tooltip if no cookie is set
+	// Open the tooltip if not cookie is set
 	/* TODO: Check cookie and open the tooltip if appropriate*/
-	
-	// Close the tooltip on click
-	$('#forum_tooltip .close').on('click', function() { 
-		/* TODO:  Check the form value to see whether to set a session cookie or the full 90 day one.*/
+	if (true) {
 		
-		// Hide the tooltip
-		$(this).parent().hide(); 
-	});
 	
+		// Close the tooltip on click
+		$('#forum_tooltip .close').on('click', function() { 
+			var input = $(this).parent().find('#message_hide');
+
+			/* TODO:  Check the form value to see whether to set a session cookie or the full 90 day one.*/
+			if (input.attr('checked')) {
+				// set forever cookie
+			} else {
+				// set session cookie
+			}
+			
+			// Hide the tooltip
+			$(this).parent().hide(); 
+		});	
+	} else {	// Remove the tooltip.
+		$('#forum_tooltip').remove();
+	}
 	
 }(jQuery));
