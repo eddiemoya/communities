9d6dd6698002670557d6268bb06ab61fbcb4845d
Eddie Moya <eddie.moya@gmail.com>
2013-09-17 12:04:16 -0500

    adding jquery ajax-request trigger to get post when forum reply link is clicked

--- wordpress/wp-content/themes/Eris/assets/js/ajax-requests.js
+++ wordpress/wp-content/themes/Eris/assets/js/ajax-requests.js
@@ -144,7 +144,31 @@ jQuery(document).ready(function($) {
 			});
 
 		}
-	})
+	});
+
+	$('#forums-topic-reply-link a').on('click', function(){
+
+   		container = $(this).closest('.reply');
+
+   		var data = {
+			action		: 'get_post_by_id',
+			post_type 	: 'reply',
+			id 			: $(container).attr('data-id')
+		};
+
+		jQuery.ajax({
+				url  : ajaxdata.ajaxurl,
+				type: 'POST',
+				data : data,
+				dataType: 'json',
+				success:function(post){
+					console.log(post);
+					$.markItUp({target:"#bbp_reply_content", openWith:'[quote id='+ post.ID +']\n'+ $.trim(post.post_content), closeWith:'\n[/quote]' });
+				
+				}
+			});	
+   			
+   	});
 
 
 
