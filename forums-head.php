94449e0378ee99f7a45ec00b60dcc7ee2b118468
Dan Crimmins <dbcrimmins@gmail.com>
2013-09-18 11:46:23 -0500

    removed pagination from occurring on subforums listing on archive page; added heading/subheading section and fields to forums settings page

--- forums-head.php
+++ forums-head.php
@@ -11,11 +11,19 @@
 <div id="forums-head">
 	<?php if(bbp_is_forum_archive()):?>
 	
-		<h2>[Forums logo] Forums Header</h2>
+		<h2><?php echo get_option('_bbp_forum_archive_heading', 'Forums Header Text');?></h2>
+		<p><?php echo get_option('_bbp_forum_archive_subheading', 'Forums Sub-header Text');?></p>
 		
 	<?php else: ?>
 	
 		<h2><?php header_breadcrumbs();?></h2>
+		
+		<?php //For topics (subforums list) page only
+			if(has_subforums()): ?>
+			
+			<p><?php echo get_option('_bbp_forum_topics_subheading', 'Forums Topic Sub-header Text');?></p>
+		
+		<?php endif;?>
 		
 	<?php endif;?>
 </div>
