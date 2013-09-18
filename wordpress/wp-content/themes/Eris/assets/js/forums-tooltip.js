51dec73afc6e6ca8de895850c68f99f7da170c4a
Matt Strick <matthew.strick@searshc.com>
2013-09-18 18:15:44 -0500

    more updates for forums.

--- wordpress/wp-content/themes/Eris/assets/js/forums-tooltip.js
+++ wordpress/wp-content/themes/Eris/assets/js/forums-tooltip.js
@@ -1,10 +1,10 @@
 // Welcome Tooltip work
 (function ($) {
 	// Open the tooltip if not cookie is set
-	/* TODO: Check cookie and open the tooltip if appropriate*/
-	if (true) {
+	if ((shcJSL.cookies("forums-tooltip").serve() != 'true')) {
+		//console.log("NO COOKIES AT ALL!");
+		$('#forum_tooltip').show();
 		
-	
 		// Close the tooltip on click
 		$('#forum_tooltip .close').on('click', function() { 
 			var input = $(this).parent().find('#message_hide');
@@ -12,14 +12,17 @@
 			/* TODO:  Check the form value to see whether to set a session cookie or the full 90 day one.*/
 			if (input.attr('checked')) {
 				// set forever cookie
+				shcJSL.cookies("forums-tooltip").bake({value: true, expiration: '1y'});
 			} else {
 				// set session cookie
+				shcJSL.cookies("forums-tooltip").bake({value: true});
 			}
 			
 			// Hide the tooltip
 			$(this).parent().hide(); 
 		});	
 	} else {	// Remove the tooltip.
+		//console.log("COOKIES!");
 		$('#forum_tooltip').remove();
 	}
 	
