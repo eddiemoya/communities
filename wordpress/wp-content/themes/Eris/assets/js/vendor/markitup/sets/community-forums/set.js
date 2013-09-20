da513a8b207c5162d2a77b1d8404c851b4939850
Eddie Moya <eddie.moya@gmail.com>
2013-09-20 12:44:38 -0500

    fixed UL and OL so they start with a single LI in focus. Made double newlines for CtrlEnter and ShiftEnter, added Underline JS

--- wordpress/wp-content/themes/Eris/assets/js/vendor/markitup/sets/community-forums/set.js
+++ wordpress/wp-content/themes/Eris/assets/js/vendor/markitup/sets/community-forums/set.js
@@ -7,16 +7,17 @@
 mySettings = {
     nameSpace:       "xbbcode", // Useful to prevent multi-instances CSS conflict
 	previewParserPath:	'', // path to your XBBCode parser
-	onShiftEnter:	{keepDefault:false, replaceWith:'[br /]\n'},
-	onCtrlEnter:	{keepDefault:false, openWith:'\n[p]', closeWith:'[/p]\n'},
+	onShiftEnter:	{keepDefault:false, openWith:'\n\n'},
+	onCtrlEnter:	{keepDefault:false, openWith:'\n\n'},
 	onTab:			{keepDefault:false, openWith:'	 '},
 	markupSet: [
 		{name:'Bold', className:'markItUp_B', key:'B', openWith:'[b]', closeWith:'[/b]' },
 		{name:'Italic', className:'markItUp_I', key:'I', openWith:'[i]', closeWith:'[/i]' },
+		{name:'Underline', className:'markItUp_U', key:'U', openWith:'[u]', closeWith:'[/u]' },
 		{name:'Stroke through', className:'markItUp_Strike', key:'S', openWith:'[s]', closeWith:'[/s]' },
 		{separator:'---------------' },
-		{name:'Ul', className:'markItUp_UL', openWith:'[ul]\n', closeWith:'[/ul]\n' },
-		{name:'Ol', className:'markItUp_OL', openWith:'[ol]\n', closeWith:'[/ol]\n' },
+		{name:'Ul', className:'markItUp_UL', openWith:'[ul]\n  [li]', closeWith:'[/li]\n[/ul]\n' },
+		{name:'Ol', className:'markItUp_OL', openWith:'[ol]\n  [li]' , closeWith:'[/li]\n[/ol]\n' },
 		{name:'Li', className:'markItUp_LI', openWith:'[li]', closeWith:'[/li]' },
 		{separator:'---------------' },
 		{name:'Quotes', className: 'markItUp_Quote', openWith:'[quote]', closeWith:'[/quote]'},
