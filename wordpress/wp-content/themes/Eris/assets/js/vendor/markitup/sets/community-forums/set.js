39e9d3ee00bba83c1411d517e3f6f1d400390f56
Eddie Moya <eddie.moya@gmail.com>
2013-09-20 19:44:38 -0500

    changing the makitup json set to automatically include a space after closing tags to workaround the double shortcode problem

--- wordpress/wp-content/themes/Eris/assets/js/vendor/markitup/sets/community-forums/set.js
+++ wordpress/wp-content/themes/Eris/assets/js/vendor/markitup/sets/community-forums/set.js
@@ -11,17 +11,17 @@ mySettings = {
 	onCtrlEnter:	{keepDefault:false, openWith:'\n\n'},
 	onTab:			{keepDefault:false, openWith:'	 '},
 	markupSet: [
-		{name:'Bold', className:'markItUp_B', key:'B', openWith:'[b]', closeWith:'[/b]' },
-		{name:'Italic', className:'markItUp_I', key:'I', openWith:'[i]', closeWith:'[/i]' },
+		{name:'Bold', className:'markItUp_B', key:'B', openWith:'[b]', closeWith:'[/b] ' },
+		{name:'Italic', className:'markItUp_I', key:'I', openWith:'[i]', closeWith:'[/i] ' },
 		{name:'Underline', className:'markItUp_U', key:'U', openWith:'[u]', closeWith:'[/u]' },
-		{name:'Stroke through', className:'markItUp_Strike', key:'S', openWith:'[s]', closeWith:'[/s]' },
+		{name:'Stroke through', className:'markItUp_Strike', key:'S', openWith:'[s]', closeWith:'[/s] ' },
 		{separator:'---------------' },
 		// {name:'Ul', className:'markItUp_UL', openWith:'[ul]\n  [li]', closeWith:'[/li]\n[/ul]\n' },
 		// {name:'Ol', className:'markItUp_OL', openWith:'[ol]\n  [li]' , closeWith:'[/li]\n[/ol]\n' },
 		// {name:'Li', className:'markItUp_LI', openWith:'[li]', closeWith:'[/li]' },
 		// {separator:'---------------' },
-		{name:'Quotes', className: 'markItUp_Quote', openWith:'[quote]', closeWith:'[/quote]'},
-		{name:'Code', className: 'markItUp_Code', openWith:'[code]', closeWith:'[/code]'}, 
+		{name:'Quotes', className: 'markItUp_Quote', openWith:'[quote]', closeWith:'[/quote] '},
+		{name:'Code', className: 'markItUp_Code', openWith:'[code]', closeWith:'[/code] '}, 
 		
 		{name:'Clean', className:'clean', replaceWith:function(markitup) { return markitup.selection.replace(/\[(.*?)\]/g, "") } },
 	]
