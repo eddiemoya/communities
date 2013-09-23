99a52a4bc7890efbc5ac4f7483019ede2538cef3
Matt Strick <matthew.strick@searshc.com>
2013-09-23 16:54:01 -0500

    Back to the top anchor link does not go all the way to the top, only halfway

--- assets/js/forums-tooltip.js
+++ assets/js/forums-tooltip.js
@@ -27,3 +27,10 @@
 	}
 	
 }(jQuery));
+
+
+// TODO: Move to a more general forums location - Matt 
+$(".bbp-footer a[href='#replies-top']").on('click', function(e) {
+	e.preventDefault();
+	window.scrollTo(0,0);
+});
