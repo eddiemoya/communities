350692b474d1bcf6c71bd3bd9fba5864ce7b0bb5
Dan Crimmins <dbcrimmins@gmail.com>
2013-10-22 14:38:05 -0500

    removed buying guides post type registration

--- wordpress/wp-content/themes/Eris/functions/post-types.php
+++ wordpress/wp-content/themes/Eris/functions/post-types.php
@@ -75,50 +75,6 @@ function register_questions_type() {
     register_post_type('question', $args);
 }
 
-/**
- * @author Jason Corradino
- */
-function register_buying_guides_type() {
-    $labels = array(
-        'name' => _x('Buying Guides', 'post type general name'),
-        'singular_name' => _x('Buying Guide', 'post type singular name'),
-        'add_new' => _x('Add New', 'Guide'),
-        'add_new_item' => __('Add New Guide'),
-        'edit_item' => __('Edit Guide'),
-        'new_item' => __('New Guide'),
-        'all_items' => __('All Guides'),
-        'view_item' => __('View Guide'),
-        'search_items' => __('Search Guides'),
-        'not_found' => __('No buying guides found'),
-        'not_found_in_trash' => __('No buying guides found in Trash'),
-        'parent_item_colon' => '',
-        'menu_name' => 'Buying Guides'
-    );
-	$rewrite = array(
-        'slug'          => 'guides',
-        'with_front'    => false,
-        'feeds'         => true,
-        'paged'         => true,
-        'ep_mask'       => array()
-    );
-    $args = array(
-        'labels'        => $labels,
-        'public'        => true,
-        'publicly_queryable' => true,
-        'show_ui'       => true,
-        'show_in_menu'  => true,
-        'query_var'     => false,
-		'rewrite'         => $rewrite,
-        'capability_type' => 'post',
-        'has_archive' => true,
-        'hierarchical' => false,
-        'menu_position' => 7,
-        'supports' => array('title', 'editor', 'author', 'comments', 'thumbnail', 'excerpt'),
-        //'menu_icon' => get_template_directory_uri() . '/assets/img/admin/questions_admin_icon.gif',
-        'taxonomies' => array('category', 'post_tag')
-    );
-    register_post_type('guide', $args);
-}
 
 
 function strip_oembed_from_excerpt($excerpt){
