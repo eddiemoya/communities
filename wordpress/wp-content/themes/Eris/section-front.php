29f129e659adc0f2e48422a51a80fd347b10be4e
Eddie Moya <eddie.moya@gmail.com>
2013-10-28 15:32:56 -0500

    checking to see node plugin is on before using node factory

--- wordpress/wp-content/themes/Eris/section-front.php
+++ wordpress/wp-content/themes/Eris/section-front.php
@@ -2,52 +2,54 @@
 
 get_template_part('parts/header');
 	
-	$term = wp_get_object_terms($post->ID, $post->post_type);
-	$term = $term[0];
-	$node = new WP_Node_Factory($term->taxonomy);
-	$node->create_node($term->term_id);
+	if(class_exists('WP_Node_Factory')){
+		$term = wp_get_object_terms($post->ID, $post->post_type);
+		$term = $term[0];
+		$node = new WP_Node_Factory($term->taxonomy);
+		$node->create_node($term->term_id);
 
-	$filter = get_query_var('sf_filter');
-	
-	$layout_id = $node->get_node_meta("sf_{$filter}_template");
-	//$layout_id = ($layout_setting > 0 ) ? $layout_setting : $node->post->ID;
+		$filter = get_query_var('sf_filter');
+		
+		$layout_id = $node->get_node_meta("sf_{$filter}_template");
+		//$layout_id = ($layout_setting > 0 ) ? $layout_setting : $node->post->ID;
 
-	$tax_query[] = array(
-		'taxonomy' => $node->term->taxonomy,
-		'terms' => $node->term->term_id,
-		'field' => 'id'
-	);
+		$tax_query[] = array(
+			'taxonomy' => $node->term->taxonomy,
+			'terms' => $node->term->term_id,
+			'field' => 'id'
+		);
 
-	$post_type = '';
-	switch($filter){
-		case 'post': 
-		case 'guide':
-		case 'question':
-			$post_type = $filter; 
+		$post_type = '';
+		switch($filter){
+			case 'post': 
+			case 'guide':
+			case 'question':
+				$post_type = $filter; 
+				break;
+			case 'category':
+				$post_type = array('post', 'guide', 'question');
+				break;
+			case 'video':
+				$tax_query[] = array(
+					'taxonomy' => 'post_format',
+					'terms' => $filter,
+					'field' => 'slug'
+				);
+				$tax_query['relation'] = 'AND';
+				$post_type = array('post', 'guide', 'question');
 			break;
-		case 'category':
-			$post_type = array('post', 'guide', 'question');
-			break;
-		case 'video':
-			$tax_query[] = array(
-				'taxonomy' => 'post_format',
-				'terms' => $filter,
-				'field' => 'slug'
-			);
-			$tax_query['relation'] = 'AND';
-			$post_type = array('post', 'guide', 'question');
-		break;
+		}
+
+		$query = array(
+			'post_type' => $post_type,
+			'tax_query' => $tax_query
+		);
+
+		query_posts($query);
+		
+		WidgetPress_Controller_Widgets::display_dropzones($layout_id);
+		wp_reset_query();
 	}
-
-	$query = array(
-		'post_type' => $post_type,
-		'tax_query' => $tax_query
-	);
-
-	query_posts($query);
-	
-	WidgetPress_Controller_Widgets::display_dropzones($layout_id);
-	wp_reset_query();
 			
 
 
