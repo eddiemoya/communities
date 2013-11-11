faad28b0512fdfe078f1797fd54606e264c8f087
Matt Strick <matthew.strick@searshc.com>
2013-11-11 13:18:41 -0600

    PHP-989 For single topics the h1 is now the topic title instead of the forum and sub-forum names.

--- forums-head.php
+++ forums-head.php
@@ -8,15 +8,17 @@
 
 ?>
 
-<div id="forums-head">
+<div id="forums-head">	
 	<?php if(bbp_is_forum_archive()):?>
-
 		<h1><?php echo get_option('_bbp_forum_archive_heading', 'Forums Header Text');?></h1>
 		<p><?php echo get_option('_bbp_forum_archive_subheading', 'Forums Sub-header Text');?></p>
 		
-	<?php else: ?>
-	
-		<h1><?php header_breadcrumbs();?></h1>
+	<?php else: ?>				
+		<?php if(!bbp_is_single_topic()): ?>
+			<h1><?php header_breadcrumbs();?></h1>
+		<?php else: // For single-topics where the H1 is defined in content-single-topic-lead.php ?>
+			<p id="header_breadcrumbs"><?php header_breadcrumbs();?><p>
+		<?php endif;?>
 		
 		<?php //For topics (subforums list) page only
 			if(has_subforums()): ?>
