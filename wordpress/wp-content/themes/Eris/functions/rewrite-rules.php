7f3a6acd243bb3f96cb1658446fd846ad8c9c9fe
Eddie Moya <eddie.moya@gmail.com>
2014-01-28 17:18:46 -0600

    modifying rewrite rules to allow category archives, post type archives, and cat/pt intersections to work on section fronts

--- wordpress/wp-content/themes/Eris/functions/rewrite-rules.php
+++ wordpress/wp-content/themes/Eris/functions/rewrite-rules.php
@@ -1,8 +1,8 @@
 <?php 
 
 
-add_filter('category_rewrite_rules', 'category_post_type_endpoints');
-add_filter('post_rewrite_rules', 'posts_endpoint');
+//add_filter('category_rewrite_rules', 'category_post_type_endpoints');
+add_filter('rewrite_rules_array', 'posts_endpoint');
 add_filter('post_format_rewrite_rules', 'post_format_endpoint');
 
 add_filter('query_vars', 	 'add_vars');
@@ -45,9 +45,7 @@ function posts_endpoint($rules){
 
 	$newrules = array();
 	
-	$newrules['posts?/?$'] = 'index.php?post_type=post';
-	
-	//$newrules['video?/?$'] = 'index.php?post_format=video';
+	unset($rules['questions/?$']);
 
 	//echo "<pre>";print_r($newrules + $rules);echo "</pre>";
 	return $newrules + $rules;
@@ -59,8 +57,8 @@ function posts_endpoint($rules){
 function category_post_type_endpoints($rules){
 
     $newrules = array();
-	$newrules['category/(.+?)/(guide|question|post|video)s?/?$'] = 'index.php?post_type=category&name=$matches[1]&sf_filter=$matches[2]';
-	$newrules['category/(.+?)/?$'] = 'index.php?post_type=category&name=$matches[1]&sf_filter=category';
+	// $newrules['category/(.+?)/(guide|question|post|video)s?/?$'] = 'index.php?post_type=category&category_name=$matches[1]&sf_filter=$matches[2]';
+	// $newrules['category/(.+?)/?$'] = 'index.php?post_type=category&category_name=$matches[1]&sf_filter=category';
 
     //$newrules['category/(.+?)/(guide|question|post)s?/page/?([0-9]{1,})/?$'] = 'index.php?category_name=$matches[1]&post_type=$matches[2]&paged=$matches[3]';
     // $newrules['category/(.+?)/videos?/?$'] = 'index.php?category_name=$matches[1]&post_format=video';
@@ -73,4 +71,48 @@ function category_post_type_endpoints($rules){
 	return $newrules;// + $rules;
 
 }
+
+
+
+function wptuts_add_rewrite_rules() {  
+    add_rewrite_rule(  
+        '^(category|post|question)/([^/]+)/?$', // String followed by a slash, followed by a date in the form '2012-04-21', followed by another slash  
+        'index.php?post_type=$matches[1]&name=$matches[2]&sf_filter=$matches[1]',  
+        'top'  
+    );  
+     add_rewrite_rule(  
+        '^(category|post|question)/([^/]+)/(guide|question|post|video)s?/?$', // String followed by a slash, followed by a date in the form '2012-04-21', followed by another slash  
+        'index.php?post_type=$matches[1]&name=$matches[2]&sf_filter=$matches[3]',  
+        'top'  
+    );
+     add_rewrite_rule(  
+        '((question|post)s)/?$', // String followed by a slash, followed by a date in the form '2012-04-21', followed by another slash  
+        'index.php?pagename=$matches[1]&sf_filter=$matches[2]',  
+        'top'  
+    );   
+}  
+add_action( 'init', 'wptuts_add_rewrite_rules' );  
+
+
+
+function wptuts_register_rewrite_tag() {  
+    add_rewrite_tag( '%post_type%', '([0-9]{4}-[0-9]{2}-[0-9]{2})');  
+}  
+add_action( 'init', 'wptuts_register_rewrite_tag');  
+
+
+
+function filter_post_links( $permalink, $post ) {  
+  
+    // Check if the %post_type% tag is present in the url:  
+    if ( false === strpos( $permalink, '%post_type%' ) )  
+        return $permalink;  
+  
+  
+    // Replace '%post_type%'  
+    $permalink = str_replace( '%post_type%', $post->post_type , $permalink );  
+  
+    return $permalink;  
+}  
+add_filter( 'post_link', 'filter_post_links' , 10, 2 );  
 
