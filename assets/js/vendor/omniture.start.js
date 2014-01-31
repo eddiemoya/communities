1af7e1498a7cd82d23f1802b8a9b1b0f76ea464a
Strick <mstric1@ushofml314140.kih.kmart.com>
2014-01-31 12:02:11 -0600

    Fixes for invoking omniture tracking code. Social Sharing called too early, and Content Tracking was never being called.

--- assets/js/vendor/omniture.start.js
+++ assets/js/vendor/omniture.start.js
@@ -97,6 +97,8 @@ var omniture = (function () {
 	};
 }());
 
+/* Invoking GLobal Functions */
+omniture.contentTracking();
 
 /************* DO NOT ALTER ANYTHING BELOW THIS LINE ! **************/
 var s_code=s.t();if(s_code)document.write(s_code)//-->
